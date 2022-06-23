f3b952082b03ba98dc650a19265a88ec07c2cb5a
Houser97 <arturo.riverar97@gmail.com>
2022-06-22 20:11:40 -0500

    Create Navbar component
    The component holds two options to change the current page.
        The Home option
        The Shop option

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import Header from './components/header';
+import Navbar from './components/navbar';
+import {useState} from "react";
 
 function App() {
+
+  const [itemsInCar, setItemsInCar] = useState(10);
+
   return (
     <div className="App">
-      <Header />
+      <Header numberItemsInCar = {itemsInCar}/>
+      <div className='fullHeight'>
+        <div className='app-body'>
+          <Navbar />
+        </div>
+      </div>
     </div>
   );
 }

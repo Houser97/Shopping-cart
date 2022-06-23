6b2c2d0ed5cf73591d5955555deb15fdeddc9eb3
Houser97 <arturo.riverar97@gmail.com>
2022-06-22 19:30:09 -0500

    Create Header component
    The component has a menu svg to open the upcoming Navbar component.
    Likewise, the title is in the middle of the section and the cart svg
    with the number of elements in it is at the end of the header.

---
+++ shopping-cart/src/components/header.js
@@ -0,0 +1,24 @@
+import '../styles/header.css'
+
+const Header = () => {
+    return(
+        <header>
+            <div className="display-navbar">
+                <svg className='menu-svg' viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+                </svg>
+            </div>
+            <div className="title-project">Shopping Cart</div>
+            <div className='cart-icon-header-number'>
+                <div className='number-cart-header'>1</div>
+                <div className="cart-icon-header">
+                    <svg className='cart-svg' viewBox="0 0 24 24">
+                        <path fill="currentColor" d="M17,18C15.89,18 15,18.89 15,20A2,2 0 0,0 17,22A2,2 0 0,0 19,20C19,18.89 18.1,18 17,18M1,2V4H3L6.6,11.59L5.24,14.04C5.09,14.32 5,14.65 5,15A2,2 0 0,0 7,17H19V15H7.42A0.25,0.25 0 0,1 7.17,14.75C7.17,14.7 7.18,14.66 7.2,14.63L8.1,13H15.55C16.3,13 16.96,12.58 17.3,11.97L20.88,5.5C20.95,5.34 21,5.17 21,5A1,1 0 0,0 20,4H5.21L4.27,2M7,18C5.89,18 5,18.89 5,20A2,2 0 0,0 7,22A2,2 0 0,0 9,20C9,18.89 8.1,18 7,18Z" />
+                    </svg>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;

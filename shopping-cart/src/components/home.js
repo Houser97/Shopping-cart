9e4bc566e8deeb5bc12568f9c1ccc926198e7167
Houser97 <arturo.riverar97@gmail.com>
2022-06-23 13:50:57 -0500

    Create Home section
    The home section contains random information simulating it is about
    the application. It has two png images which were given a keyframe
    effect of translation in the Y-axis.

--- shopping-cart/src/components/home.js
+++ shopping-cart/src/components/home.js
@@ -1,7 +1,38 @@
+import '../styles/home.css';
+import Imagen from './ShoppingCart.png'
+import Items from './ShoppingItems.png'
+
 const Home = () => {
     return(
         <div className="home-page">
-            Hi from home
+        {/*
+            <div className='news'>
+                <div className='news-title'>--Upcoming new items--</div>
+                <div className='image-slider'></div>
+            </div>
+            */}
+            <div className='about-section'>
+                <div className='news-title'>HOME</div>
+                <div className='text-image'>
+                    <div className='text'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in ipsum non nisi vehicula tristique lacinia et libero. Sed vitae lorem eu orci ornare fermentum sed sit amet magna. Mauris sagittis augue vel pretium ullamcorper. In eget quam at ex condimentum tincidunt. Integer eget dictum leo.</p>
+                    <p>Morbi et quam eget velit elementum cursus. In ornare nulla sem, id porttitor neque interdum eu. Donec vulputate, nulla at porta lobortis, ipsum quam facilisis odio, nec fringilla mi nisi ac mauris. Quisque et odio sit amet ligula fringilla imperdiet.</p>
+                    </div>
+                    <div className='image-home'>
+                        <img src={Items} alt='H'></img>
+                    </div>
+                </div>
+
+                <div className='text-image'>
+                <div className='image-home'>
+                        <img src={Imagen} alt='H'></img>
+                    </div>
+                    <div className='text'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in ipsum non nisi vehicula tristique lacinia et libero. Sed vitae lorem eu orci ornare fermentum sed sit amet magna. Mauris sagittis augue vel pretium ullamcorper. In eget quam at ex condimentum tincidunt. Integer eget dictum leo.</p>
+                    <p>Morbi et quam eget velit elementum cursus. In ornare nulla sem, id porttitor neque interdum eu. Donec vulputate, nulla at porta lobortis, ipsum quam facilisis odio, nec fringilla mi nisi ac mauris. Quisque et odio sit amet ligula fringilla imperdiet.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- shopping-cart/src/components/shop.js
+++ shopping-cart/src/components/shop.js
@@ -1,3 +1,5 @@
+import '../styles/shop.css';
+
 const Shop = () => {
     return(
         <div className="shop-page">

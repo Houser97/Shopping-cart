8c8bd96a1b1047ef12df69ee7444601e753078af
Houser97 <arturo.riverar97@gmail.com>
2022-06-22 20:31:17 -0500

    Close and show navbar if button menu selected

--- shopping-cart/src/components/header.js
+++ shopping-cart/src/components/header.js
@@ -2,11 +2,39 @@ import '../styles/header.css'
 
 const Header = (props) => {
     const {numberItemsInCar} = props;
+
+    const hideNavbar = () => {
+        const navbar = document.querySelector(".navbar");
+        navbar.style.width = "14rem";
+        const menuSVG = document.querySelector(".display-navbar")
+        menuSVG.style.display = "none";
+        const xSVG = document.querySelector(".hide-navbar");
+        xSVG.style.display = "flex";
+        const navbarOptions = document.querySelector(".navbar-options");
+        navbarOptions.style.display = "flex";
+    }
+
+    const showNavbar = () => {
+        const navbar = document.querySelector(".navbar");
+        navbar.style.width = "0px";
+        const menuSVG = document.querySelector(".display-navbar")
+        menuSVG.style.display = "flex";
+        const xSVG = document.querySelector(".hide-navbar");
+        xSVG.style.display = "none";
+        const navbarOptions = document.querySelector(".navbar-options");
+        navbarOptions.style.display = "none";
+    }
+
     return(
         <header>
-            <div className="display-navbar">
+            <div className="display-navbar" onClick={hideNavbar}>
                 <svg className='menu-svg' viewBox="0 0 24 24">
                     <path fill="currentColor" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+                </svg>
+            </div>
+            <div className="hide-navbar" onClick={showNavbar}>
+                <svg className='close-svg' viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
                 </svg>
             </div>
             <div className="title-project">Shopping Cart</div>

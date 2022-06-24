cbafb7792650ec3c2991401f97ab7c13d4da96c5
Houser97 <arturo.riverar97@gmail.com>
2022-06-23 22:55:41 -0500

    Create unit tests for Card component
    The feature of decreasing or increasing the amount of products was
    tested to verify whether it worked properly or not. The functions
    to achieve these features were changed a bit in order to correct the
    first mistakes, in which the click events happened so fast that the
    event handlers were using obsolete data.
    
    On the other hand, in order to prevent the Card component from
    decreasing the amount of products below 0, this condition is also
    evaluated within the useEffect hook, as the conditional inside the
    decrement function was not enough when the events happen too fast.

--- shopping-cart/src/App.test.js
+++ shopping-cart/src/App.test.js
@@ -1,8 +1,62 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 import App from './App';
+import Card from './components/card';
+import Apple from './images/Apple.jpg';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('Card component tests', () => {
+  it("Increments amount of products correctly", () =>{
+    render(<Card image= {Apple} imageName = {"Apple"} price = {10} id={0}/>)
+    const buttonIncrease = screen.getByTestId("0");
+    const DIV = screen.getByTestId('01');
+    act(()=>{
+      for(let i = 0; i<3; i++){
+        userEvent.click(buttonIncrease);
+      }
+    })
+     expect(DIV.innerHTML).toBe("4");
+  })
+
+  it("Decrements amount of products correctly", () => {
+    render(<Card image= {Apple} imageName = {"Apple"} price = {10} id={0}/>);
+    const decreaseButton = screen.getByTestId("decrement");
+    const increaseButton = screen.getByTestId("0");
+    const DIV = screen.getByTestId("01");
+
+    act(() => {
+      for(let i = 0; i<10; i++){
+        userEvent.click(increaseButton);
+      }
+    })
+
+    act(() => {
+      for(let j = 0; j<5; j++){
+        userEvent.click(decreaseButton);
+      }
+    })
+    expect(DIV.textContent).toBe("6");
+  })
+
+  it("Does not decrement below 1", () => {
+    render(<Card image= {Apple} imageName = {"Apple"} price = {10} id={0}/>);
+    const decreaseButton = screen.getByTestId("decrement");
+    const increaseButton = screen.getByTestId("0");
+    const DIV = screen.getByTestId("01");
+
+    act(() => {
+      for(let i = 0; i<2; i++){
+        userEvent.click(increaseButton);
+      }
+    })
+
+    act(() => {
+      for(let j = 0; j<45; j++){
+        userEvent.click(decreaseButton);
+      }
+    })
+
+    expect(DIV.textContent).toBe("1");
+  })
+})
--- shopping-cart/src/components/card.js
+++ shopping-cart/src/components/card.js
@@ -10,17 +10,20 @@ const Card = (props) => {
     useEffect(()=>{
         const divNumberProducts = document.getElementById(`${currentId}-DIV-products`);
         divNumberProducts.textContent = numberOfProducts;
+        if (numberOfProducts < 1) {
+            setNumberOfProducts(1);
+        } 
     },[numberOfProducts, currentId])
 
     function decrementProduct(e){
         if (numberOfProducts !== 1) {
-            setNumberOfProducts(numberOfProducts - 1);
+            setNumberOfProducts(numberOfProducts => numberOfProducts - 1);
             setCurrentId(e.target.id);
-        }
+        } 
     }
     
     function incrementProduct(e){
-        setNumberOfProducts(numberOfProducts + 1);
+        setNumberOfProducts(numberOfProducts => numberOfProducts + 1);
         setCurrentId(e.target.id);
     }
 
@@ -34,13 +37,13 @@ const Card = (props) => {
                 <div className='name-product'>{imageName}</div>
             </div>
             <div className="card-decrase-increase">
-                <button id={id} className='decrease-button button-IncDec' onClick={decrementProduct}>
+                <button id={id} data-testid = "decrement" className='decrease-button button-IncDec' onClick={decrementProduct}>
                     <svg id={id}  className='minus-svg svg-IncDec' viewBox="0 0 24 24">
                         <path id={id}  fill="currentColor" d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M7,13H17V11H7" />
                     </svg>
                 </button>
-                <div id={`${id}-DIV-products`}  className='number-products'>{numberOfProducts}</div>
-                <button id={id}  className='increase-button button-IncDec' onClick={incrementProduct}>
+                <div id={`${id}-DIV-products`} data-testid="01" className='number-products'>{numberOfProducts}</div>
+                <button id={id} data-testid="0" className='increase-button button-IncDec' onClick={incrementProduct}>
                     <svg id={id}  className='plus-svg svg-IncDec' viewBox="0 0 24 24">
                         <path id={id}  fill="currentColor" d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M13,7H11V11H7V13H11V17H13V13H17V11H13V7Z" />
                     </svg>

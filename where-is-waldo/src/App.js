40e2250e72e9f75ab1b7db47cd7205ac9fa7ca31
Houser97 <arturo.riverar97@gmail.com>
2022-06-29 17:21:19 -0500

    Return DIV containing image for the game

--- where-is-waldo/src/App.js
+++ where-is-waldo/src/App.js
@@ -1,8 +1,22 @@
+import { useRef } from 'react';
 import './App.css';
+import image from './adventure-time.png'
+
 
 function App() {
+
+  const imgRef = useRef(null)
+  const square = useRef(null)
+
+  const eventDIV = (e) => {
+
+  } 
+
+
   return (
     <div className="App">
+      <img src={image} alt='cartoon-network' className='img-project' ref={imgRef} onClick={eventDIV}></img>
+      <div className='magic-div' ref={square}></div>
     </div>
   );
 }

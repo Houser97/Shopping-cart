d7c78789df6cdf6bf95090716b42687c7de3f38e
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-15 21:00:55 -0600

    Import productsData and optimize function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Shop from './components/Shop';
 import {useState, createContext, useEffect} from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { productsData } from './assets/constants';
 
 export const CartContext = createContext()
 
@@ -13,43 +14,15 @@ function App() {
   const [productsInCar, setProductsInCar] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
 
-  const removeProduct = (e) => {
-    const ID = e.target.id;
-    let productsInCarHelper = productsInCar;
-    let iterator = 0;
-    let elementPosition = 0;
-    productsInCar.forEach(product => {
-      if(product.id === ID){
-        elementPosition = iterator;
-      }
-      iterator ++;
-    })
-    setProductsInCar([...productsInCarHelper.slice(0,elementPosition), ...productsInCarHelper.slice(elementPosition+1)]);
+  const checkIfProductAlreadyInCar = (productId) => {
+    return productsInCar.some(product => product.id === productId);
   }
 
-  const checkIfProductAlreadyInCar = (currentProductId) => {
-    let productsIdInCar = []
-    productsInCar.forEach(product => productsIdInCar.push(product.id));
-    const isProductAlreadyInCar = productsIdInCar.includes(currentProductId);
-    return isProductAlreadyInCar;
-  }
-
-  const addCurrentProductQuantity = (productId, Quantity, newProduct) => {
-    let productsInCarHelper = [...productsInCar];
-    let iterator = 0;
-    let positionProduct = 0;
-    let quantityToAdd = parseInt(newProduct.quantity);
-
-    productsInCar.forEach(product => {
-      if(product.id === productId){
-       quantityToAdd += parseInt(product.quantity);
-       positionProduct = iterator;
-      }
-      iterator ++;
-    })
-    newProduct.quantity = quantityToAdd;
-    productsInCarHelper[positionProduct] = newProduct;
-    setProductsInCar([...productsInCarHelper]);
+  const addCurrentProductQuantity = (newQuantity, productId) => {
+    const productsHelper = [...productsInCar]
+    const productIndex = productsHelper.findIndex(product => product.id === productId)
+    productsHelper[productIndex].quantity += newQuantity
+    setProductsInCar([...productsHelper]);
   }
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -62,31 +35,26 @@ function App() {
   }
 
   const updateTotalProductInCard = () => {
-    let products = 0;
-    productsInCar.forEach(product => {
-      products += parseInt(product.quantity);
-    })
-    setItemsInCar(products)
+    setItemsInCar(productsInCar.reduce((acc, current) => acc + current, 0))
   }
 
-  const addProduct = (e) => {
-    const ID = e.target.id;
-    let productRaw = document.getElementById(`${ID}-card`);
-    let childs = productRaw.childNodes;
-
-    const price = childs[0].textContent.slice(1);
-    const imgSrc = childs[1].childNodes[0].src;
-    const nameProduct = childs[2].textContent;
-    const quantityProducts = childs[3].childNodes[1].textContent;
-
-    const product = {image: imgSrc, name: nameProduct, price: price, id: ID, quantity: quantityProducts}
-
-    if(checkIfProductAlreadyInCar(ID)){
-      addCurrentProductQuantity(ID, quantityProducts, product)
+  const addProduct = (numberOfProducts, id) => {
+    if(checkIfProductAlreadyInCar(id)){
+      addCurrentProductQuantity(numberOfProducts, id)
     } else {
+      /*usar [...] crea SHALLOW COPY del arreglo pero las referencias a los objetos siguen presente. */
+      /*Por esta razón se debe aplicar DEEP COPY */
+      let helper = structuredClone(productsData)
+      const product = helper.filter(product => product.id === id)[0];
+      product.quantity = numberOfProducts;
       setProductsInCar([...productsInCar, product])
     }
   }
+
+  const removeProduct = (e) => {
+    const id = e.target.id;
+    setProductsInCar(oldArray => oldArray.filter(product => product.id !== id))
+  } 
 
   useEffect(() => {
     changeTotalPrice();

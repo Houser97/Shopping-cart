356208998221311cb1ec0644c27dcd7d8401492b
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-16 21:39:03 -0600

    Import CartContext and set removeProduct function

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -1,9 +1,9 @@
 import '../styles/CartCard.css'
-import { RemoveElementContext } from '../App';
+import { CartContext } from '../App';
 import { useContext } from 'react';
 
 const CartCard = ({submission}) => {
-    const removeElement = useContext(RemoveElementContext);
+    const removeProduct = useContext(CartContext).removeProduct;
 
     return(
         submission.map(
@@ -11,7 +11,7 @@ const CartCard = ({submission}) => {
                 const {image, name, price, id, quantity} = item;
                 return(
                     <div id={id} key = {iterator} className="card-cart">
-                        <div id={id} className='remove-product' onClick={removeElement}>
+                        <div id={id} className='remove-product' onClick={removeProduct}>
                             <svg id={id} viewBox="0 0 24 24">
                                 <path id={id} fill="currentColor" d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" />
                             </svg>
@@ -25,7 +25,6 @@ const CartCard = ({submission}) => {
                                 <div className="price-cart">Unit price: ${price}</div>
                                 <div data-testid={`${iterator}-quantity-display`} className="quantity-cart">Quantity: {quantity}</div>
                             </div>
-                            
                         </div>
                     </div>
                 )

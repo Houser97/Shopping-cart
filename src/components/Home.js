463c29ef53f5d23795b2620b3df1038e27499aea
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-17 21:03:34 -0600

    Delete Home component content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,10 @@
 import '../styles/Home.css';
-import Imagen from './ShoppingCart.png'
-import Items from './ShoppingItems.png'
+import main from '../assets/Home/main.jpg'
 
 const Home = () => {
     return(
         <div className="home-page">
-        {/*
-            <div className='news'>
-                <div className='news-title'>--Upcoming new items--</div>
-                <div className='image-slider'></div>
-            </div>
-            */}
-            <div className='about-section'>
-                <div className='news-title'>Home</div>
-                <div className='text-image'>
-                    <div className='text'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in ipsum non nisi vehicula tristique lacinia et libero. Sed vitae lorem eu orci ornare fermentum sed sit amet magna. Mauris sagittis augue vel pretium ullamcorper. In eget quam at ex condimentum tincidunt. Integer eget dictum leo.</p>
-                    <p>Morbi et quam eget velit elementum cursus. In ornare nulla sem, id porttitor neque interdum eu. Donec vulputate, nulla at porta lobortis, ipsum quam facilisis odio, nec fringilla mi nisi ac mauris. Quisque et odio sit amet ligula fringilla imperdiet.</p>
-                    </div>
-                    <div className='image-home'>
-                        <img className='img-home' src={Items} alt='H'></img>
-                    </div>
-                </div>
-
-                <div className='text-image'>
-                <div className='image-home'>
-                        <img className='img-home' src={Imagen} alt='H'></img>
-                    </div>
-                    <div className='text'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in ipsum non nisi vehicula tristique lacinia et libero. Sed vitae lorem eu orci ornare fermentum sed sit amet magna. Mauris sagittis augue vel pretium ullamcorper. In eget quam at ex condimentum tincidunt. Integer eget dictum leo.</p>
-                    <p>Morbi et quam eget velit elementum cursus. In ornare nulla sem, id porttitor neque interdum eu. Donec vulputate, nulla at porta lobortis, ipsum quam facilisis odio, nec fringilla mi nisi ac mauris. Quisque et odio sit amet ligula fringilla imperdiet.</p>
-                    </div>
-                </div>
-            </div>
+            <img className='home-main-img' src={main}></img>
         </div>
     )
 }

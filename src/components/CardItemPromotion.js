d7223cbc520fbca45af48023426a990c71d3e08a
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-19 21:04:26 -0600

    Remove placeholders and set props

--- src/components/CardItemPromotion.js
+++ src/components/CardItemPromotion.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import Controller from '../assets/Home/ControllerPlay.png'
 import '../styles/CardItemPromotion.css'
 
-const CardItemPromotion = () => {
+const CardItemPromotion = ({section, image, price}) => {
   return (
     <div className='cardItemPromotion'>
         <div className='promotion_data'>
-            <div className='section_promotion'>VR Video Games</div>
+            <div className='section_promotion'>{section}</div>
             <div className='offer_duration'>Offer available only this week</div>
-            <div className='price_promotion'>Only</div>
+            <div className='price_promotion'>Only {price}</div>
         </div>
-        <img className='item_promotion_img' alt='item_promotion' src = {Controller}></img>
+        <img className='item_promotion_img' alt='item_promotion' src = {image}></img>
     </div>
   )
 }

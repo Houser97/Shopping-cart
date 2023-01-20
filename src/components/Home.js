4ad51da6baa073fd13abd54fbfe17a71f09f403e
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-19 21:03:54 -0600

    Set up containers and import Banner image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,24 @@
 import '../styles/Home.css';
 import CardItemPromotion from './CardItemPromotion';
+import { itemsPromotion } from '../assets/constants'
+import Banner from '../assets/Home/banner.jpg'
 
 const Home = () => {
     return(
         <div className="home-page">
             <section className='presentation'>
-                <img className='home-main-img' ></img>
-                <CardItemPromotion />
+                <img className='home-main-img' src={Banner} alt='Banner'></img>
+                <div className='promotions-grid'>
+                    {
+                        itemsPromotion.map((item, index) => {
+                            return(
+                                <CardItemPromotion 
+                                key={`promotion-${index}`} 
+                                {...item} />
+                            )
+                        })
+                    }
+                </div>
             </section>
         </div>
     )

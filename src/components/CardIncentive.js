336566773810d3a0d4f593df4dff50b9971ef909
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-19 21:34:26 -0600

    Set up file to create CardIncentive component

---
+++ src/components/CardIncentive.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { svgIncentives } from '../assets/constants'
+
+
+const CardIncentive = () => {
+  return (
+    <div className='card-incentive'>
+        <div className='incentive-container'>
+
+        </div>
+    </div>
+  )
+}
+
+export default CardIncentive

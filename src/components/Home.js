9bf54562f186c57a7b468dfa9b7637c7bc02512b
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-21 14:36:33 -0600

    Remove assets belonging to newly imported components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,14 @@
 import '../styles/Home.css';
-import CardItemPromotion from './CardItemPromotion';
-import { itemsPromotion } from '../assets/constants'
-import Banner from '../assets/Home/banner.jpg'
-import CardIncentive from './CardIncentive';
-import BannerVertical from '../assets/Home/bannerVertical2.jpg'
+import About from './About';
+import Incentives from './Incentives';
+import Presentation from './Presentation';
 
 const Home = () => {
     return(
         <div className="home-page">
-            <section className='presentation'>
-                <div className='presentation-subcontainer'>
-                    <picture className='home-main-img'>
-                        <source 
-                            srcSet={Banner}
-                            media="(min-width: 600px)"
-                        />
-                        <img 
-                            src={BannerVertical}
-                            alt="Banner"
-                        ></img>
-                    </picture>
-                    <div className='promotions-grid'>
-                        {
-                            itemsPromotion.map((item, index) => {
-                                return(
-                                    <CardItemPromotion 
-                                    key={`promotion-${index}`} 
-                                    {...item} />
-                                )
-                            })
-                        }
-                    </div>
-                </div>
-            </section>
-            <section className='incentives'>
-                <CardIncentive />
-            </section>
+            <Presentation />
+            <Incentives />
+            <About />
         </div>
     )
 }

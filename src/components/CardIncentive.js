322b80f68b424e319b5914f353af670e8815682e
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-21 13:45:17 -0600

    Pass props and set them

--- src/components/CardIncentive.js
+++ src/components/CardIncentive.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { svgIncentives } from '../assets/constants'
+import '../styles/CardIncentive.css'
 
 
-const CardIncentive = () => {
+const CardIncentive = ({svg, text1, text2}) => {
   return (
     <div className='card-incentive'>
-        <div className='incentive-container'>
-
-        </div>
+        <div className='card-incentive-text'>{text1}</div>
+        <div className='card-incentive-text2'>{text2}</div>
+        {svg}
     </div>
   )
 }

bcbad525e922237b051f8afd7efe6625851e3a1d
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-22 17:49:49 -0600

    Set categoriesList array and map it to display items in categories

--- src/components/ShopFilter.js
+++ src/components/ShopFilter.js
@@ -2,18 +2,27 @@ import React from 'react'
 import '../styles/ShopFilter.css'
 
 const ShopFilter = () => {
+
+    const categoriesList = ['Videogames','Electronics','Music']
+
   return (
     <div className='shop-filter'>
-        <div className='categories'>
+        <div className='categories filter-subsection'>
             <h3>Categories</h3>
             <div className='categories-list'>
-                Videogames, Electronics, Music
+                {
+                    categoriesList.map((category, index) => {
+                        return(
+                            <div key={`category-item-${index}`} className='category-item'>{category}</div>
+                        )
+                    })
+                }
             </div>
         </div>
-        <div className='price'>
+        <div className='price filter-subsection'>
             <h3>Prices</h3>
             <div className='prices-list'>
-                Videogames, Electronics, Music
+
             </div>
         </div>
     </div>

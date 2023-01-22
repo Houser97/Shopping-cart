2802a89b841fd235f66eabbc7f04efa0157cfa77
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-22 10:57:43 -0600

    Set testimonials container and TestimonialCard component

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import HomeLottie from './HomeLottie'
 import '../styles/About.css'
+import TestimonialCard from './TestimonialCard'
 
 const About = () => {
   return (
     <section className='about'>
         <HomeLottie />
         <div className='about-text'>
-            <h2>Sign up for new offers</h2>
-            <div>Get our app to receive %15 discount in your first purchase.</div>
+            <h2>Testimonials</h2>
+            <div className='testimonials-grid'>
+              <TestimonialCard />
+              <TestimonialCard />
+              <TestimonialCard />
+              <TestimonialCard />
+              <TestimonialCard />
+              <TestimonialCard />
+            </div>
         </div>
     </section>
   )

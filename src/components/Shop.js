a309671497f6411bb7c27263fef905e5e11e9cda
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-23 21:01:08 -0600

    Import and set ShopFilter component

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,11 +1,13 @@
 import '../styles/Shop.css';
 import Card from './Card';
 import { productsData } from '../assets/constants';
+import ShopFilter from './ShopFilter';
 
 const Shop = () => {
     return(
     <div className='shop-page'>
         <div className='shop-title'>Shop</div>
+        <ShopFilter />
         <div className="shop-page">
             {           
                 productsData.map(

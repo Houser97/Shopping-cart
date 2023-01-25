5b7bebe310543b5191f84aae66fe6a7ba47cc48b
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-24 20:25:35 -0600

    Set itemsToShow state and useEffect to change its value
    The itemsToShow state will store the array of product objects to be
    mapped. When the filterCategory state changes it will trigger the
    useEffect hook to assign the products belonging to the current category
    to the itemsToShow state.

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -2,13 +2,22 @@ import '../styles/Shop.css';
 import Card from './Card';
 import { productsData } from '../assets/constants';
 import ShopFilter from './ShopFilter';
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 export const FilterShopContext = createContext();
 
 const Shop = () => {
 
     const [filterCategory, setFilterCategory] = useState('all');
+    const [itemsToShow, setItemsToShow] = useState(productsData)
+
+    useEffect(() => {
+        if(filterCategory !== 'all'){
+            const productsDataCopy = structuredClone(productsData)
+            setItemsToShow(productsDataCopy.filter(product => product.categories.includes(filterCategory)))
+        }
+    }, [filterCategory])
+    
 
     const contextProvider = {setFilterCategory}
 
@@ -19,7 +28,7 @@ const Shop = () => {
             <ShopFilter />
             <div className="shop-page">
                 {           
-                    productsData.map(
+                    itemsToShow.map(
                         function iterateImages(product, iterator){
                             return(
                                 <Card key = {iterator} {...product} />

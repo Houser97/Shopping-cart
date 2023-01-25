a0233f4108fe9388c58a53073a1a48f248b5c77c
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-24 20:06:04 -0600

    Set filterCategory state and createContext to change its value
    The FilterShopContext is created to pass the setFilterCategory function
    to the Categories component by means of the value attribute in Provider.
    The Categories component will provide the current filter value according
    to the clicked category by the user.

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -2,22 +2,32 @@ import '../styles/Shop.css';
 import Card from './Card';
 import { productsData } from '../assets/constants';
 import ShopFilter from './ShopFilter';
+import { createContext, useState } from 'react';
+
+export const FilterShopContext = createContext();
 
 const Shop = () => {
+
+    const [filterCategory, setFilterCategory] = useState('all');
+
+    const contextProvider = {setFilterCategory}
+
     return(
     <div className='shop-page'>
-        <div className='shop-title'>Shop</div>
-        <ShopFilter />
-        <div className="shop-page">
-            {           
-                productsData.map(
-                    function iterateImages(product, iterator){
-                        return(
-                            <Card key = {iterator} {...product} />
-                        )
-                    }) 
-            } 
-        </div>  
+        <FilterShopContext.Provider value={contextProvider}>
+            <div className='shop-title'>Shop</div>
+            <ShopFilter />
+            <div className="shop-page">
+                {           
+                    productsData.map(
+                        function iterateImages(product, iterator){
+                            return(
+                                <Card key = {iterator} {...product} />
+                            )
+                        }) 
+                } 
+            </div>  
+        </FilterShopContext.Provider>
     </div>     
     )
         

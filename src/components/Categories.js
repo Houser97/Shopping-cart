441520ef47e78e259d73a5442ca6cbf83f3a29b7
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-24 21:36:09 -0600

    Set categoryPreviousIndex state and setCategoryBackground function
    The categoryPreviousIndex state stores the previous index used to
    add and remove classes from the respective element in the
    categoriesArray ref.
    
    The setCategoryBackground function adds and removes classes from the
    previous and current selected categories to set the background color.
    This function leverages the categoryPreviousIndex to identify what
    operation to perform on each category div.

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,12 +1,24 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import '../styles/Categories.css'
 import { FilterShopContext } from './Shop'
 
 const Categories = () => {
 
     const setFilterCategory = useContext(FilterShopContext).setFilterCategory;
+    const [categoryPreviousIndex, setCategoryPreviousIndex] = useState(null);
+    const categoriesArray = useRef([])
 
     const categoriesList = ['Videogames','Electronics','Music']
+    const setCategoryBackground = (index) => {
+        if(categoryPreviousIndex || categoryPreviousIndex === 0 ){
+            categoriesArray.current[categoryPreviousIndex].classList.remove("selected")
+            categoriesArray.current[categoryPreviousIndex].classList.add("category-item")
+            console.log(categoriesArray.current)
+        }
+        categoriesArray.current[index].classList.add("selected")
+        categoriesArray.current[index].classList.remove("category-item")
+        setCategoryPreviousIndex(index)
+    }
 
   return (
     <div className='categories filter-subsection'>
@@ -18,8 +30,12 @@ const Categories = () => {
                         <div 
                         key={`category-item-${index}`} 
                         className='category-item'
-                        onClick={() => setFilterCategory(category)}>
-                                {category}
+                        ref={((category) => categoriesArray.current[index] = category)}
+                        onClick={() => {
+                            setFilterCategory(category);
+                            setCategoryBackground(index);
+                        }}>
+                            {category}
                         </div>
                     )
                 })

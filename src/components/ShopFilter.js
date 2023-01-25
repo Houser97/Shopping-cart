c816913d73b55e0a71fa3f072ec69355e395cd19
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-24 19:53:23 -0600

    Remove Categories component logic and style

--- src/components/ShopFilter.js
+++ src/components/ShopFilter.js
@@ -1,28 +1,14 @@
 import React from 'react'
 import '../styles/ShopFilter.css'
+import Categories from './Categories'
 
 const ShopFilter = () => {
-
-    const categoriesList = ['Videogames','Electronics','Music']
-
   return (
     <div className='shop-filter'>
-        <div className='categories filter-subsection'>
-            <h3>Categories</h3>
-            <div className='categories-list'>
-                {
-                    categoriesList.map((category, index) => {
-                        return(
-                            <div key={`category-item-${index}`} className='category-item'>{category}</div>
-                        )
-                    })
-                }
-            </div>
-        </div>
+        <Categories />
         <div className='price filter-subsection'>
             <h3>Prices</h3>
             <div className='prices-list'>
-
             </div>
         </div>
     </div>

11c2505f849fd9e2acf11df4189a8440906662c2
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-26 21:13:45 -0600

    Set useRef to target slider and useEffect to add event listeners
    The slider represents the range type input. By targeting it using
    useRef it is possible to add event listeners for change and input to
    handle the value with upcomming functions.

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRef } from 'react'
 import '../styles/Prices.css'
 
 const Prices = () => {
+    
+    const slider = useRef(null);
+
+    useEffect(() => {
+        slider.current.addEventListener('change',(e)=>{
+            console.log(e.target.value);
+        });
+            
+        slider.current.addEventListener('input',(e)=>{
+            console.log('input');
+        });
+
+        return () => {
+            slider.current.removeEventListener('change',(e)=>{
+                console.log('change');
+            });
+                
+            slider.current.removeEventListener('input',(e)=>{
+                console.log('input');
+            });
+        }
+    }, [])
+    
+
   return (
     <div className='price-container filter-subsection'>
         <h3>Prices</h3>
         <div className='prices-range'>
-            <input type="range" min="0" max="2000" id='slider' defaultValue="1000"></input>
-            <div className='selector'></div>
+            <input ref={slider} type="range" min="0" max="2000" id='slider' defaultValue="1000"></input>
         </div>
     </div>
   )

a3d3e398d08fdb01fdeba695b292a089ceadfde1
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-28 12:32:34 -0600

    Remove unnecessary divs and wrap product data in new container

--- src/components/Card.js
+++ src/components/Card.js
@@ -27,26 +27,22 @@ const Card = ({image, name, price, id}) => {
     return(
         <div className="card">
             <div className='price-product'>${price}</div>
-            <div className="card-image-section">
-                <img src = {image} alt = "Apple"></img>
-            </div>
-            <div className="card-name-section">
+            <img src = {image} alt = "Apple"></img>
+            <div className='product-data'>
                 <div className='name-product'>{name}</div>
-            </div>
-            <div className="card-decrase-increase">
-                <button className='decrease-button button-IncDec' onClick={() => dispatch({type: 'decrement'})}>
-                    <svg className='minus-svg svg-IncDec' viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M7,13H17V11H7" />
-                    </svg>
-                </button>
-                <div className='number-products'>{state.numberOfProducts}</div>
-                <button data-testid="0" className='increase-button button-IncDec' onClick={() => dispatch({type: 'increment'})}>
-                    <svg className='plus-svg svg-IncDec' viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M13,7H11V11H7V13H11V17H13V13H17V11H13V7Z" />
-                    </svg>
-                </button>
-            </div>
-            <div className='add-product'>
+                <div className="card-decrase-increase">
+                    <button className='decrease-button button-IncDec' onClick={() => dispatch({type: 'decrement'})}>
+                        <svg className='minus-svg svg-IncDec' viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M7,13H17V11H7" />
+                        </svg>
+                    </button>
+                    <div className='number-products'>{state.numberOfProducts}</div>
+                    <button data-testid="0" className='increase-button button-IncDec' onClick={() => dispatch({type: 'increment'})}>
+                        <svg className='plus-svg svg-IncDec' viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M13,7H11V11H7V13H11V17H13V13H17V11H13V7Z" />
+                        </svg>
+                    </button>
+                </div>
                 <button className='add-to-cart' onClick={() => addProduct(state.numberOfProducts, id)}>Add to cart</button>
             </div>
         </div>

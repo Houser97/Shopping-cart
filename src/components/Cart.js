50d107c8517d31ce3aca74d83546d0620e41f654
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-30 21:10:51 -0600

    Wrap cart section to add black background
    The cart section was comprised of only a single container that appears
    on the right side when the cart is unfolded. However, in order to add
    a black background that covers the whole screen the cart section was
    wrap withing a new container.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -8,26 +8,28 @@ const Cart = ({toggleCart}) => {
     const totalPrice = useContext(CartContext).totalPrice;
     const isEmpty = productsInCar.length > 0;
     return(
-        <div className={`cart ${toggleCart ? 'show-cart':''}`}>
-            <div className='cart-content'>
-            {
-                isEmpty ? (
-                    <div className='cart-content-submission'>
-                        <div className='products-section-cart'>
-                            <CartCard submission={productsInCar} />
+        <div className={`cart-container ${toggleCart ? 'show-cart-container' : ''}`}>
+            <div className={`cart ${toggleCart ? 'show-cart':''}`}>
+                <div className='cart-content'>
+                {
+                    isEmpty ? (
+                        <div className='cart-content-submission'>
+                            <div className='products-section-cart'>
+                                <CartCard submission={productsInCar} />
+                            </div>
+                            <div className='DIV-total-price'>
+                                <div className='total-price'>Total price:</div>
+                                <div data-testid="total-price-id" className='total-price-number'>${totalPrice}</div>
+                            </div>
+                            <div className='pay'>
+                                <button className='pay-button'>Pay</button>
+                            </div>
                         </div>
-                        <div className='DIV-total-price'>
-                            <div className='total-price'>Total price:</div>
-                            <div data-testid="total-price-id" className='total-price-number'>${totalPrice}</div>
-                        </div>
-                        <div className='pay'>
-                            <button className='pay-button'>Pay</button>
-                        </div>
-                    </div>
-                ) 
-                : (
-                <h2 className='no-submission'> Cart empty</h2>)
-            }
+                    ) 
+                    : (
+                    <h2 className='no-submission'> Cart empty</h2>)
+                }
+                </div>
             </div>
         </div>
     )

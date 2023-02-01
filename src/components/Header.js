67e3c3c912eed164633fbdc9e8dfb0a4565f486a
Arturo Rivera <arturo.riverar97@gmail.com>
2023-01-31 20:15:25 -0600

    Restyle Header component using tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import '../styles/Header.css';
 import Cart from './Cart';
 import CartIcon from './CartIcon';
 import Navbar from './Navbar';
@@ -11,10 +10,12 @@ const Header = () => {
     const [toggleCart, setToggleCart] = useState(false);
 
     return(
-        <header>
+        <header className='flex flex-row justify-between items-center 
+        text-5xl fixed w-full z-10 h-[80px] font-bold bg-header px-8 text-white
+        sm:px-10'>
             <ToggleBtn toggle={toggleNavbar} setToggle = {setToggleNavbar}/>
             <Navbar toggle = {toggleNavbar} />
-            <h1 className="title-project">Shopping Cart</h1>
+            <h1 className="text-3xl mx-3 sm:text-5xl">Shopping Cart</h1>
             <CartIcon setToggleCart = {setToggleCart} toggle = {toggleCart}/>
             <Cart toggleCart={toggleCart} />
         </header>

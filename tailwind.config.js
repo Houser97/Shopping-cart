d845da896d93ae9fb79cc1e0a6b06429c2a26d27
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-01 21:16:56 -0600

    Add custom breakpoint
    The defaultTheme object was imported to add the new breakpoint since
    using the extend field would fail. The reason for that was that the
    new breakpoint would be the smallest and tailwind needs values to be
    sorted from the smallest to the largest.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./public/**/*.{html,js}", "./src/**/*.js"],
   theme: {
-    extend: {},
+    screens:{
+      'xs': '400px',
+      ...defaultTheme.screens,
+    }
   },
   plugins: [],
 }

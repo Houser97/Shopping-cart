4e002947bff45bc1fefe4934ab6dc05f5236dcce
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-03 22:11:46 -0600

    Set grid in component and add breakpoints to improve look
    The initial design followed the mobile-first approach. As a result,
    the grid display was used and custom grid-rows/grid-cols were employed
    to keep image ratio aspect. On the other hand, grid items were centered
    and textarea border was changed from black to gray.

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -15,13 +15,13 @@ const ReviewForm = () => {
     
 
   return (
-    <div className='flex justify-center items-center w-full min-h-screen px-10 py-20 bg-[#091F44] mt-10'>
-        <div className='flex p-5 flex-col w-full bg-white rounded-2xl sm:p-7 sm:grid sm:grid-cols-2 md:flex md:flex-row'>
-            <img className='h-auto p-4 max-w-auto sm:h-auto mg:h-96' src={item ? item.image : ''} alt='product'></img>
-            <form className='flex flex-col w-full h-80 text-3xl'>
+    <div className='flex justify-center items-center w-full min-h-screen px-3 py-20 bg-[#091F44] mt-10 xs:px-10'>
+        <div className='grid grid-rows-[min-content,minmax(0,1fr)] grid-cols-1 justify-center items-center gap-5 p-5 w-full bg-white rounded-2xl md:grid-cols-[minmax(0,350px),minmax(300px,1fr)]'>
+            <img className='w-full max-w-[350px] justify-self-center h-auto md:mr-10' src={item ? item.image : ''} alt='product'></img>
+            <form className='flex flex-col w-full h-full text-3xl justify-around'>
                 {item ? item.name : ''}
                 <StarRate product={item ? item.name : ''} />
-                <textarea className='h-full text-xl p-2 border-solid border-black border-2'></textarea>
+                <textarea className='max-h-60 h-60 text-xl p-2 border-solid border-gray-400 border-2 outline-none rounded-md'></textarea>
                 <button>submit</button>
             </form>
         </div>

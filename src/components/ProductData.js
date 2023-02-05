3abd271fb7324cb646212454b9b77da2e011688a
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-05 15:32:43 -0600

    Style ProductData componente with tailwindcss

--- src/components/ProductData.js
+++ src/components/ProductData.js
@@ -17,9 +17,14 @@ const ProductData = () => {
     }, [])
 
   return (
-    <div className='productData-container'>
-        <img src={product ? product.image : ''}></img>
-        <h1>{product ? product.name : ''}</h1>
+    <div className='flex justify-center items-center bg-[var(--blue-color)] w-full py-28 px-2 sm:px-8 md:px-20'>
+      <div className='flex flex-col bg-white rounded-lg p-6 w-full'>
+        <div className='grid auto-rows-auto grid-cols-1 2sm:grid-cols-[minmax(200px,max-content),minmax(300px,1fr)]'>
+          <img className='max-w-[300px] w-full justify-self-center' src={product ? product.image : ''}></img>
+          <h1 className='w-full text-center text-4xl sm:text-5xl'>{product ? product.name : ''}</h1>
+        </div>
+        <div className='w-full font-bold text-4xl text-center'>Reviews</div>
+      </div>
     </div>
   )
 }

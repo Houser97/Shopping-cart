b79665a5242142b8f525d6f153f71dbfe2c28da7
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-05 18:55:56 -0600

    Update StarRate to only show rate or allow to set new rating
    The StarRate component should only display the rating of items in the
    Card component. However, when reviewing an item the StarRate component
    should allow users to set their score in order send information to
    upcoming server. This is done by changing input type to radio when
    updating score and checkbox when displaying rating. On the other hand,
    a className is added to handle animation according to current context.

--- src/components/StarRate.js
+++ src/components/StarRate.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import '../styles/StarRate.css'
 
-const StarRate = ({product}) => {
+const StarRate = ({product, isCustomizable}) => {
+    const attributes = {
+      type: isCustomizable ? 'radio' : 'checkbox',
+      class: isCustomizable ? 'customizable' : '',
+    }
   return (
     <div className='rate-container'>
-        <input type="radio" name={product}></input>
-        <input type="radio" name={product}></input>
-        <input type="radio" name={product}></input>
-        <input type="radio" name={product}></input>
-        <input type="radio" name={product}></input>
+        <input type= {attributes.type} name={product} className={attributes.class}  disabled = {!isCustomizable}></input>
+        <input type= {attributes.type} name={product} className={attributes.class}  disabled = {!isCustomizable} defaultChecked></input>
+        <input type= {attributes.type} name={product} className={attributes.class}  disabled = {!isCustomizable}></input>
+        <input type= {attributes.type} name={product} className={attributes.class}  disabled = {!isCustomizable}></input>
+        <input type= {attributes.type} name={product} className={attributes.class}  disabled = {!isCustomizable}></input>
     </div>
   )
 }

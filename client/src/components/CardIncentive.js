b61bdaae835d10b69a5e8b3cd6bedfc250d49a5e
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-06 10:32:23 -0600

    Style CardIncentive using tailwindcss

--- client/src/components/CardIncentive.js
+++ client/src/components/CardIncentive.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import '../styles/CardIncentive.css'
-
 
 const CardIncentive = ({svg, text1, text2}) => {
   return (
-    <div className='card-incentive'>
-        <div className='card-incentive-text'>{text1}</div>
-        <div className='card-incentive-text2'>{text2}</div>
-        {svg}
+    <div className='flex flex-col justify-center items-center group/card w-full h-full
+    font-bold text-xl p-2 bg-[var(--blue-color)] text-white text-center'>
+        <div className='text-2xl mb-2 md:text-3xl'>{text1}</div>
+        <div className='text-[17px] font-light md:text-[22px]'>{text2}</div>
+        <div className='group-hover/card:animate-[svgAnimation_700ms_ease-in-out_1]'>{svg}</div>
     </div>
   )
 }

2b96912a374af037acb5d1f18fa368010ccf8965
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-07 20:51:01 -0600

    Change item type with number and set likes/dislikes fields
    The likes and dislikes fields are arrays that will store user's id's to
    count number of votes and link users with reviews that have been marked.

--- server/models/review.js
+++ server/models/review.js
@@ -3,9 +3,11 @@ const Schema = mongoose.Schema;
 
 const ReviewSchema = new Schema({
     author: {type: Schema.Types.ObjectId, ref: 'User'},
-    item: {type: String, required: true},
+    item: {type: Number, required: true},
     rating: {type: Number, required: true},
     date: {type: Date, default: Date.now()},
+    likes: [],
+    dislikes: [],
     comment: {type: String, required: true}
 })
 

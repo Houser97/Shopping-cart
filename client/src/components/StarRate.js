bc86fc1cb354979c9bcc0619adcd7ace767a7825
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-08 21:07:08 -0600

    Set rating prop and name attribute in object
    The rating was given the default value of 0 to indicate users that
    a determined item has no reviews in case this prop is not passed.
    On the other hand, this prop will contain the overall reviews from users
    as well as the rating given by users depending on the component the
    StarRate renders.
    
    In addition, the input name attribute is set to 'rating' or the
    product's name. The former will be used when creating reviews so that
    the name remains constant, and the latter will be used when displaying
    the item's review.

--- client/src/components/StarRate.js
+++ client/src/components/StarRate.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import '../styles/StarRate.css'
 
-const StarRate = ({product, isCustomizable}) => {
+const StarRate = ({product, isCustomizable, rating = 0}) => {
     const attributes = {
       type: isCustomizable ? 'radio' : 'checkbox',
       class: isCustomizable ? 'customizable' : '',
+      name: isCustomizable ? 'rating' : product,
     }
   return (
     <div className='rate-container'>
-        <input type= {attributes.type} name={product} className={attributes.class}  disabled = {!isCustomizable}></input>
-        <input type= {attributes.type} name={product} className={attributes.class}  disabled = {!isCustomizable} defaultChecked></input>
-        <input type= {attributes.type} name={product} className={attributes.class}  disabled = {!isCustomizable}></input>
-        <input type= {attributes.type} name={product} className={attributes.class}  disabled = {!isCustomizable}></input>
-        <input type= {attributes.type} name={product} className={attributes.class}  disabled = {!isCustomizable}></input>
+        <input type= {attributes.type} name={attributes.name} className={attributes.class}  disabled = {!isCustomizable} value={5} defaultChecked = {rating === 5}></input>
+        <input type= {attributes.type} name={attributes.name} className={attributes.class}  disabled = {!isCustomizable} value={4} defaultChecked = {rating === 4}></input>
+        <input type= {attributes.type} name={attributes.name} className={attributes.class}  disabled = {!isCustomizable} value={3} defaultChecked = {rating === 3}></input>
+        <input type= {attributes.type} name={attributes.name} className={attributes.class}  disabled = {!isCustomizable} value={2} defaultChecked = {rating === 2}></input>
+        <input type= {attributes.type} name={attributes.name} className={attributes.class}  disabled = {!isCustomizable} value={1} defaultChecked = {rating === 1}></input>
     </div>
   )
 }

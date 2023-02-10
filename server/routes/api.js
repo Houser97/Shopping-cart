577927554739d093d34f2425125508df0531ca9b
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-09 22:01:25 -0600

    Add route to create users

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const apiReviewController = require('../controllers/apiReviewController')
+const apiUserController = require('../controllers/apiUserController')
 
 router.get('/:itemId/get_reviews', apiReviewController.get_reviews);
 
-router.post('/create_review', apiReviewController.create_review);
+router.post('/create_review',apiReviewController.create_review);
+
+router.post('/create_user', apiUserController.check_email ,apiUserController.create_user);
 
 module.exports = router;
 

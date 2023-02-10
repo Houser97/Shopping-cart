57671c25120ba094ab6158fe35454851c2fc68b2
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-09 22:01:55 -0600

    Add username field

--- server/models/user.js
+++ server/models/user.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     email: {type: String, required: true},
     password: {type: String, required: true},
+    username: {type: String, required: true},
     cart: [{}]
 })
 

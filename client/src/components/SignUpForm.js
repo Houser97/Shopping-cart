7401e02ee1b8a7f8f1d51ff9a061fedb672edc14
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-09 21:56:04 -0600

    Add username section, style inputs/labels and fine tune padding

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -4,6 +4,7 @@ const SignUpForm = () => {
 
     const [email, setEmail] = useState(null);
     const [pwd, setPwd] = useState(null);
+    const [username, setUsername] = useState(null);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -12,24 +13,34 @@ const SignUpForm = () => {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({email, pwd})
+            body: JSON.stringify({email, pwd, username})
         }).then(data => data.json()).then(data => console.log(data))
     }
 
   return (
     <div className='flex flex-row justify-center items-center 
     pt-[var(--header-height)] min-h-screen bg-[var(--blue-color)] w-full p-5'>
-        <form className='flex flex-col bg-white rounded-md w-full max-w-lg text-xl p-3 items-center'
+        <form className='flex flex-col bg-white rounded-md w-full max-w-lg text-xl p-7 items-center'
         onSubmit={(e) => handleSubmit(e)}>
             <div className='flex flex-col w-full'>
-                <label htmlFor='email'>E-mail</label>
-                <input type='email' id='email' name='email' onChange={(e) => setEmail(e.target.value)} required></input>
+                <label htmlFor='email' className='font-bold'>E-mail</label>
+                <input type='email' id='email' name='email' 
+                className='border-slate-500 border-2 rounded-md p-1 px-3 
+                outline-[var(--blue-color)]' onChange={(e) => setEmail(e.target.value)} required></input>
             </div>
-            <div className='flex flex-col my-8 w-full'>
-                <label htmlFor='password'>Password</label>
-                <input type='password' id='password' name='pwd' onChange={(e) => setPwd(e.target.value)} required></input>
+            <div className='flex flex-col my-4 w-full'>
+                <label htmlFor='password' className='font-bold'>Password</label>
+                <input type='password' id='password' name='pwd'
+                className='border-slate-500 border-2 rounded-md p-1 px-3 
+                outline-[var(--blue-color)]' onChange={(e) => setPwd(e.target.value)} required></input>
             </div>
-            <button className='bg-[var(--blue-color)] text-white px-3 py-1 rounded-md'>Login</button>
+            <div className='flex flex-col w-full'>
+                <label htmlFor='username' className='font-bold'>Username</label>
+                <input type='text' id='username' name='username' 
+                className='border-slate-500 border-2 rounded-md p-1 px-3 
+                outline-[var(--blue-color)]' onChange={(e) => setUsername(e.target.value)} required></input>
+            </div>
+            <button className='bg-[var(--blue-color)] text-white px-3 py-1 my-4 rounded-md font-bold'>Sign up</button>
         </form>
     </div>
   )

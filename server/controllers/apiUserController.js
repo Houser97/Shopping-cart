566b70dba6645107c5ca1e744195c2822b9517cc
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-09 21:58:47 -0600

    Validate username, add bcryptjs and fine tune check_email controller
    The check_email middleware was no calling the next() middleware
    to ensure the create_user middleware would be called after checking
    email had not been used. Likewise, if the email had already been used
    then a message is sent to the client.

--- server/controllers/apiUserController.js
+++ server/controllers/apiUserController.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const {body, validationResult} = require('express-validator');
+const bcryptjs = require('bcryptjs');
 
 exports.check_email = [
     body('email', 'E-mail must be a valid address.').isEmail()
@@ -7,13 +8,17 @@ exports.check_email = [
     .escape()
     .normalizeEmail(),
 
-    (req, res) => {
+    (req, res, next) => {
         const errors = validationResult(req);
         if(!errors.isEmpty()) return res.json(errors.array());
         else {
             User.findOne({email: {$eq: req.body.email}}).exec((err, user) => {
                 if(err) return res.json(err);
-                return res.json(user.email)
+                if(!user) next()
+                else {
+                    return res.json('Email in use')
+                }
+                
             })
         }
     }
@@ -24,22 +29,30 @@ exports.create_user = [
         .isLength({min:4}).withMessage('Password must contain at least 4 characters.')
         .trim()
         .escape(),
+    body('username', 'Username must not be empty.')
+        .isLength({min:4, max:12}).withMessage('Username must contain at between 4 and 12 characters.')
+        .trim()
+        .escape(),
 
     (req, res) => {
         const errors = validationResult(req);
         if(!errors.isEmpty()) return res.json(errors.array());
-        const user = new User({
-            email: req.body.email,
-            password: req.body.pwd,
-            cart: [],
-        }).save((err, user) => {
+        bcryptjs.hash(req.body.pwd, 6, (err, hashedPwd) => {
             if(err) return res.json(err);
-            req.login(user, (err) => {
+            const user = new User({
+                email: req.body.email,
+                password: hashedPwd,
+                username: req.body.username,
+                cart: [],
+            }).save((err, user) => {
                 if(err) return res.json(err);
-                return res.json({
-                    email: user.email,
-                    cart: user.cart,
-                    id: user._id
+                req.login(user, (err) => {
+                    if(err) return res.json(err);
+                    return res.json({
+                        email: user.email,
+                        cart: user.cart,
+                        id: user._id
+                    })
                 })
             })
         })

99ca9f6f9aed436a7b8d4ba3d9ed0de9284f1966
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-14 22:52:40 -0600

    Set states and context variables to create Reviews
    The states created are 'comment' and 'rating'. The rating updater is
    passed as a prop to the StarRate component. On the other hand, the
    comment updater is used every time the textarea element changes.
    
    Finally, the API and user variables are set from CartContext to define
    the upcoming function to create Reviews.

--- client/src/components/ReviewForm.js
+++ client/src/components/ReviewForm.js
@@ -1,19 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { productsData } from '../assets/constants'
+import { CartContext } from '../App';
 import StarRate from './StarRate'
 
 const ReviewForm = () => {
 
-    const [item, setItem] = useState(null)
+    const [item, setItem] = useState(null);
+    const [comment, setComment] = useState(null);
+    const [rating, setRating] = useState(0);
 
     const {id} = useParams()
+    const API = useContext(CartContext).API;
+    const user = useContext(CartContext).user;
 
     useEffect(() => {
         setItem(productsData.filter(product => product.id === parseInt(id))[0])
     }, [])
     
-
   return (
     <div className='flex justify-center items-center w-full min-h-screen px-3 py-20 bg-[#091F44] mt-10 xs:px-10'>
         <div className='grid grid-rows-[min-content,minmax(0,1fr)] grid-cols-1 justify-center 
@@ -22,10 +26,10 @@ const ReviewForm = () => {
             <form className='flex flex-col w-full h-full text-3xl justify-around'>
                 <h1 className='w-full text-center md:text-5xl'>{item ? item.name : ''}</h1>
                 <div className='flex w-full px-10 justify-center text-3xl my-5 md:text-4xl'>
-                    <StarRate product={item ? item.name : ''} isCustomizable={true} />
+                    <StarRate product={item ? item.name : ''} isCustomizable={true} setRating={setRating} />
                 </div>
-                <textarea className='max-h-60 h-56 text-xl p-2 w-full border-solid border-gray-400 border-2 outline-none rounded-md'></textarea>
-                <input type='hidden' name='itemId' value={id}></input>
+                <textarea className='max-h-60 h-56 text-xl p-2 w-full border-solid border-gray-400 border-2 
+                outline-none rounded-md' minLength='4' required onChange={(e) => setComment(e.target.value)}></textarea>
                 <button className='flex text-2xl bg-[#fcc902] rounded-lg w-[min-content] mt-5
                 px-4 py-2 font-bold self-center transition-transform hover:bg-[#fcd01f] sm:px-5 sm:py-3'>Submit</button>
             </form>

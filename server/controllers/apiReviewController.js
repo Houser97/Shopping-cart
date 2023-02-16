1b40f63460edb08d733e02379960c44a1e9854af
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-15 22:05:34 -0600

    Populate author field and sort by date in reviews

--- server/controllers/apiReviewController.js
+++ server/controllers/apiReviewController.js
@@ -2,7 +2,10 @@ const Review = require('../models/review')
 const {body, validationResult} = require('express-validator');
 
 exports.get_reviews = (req, res, next) => {
-    Review.find({item: {$eq: req.params.itemId}}).exec((err, reviews) => {
+    Review.find({item: {$eq: req.params.itemId}})
+    .sort({date: -1})
+    .populate('author', 'username')
+    .exec((err, reviews) => {
         if(err) return res.json(err);
         return res.json(reviews)
     })

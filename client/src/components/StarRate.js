126825174974952f1cc47e6545eb172eef8091c9
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-16 22:02:05 -0600

    Set two versions of StarRate component
    When the component is customizable then the respective inputs with
    the required attributes will be returned. Otherwise, the static
    component is returned.

--- client/src/components/StarRate.js
+++ client/src/components/StarRate.js
@@ -1,21 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/StarRate.css'
 
 const StarRate = ({product, isCustomizable, rating = 0, setRating}) => {
+
+    const [localRating, setLocalRating] = useState(rating)
+
     const attributes = {
       type: isCustomizable ? 'radio' : 'checkbox',
       class: isCustomizable ? 'customizable' : '',
       name: isCustomizable ? 'rating' : product,
     }
+
+    const updateRating = (e) => {
+      console.log(e.target.value)
+      if(isCustomizable){
+        setLocalRating(e.target.value)
+        setRating(e.target.value)
+      }
+    }
+  if(isCustomizable){
+    return(
+      <div className='rate-container'>
+        <input type='radio' className={attributes.class} value={5} onChange={(e) => setRating(e)}></input>
+        <input type='radio' className={attributes.class} value={4} onChange={(e) => setRating(e)}></input>
+        <input type='radio' className={attributes.class} value={3} onChange={(e) => setRating(e)}></input>
+        <input type='radio' className={attributes.class} value={2} onChange={(e) => setRating(e)}></input>
+        <input type='radio' className={attributes.class} value={1} onChange={(e) => setRating(e)} required></input>
+      </div>
+    )
+  }
+
   return (
     <div className='rate-container'>
-        <input type= {attributes.type} name={attributes.name} className={attributes.class}  disabled = {!isCustomizable} value={5} defaultChecked = {rating === 5} onClick={isCustomizable ? (e) => setRating(e.target.value) : undefined}></input>
-        <input type= {attributes.type} name={attributes.name} className={attributes.class}  disabled = {!isCustomizable} value={4} defaultChecked = {rating === 4} onClick={isCustomizable ? (e) => setRating(e.target.value) : undefined}></input>
-        <input type= {attributes.type} name={attributes.name} className={attributes.class}  disabled = {!isCustomizable} value={3} defaultChecked = {rating === 3} onClick={isCustomizable ? (e) => setRating(e.target.value) : undefined}></input>
-        <input type= {attributes.type} name={attributes.name} className={attributes.class}  disabled = {!isCustomizable} value={2} defaultChecked = {rating === 2} onClick={isCustomizable ? (e) => setRating(e.target.value) : undefined}></input>
-        <input type= {attributes.type} name={attributes.name} className={attributes.class}  disabled = {!isCustomizable} value={1} defaultChecked = {rating === 1} onClick={isCustomizable ? (e) => setRating(e.target.value) : undefined} required></input>
+        <input type='checkbox' className={attributes.class}  disabled = {!isCustomizable} value={5} checked = {rating === 5} onChange={(e) => updateRating(e)}></input>
+        <input type='checkbox' className={attributes.class}  disabled = {!isCustomizable} value={4} checked = {rating === 4} onChange={(e) => updateRating(e)}></input>
+        <input type='checkbox' className={attributes.class}  disabled = {!isCustomizable} value={3} checked = {rating === 3} onChange={(e) => updateRating(e)}></input>
+        <input type='checkbox' className={attributes.class}  disabled = {!isCustomizable} value={2} checked = {rating === 2} onChange={(e) => updateRating(e)}></input>
+        <input type='checkbox' className={attributes.class}  disabled = {!isCustomizable} value={1} checked = {rating === 1} onChange={(e) => updateRating(e)} required></input>
     </div>
   )
 }
-
 export default StarRate

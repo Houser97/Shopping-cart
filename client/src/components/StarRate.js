dee5058f1dcc885e978179aae5a22582e7bf4ab5
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-17 21:48:59 -0600

    Tune attributes according to isCustomizable state
    Aside from tuning attributes, a single return is used instead of 2.
    
    When the component is customizable then in the checked attribute the
    localRating state must be used since the rating prop is undefined. This
    will allow to check radio buttons when reviewing products. Moreover,
    the 'radio' type is used instead of also using the checkbox type.
    
    On the other hand, when udpdating the rating prop and localRating state
    the value must be converted into an int, otherwise the value is a
    string, which will lead to problems since this value is compared to
    integers using the strict equality operator (===) preventing from
    checking radio buttons.

--- client/src/components/StarRate.js
+++ client/src/components/StarRate.js
@@ -6,37 +6,24 @@ const StarRate = ({product, isCustomizable, rating = 0, setRating}) => {
     const [localRating, setLocalRating] = useState(rating)
 
     const attributes = {
-      type: isCustomizable ? 'radio' : 'checkbox',
       class: isCustomizable ? 'customizable' : '',
       name: isCustomizable ? 'rating' : product,
     }
 
     const updateRating = (e) => {
-      console.log(e.target.value)
       if(isCustomizable){
-        setLocalRating(e.target.value)
-        setRating(e.target.value)
+        setLocalRating(parseInt(e.target.value))
+        setRating(parseInt(e.target.value))
       }
     }
-  if(isCustomizable){
-    return(
-      <div className='rate-container'>
-        <input type='radio' className={attributes.class} value={5} onChange={(e) => setRating(e)}></input>
-        <input type='radio' className={attributes.class} value={4} onChange={(e) => setRating(e)}></input>
-        <input type='radio' className={attributes.class} value={3} onChange={(e) => setRating(e)}></input>
-        <input type='radio' className={attributes.class} value={2} onChange={(e) => setRating(e)}></input>
-        <input type='radio' className={attributes.class} value={1} onChange={(e) => setRating(e)} required></input>
-      </div>
-    )
-  }
 
   return (
     <div className='rate-container'>
-        <input type='checkbox' className={attributes.class}  disabled = {!isCustomizable} value={5} checked = {rating === 5} onChange={(e) => updateRating(e)}></input>
-        <input type='checkbox' className={attributes.class}  disabled = {!isCustomizable} value={4} checked = {rating === 4} onChange={(e) => updateRating(e)}></input>
-        <input type='checkbox' className={attributes.class}  disabled = {!isCustomizable} value={3} checked = {rating === 3} onChange={(e) => updateRating(e)}></input>
-        <input type='checkbox' className={attributes.class}  disabled = {!isCustomizable} value={2} checked = {rating === 2} onChange={(e) => updateRating(e)}></input>
-        <input type='checkbox' className={attributes.class}  disabled = {!isCustomizable} value={1} checked = {rating === 1} onChange={(e) => updateRating(e)} required></input>
+        <input type='radio' className={attributes.class} name={attributes.name}  disabled = {!isCustomizable} value={5} checked = {isCustomizable ? localRating === 5 : rating === 5} onChange={(e) => updateRating(e)}></input>
+        <input type='radio' className={attributes.class} name={attributes.name}  disabled = {!isCustomizable} value={4} checked = {isCustomizable ? localRating === 4 : rating === 4} onChange={(e) => updateRating(e)}></input>
+        <input type='radio' className={attributes.class} name={attributes.name}  disabled = {!isCustomizable} value={3} checked = {isCustomizable ? localRating === 3 : rating === 3} onChange={(e) => updateRating(e)}></input>
+        <input type='radio' className={attributes.class} name={attributes.name}  disabled = {!isCustomizable} value={2} checked = {isCustomizable ? localRating === 2 : rating === 2} onChange={(e) => updateRating(e)}></input>
+        <input type='radio' className={attributes.class} name={attributes.name}  disabled = {!isCustomizable} value={1} checked = {isCustomizable ? localRating === 1 : rating === 1} onChange={(e) => updateRating(e)} required></input>
     </div>
   )
 }

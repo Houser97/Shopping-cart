8716a87d9bded70b940cf6737d9eff141bc59547
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-19 20:22:16 -0600

    Set logic to update likes in ReviewCard
    The way it works is by setting the localLikes state in order to store
    the likes prop. This way, the state will be used to render dinamically
    the likes count according to the click and whether the user has already
    liked that review.
    
    Likewise, a copy of the localLikes state is created within the
    updateReviewLikes function to handle the changes and send the current
    value, as well as the current data of the review, to the respective API
    endpoint so the database is also updated.

--- client/src/components/ReviewCard.js
+++ client/src/components/ReviewCard.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { CartContext } from '../App'
 import StarRate from './StarRate'
 
-const ReviewCard = ({likes, dislikes, comment, author, rating, date, _id, productId}) => {
+const ReviewCard = ({likes, dislikes, comment, author, rating, date, _id, productId, setReviews}) => {
+
+    const API = useContext(CartContext).API
+    const user = useContext(CartContext).user
+    const [localLikes, setLocalLikes] = useState(likes)
+
+    const updateReviewLikes = () => {
+        const likesCopy = [...localLikes]
+        if(localLikes.includes(user.id)){
+            const indexUser = localLikes.indexOf(user.id)
+            likesCopy.splice(indexUser,1)
+        } else {
+            likesCopy.push(user.id)
+        }
+        setLocalLikes(likesCopy)
+        fetch(`${API}/update_review`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: _id,
+                author: user.id,
+                item: parseInt(productId),
+                rating: parseFloat(rating),
+                date: new Date(date),
+                comment,
+                dislikes,
+                likes: likesCopy
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            setReviews(oldArray => {
+                const index = oldArray.findIndex(review => review._id === _id)
+                oldArray[index] = data
+                return oldArray
+            })
+        })
+    }
   return (
     <div className='flex flex-col w-full justify-evenly bg-white mb-3 rounded-lg p-3 sm:px-9'>
         <div className='flex flex-row flex-wrap w-full justify-between'>
@@ -12,9 +52,9 @@ const ReviewCard = ({likes, dislikes, comment, author, rating, date, _id, produc
         </div>
         <div className='w-full my-4 text-base text-justify sm:text-xl'>{comment}</div>
         <div className='flex flex-row w-full my-3'>
-            <div className='flex flex-row items-center mr-3'>
+            <div className='flex flex-row items-center mr-3' onClick={() => updateReviewLikes()}>
                 <svg className='w-5 h-5 cursor-pointer mr-2 fill-blue-600' viewBox="0 0 24 24"><path d="M23,10C23,8.89 22.1,8 21,8H14.68L15.64,3.43C15.66,3.33 15.67,3.22 15.67,3.11C15.67,2.7 15.5,2.32 15.23,2.05L14.17,1L7.59,7.58C7.22,7.95 7,8.45 7,9V19A2,2 0 0,0 9,21H18C18.83,21 19.54,20.5 19.84,19.78L22.86,12.73C22.95,12.5 23,12.26 23,12V10M1,21H5V9H1V21Z" /></svg>
-                <div className='text-lg'>{likes.length}</div>
+                <div className='text-lg'>{localLikes.length}</div>
             </div>
             <div className='flex flex-row items-center'>
                 <svg className='w-5 h-5 cursor-pointer mr-2 fill-orange-600' viewBox="0 0 24 24"><path d="M19,15H23V3H19M15,3H6C5.17,3 4.46,3.5 4.16,4.22L1.14,11.27C1.05,11.5 1,11.74 1,12V14A2,2 0 0,0 3,16H9.31L8.36,20.57C8.34,20.67 8.33,20.77 8.33,20.88C8.33,21.3 8.5,21.67 8.77,21.94L9.83,23L16.41,16.41C16.78,16.05 17,15.55 17,15V5C17,3.89 16.1,3 15,3Z" /></svg>

d8b64ac7c3def6c78fb9ef4712695d388ee85158
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-21 21:38:26 -0600

    Set data attribute to svg's container and add pointer-events-none
    The data attribute will be used to distinguish the like from the dislike
    svg to handle the respective operation. Thus, the pointer-events-none is
    added to each svg in order to prevent these elements from triggering
    the event, which would prevent the logic from retrieving the data
    attribute set in the respective parent.
    
    On the other hand, the pointer-events-none would disable the
    cursor-pointer style in each svg, so it was removed from the svg's and
    set in the parent.

--- client/src/components/ReviewCard.js
+++ client/src/components/ReviewCard.js
@@ -9,7 +9,8 @@ const ReviewCard = ({likes, dislikes, comment, author, rating, date, _id, produc
     const [localLikes, setLocalLikes] = useState(likes)
     const [localDislikes, setLocalDislikes] = useState(dislikes);
 
-    const updateReviewLikes = () => {
+    const updateReviewLikes = (e) => {
+        const isLike = e.target.getAttribute('data') === 'like'
         if(user){
             const likesCopy = [...localLikes]
             if(localLikes.includes(user.id)){
@@ -55,14 +56,14 @@ const ReviewCard = ({likes, dislikes, comment, author, rating, date, _id, produc
         </div>
         <div className='w-full my-4 text-base text-justify sm:text-xl'>{comment}</div>
         <div className='flex flex-row w-full my-3'>
-            <div className='flex flex-row items-center mr-3' onClick={() => updateReviewLikes()}>
-                <svg className={`w-5 h-5 mr-2 
-                ${user ? `cursor-pointer ${localLikes.includes(user.id) ? 'fill-blue-600':''}`:''}`} viewBox="0 0 24 24"><path d="M23,10C23,8.89 22.1,8 21,8H14.68L15.64,3.43C15.66,3.33 15.67,3.22 15.67,3.11C15.67,2.7 15.5,2.32 15.23,2.05L14.17,1L7.59,7.58C7.22,7.95 7,8.45 7,9V19A2,2 0 0,0 9,21H18C18.83,21 19.54,20.5 19.84,19.78L22.86,12.73C22.95,12.5 23,12.26 23,12V10M1,21H5V9H1V21Z" /></svg>
+            <div data = 'like' className={`flex flex-row items-center mr-3 ${user ? `cursor-pointer`:''}`} onClick={(e) => updateReviewLikes(e)}>
+                <svg className={`w-5 h-5 mr-2 pointer-events-none
+                ${user ? `${localLikes.includes(user.id) ? 'fill-blue-600':''}`:''}`} viewBox="0 0 24 24"><path d="M23,10C23,8.89 22.1,8 21,8H14.68L15.64,3.43C15.66,3.33 15.67,3.22 15.67,3.11C15.67,2.7 15.5,2.32 15.23,2.05L14.17,1L7.59,7.58C7.22,7.95 7,8.45 7,9V19A2,2 0 0,0 9,21H18C18.83,21 19.54,20.5 19.84,19.78L22.86,12.73C22.95,12.5 23,12.26 23,12V10M1,21H5V9H1V21Z" /></svg>
                 <div className='text-lg'>{localLikes.length}</div>
             </div>
-            <div className='flex flex-row items-center'>
-                <svg className={`w-5 h-5 mr-2  
-                ${user ? `cursor-pointer ${localDislikes.includes(user.id) ? 'fill-blue-600':''}`:''}`} viewBox="0 0 24 24"><path d="M19,15H23V3H19M15,3H6C5.17,3 4.46,3.5 4.16,4.22L1.14,11.27C1.05,11.5 1,11.74 1,12V14A2,2 0 0,0 3,16H9.31L8.36,20.57C8.34,20.67 8.33,20.77 8.33,20.88C8.33,21.3 8.5,21.67 8.77,21.94L9.83,23L16.41,16.41C16.78,16.05 17,15.55 17,15V5C17,3.89 16.1,3 15,3Z" /></svg>
+            <div data = 'dislike' className='flex flex-row items-center'>
+                <svg className={`w-5 h-5 mr-2 pointer-events-none
+                ${user ? `${localDislikes.includes(user.id) ? 'fill-blue-600':''}`:''}`} viewBox="0 0 24 24"><path d="M19,15H23V3H19M15,3H6C5.17,3 4.46,3.5 4.16,4.22L1.14,11.27C1.05,11.5 1,11.74 1,12V14A2,2 0 0,0 3,16H9.31L8.36,20.57C8.34,20.67 8.33,20.77 8.33,20.88C8.33,21.3 8.5,21.67 8.77,21.94L9.83,23L16.41,16.41C16.78,16.05 17,15.55 17,15V5C17,3.89 16.1,3 15,3Z" /></svg>
                 <div className='text-lg'>{dislikes.length}</div>
             </div>
         </div>

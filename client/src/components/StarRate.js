e3df76f882760a720a3873045d925ad1c6bdf1f5
Arturo Rivera <arturo.riverar97@gmail.com>
2023-02-26 17:45:03 -0600

    Set type attribute in StarRate inputs
    The type attribute switches between radio and checkbox. The radio type
    is used only when users are writing a review; conversely, the checkbox
    type is used to display item's rating without allowing users to alter
    it.
    
    The reason behind this was that the radio type was not rendering
    some inputs to display item's rating, but the checkbox type proved to be
    better to keem consistency in the rating of each product when refreshing
    the page.

--- client/src/components/StarRate.js
+++ client/src/components/StarRate.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import '../styles/StarRate.css'
 
-const StarRate = ({product, isCustomizable, rating = 0, setRating}) => {
+const StarRate = ({product, isCustomizable, rating, setRating}) => {
 
     const [localRating, setLocalRating] = useState(rating)
 
     const attributes = {
+      type: isCustomizable ? 'radio':'checkbox',
       class: isCustomizable ? 'customizable' : '',
       name: isCustomizable ? 'rating' : product,
     }
@@ -19,11 +20,11 @@ const StarRate = ({product, isCustomizable, rating = 0, setRating}) => {
 
   return (
     <div className='rate-container'>
-        <input type='radio' className={attributes.class} name={attributes.name}  disabled = {!isCustomizable} value={5} checked = {isCustomizable ? localRating === 5 : rating === 5} onChange={(e) => updateRating(e)}></input>
-        <input type='radio' className={attributes.class} name={attributes.name}  disabled = {!isCustomizable} value={4} checked = {isCustomizable ? localRating === 4 : rating === 4} onChange={(e) => updateRating(e)}></input>
-        <input type='radio' className={attributes.class} name={attributes.name}  disabled = {!isCustomizable} value={3} checked = {isCustomizable ? localRating === 3 : rating === 3} onChange={(e) => updateRating(e)}></input>
-        <input type='radio' className={attributes.class} name={attributes.name}  disabled = {!isCustomizable} value={2} checked = {isCustomizable ? localRating === 2 : rating === 2} onChange={(e) => updateRating(e)}></input>
-        <input type='radio' className={attributes.class} name={attributes.name}  disabled = {!isCustomizable} value={1} checked = {isCustomizable ? localRating === 1 : rating === 1} onChange={(e) => updateRating(e)} required></input>
+        <input type={attributes.type} className={attributes.class} name={attributes.name}  disabled = {!isCustomizable} value={5} checked = {isCustomizable ? localRating === 5 : rating === 5} onChange={(e) => updateRating(e)}></input>
+        <input type={attributes.type} className={attributes.class} name={attributes.name}  disabled = {!isCustomizable} value={4} checked = {isCustomizable ? localRating === 4 : rating === 4} onChange={(e) => updateRating(e)}></input>
+        <input type={attributes.type} className={attributes.class} name={attributes.name}  disabled = {!isCustomizable} value={3} checked = {isCustomizable ? localRating === 3 : rating === 3} onChange={(e) => updateRating(e)}></input>
+        <input type={attributes.type} className={attributes.class} name={attributes.name}  disabled = {!isCustomizable} value={2} checked = {isCustomizable ? localRating === 2 : rating === 2} onChange={(e) => updateRating(e)}></input>
+        <input type={attributes.type} className={attributes.class} name={attributes.name}  disabled = {!isCustomizable} value={1} checked = {isCustomizable ? localRating === 1 : rating === 1} onChange={(e) => updateRating(e)} required></input>
     </div>
   )
 }

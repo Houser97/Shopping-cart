10296988db50f982ee61c2d59b58e28422efcd96
Arturo Rivera <arturo.riverar97@gmail.com>
2023-03-08 21:00:02 -0600

    Update user's cart using handleProductAddition function
    This function calls the respective API to update the current user's
    cart. Moreover, the function updates the user from the client by
    checking whether the product added is already in the cart or is a new
    addition. Finally, the outcome is sent to the backend to update
    the user data.

--- client/src/components/ProductBtns.js
+++ client/src/components/ProductBtns.js
@@ -3,13 +3,40 @@ import { Link } from 'react-router-dom'
 import { CartContext } from '../App'
 
 const ProductBtns = ({productId, reduceState = {numberOfProducts: 1}}) => {
+  /*reduceState se inicializa con 1 para el caso en que se agregue producto
+  desde ProductData, en donde no se puede indicar el número de productos a agregar. */
     const addProduct = useContext(CartContext).addProduct
     const user = useContext(CartContext).user;
+    const setUser = useContext(CartContext).setUser;
+    const API = useContext(CartContext).API;
+    
+    const handleProductAddition = () => {
+      addProduct(reduceState.numberOfProducts, productId)
+      const cartUser = structuredClone(user.cart)
+      const itemIndex = cartUser.findIndex(product => product.id === productId)
+      if(itemIndex >= 0){
+        cartUser[itemIndex] = {id: productId, quantity: cartUser[itemIndex].quantity + reduceState.numberOfProducts}
+      } else {
+        cartUser.push({id: productId, quantity: reduceState.numberOfProducts})
+      }
+      fetch(`${API}/update_user_cart`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({cartUser})
+      }).then(data=> data.json()).then(data => {
+        const userCopy = structuredClone(user)
+        userCopy.cart = cartUser
+        setUser(userCopy)
+      })
+    }
   return (
     <div>
       {user ? 
           <div className='flex flex-row flex-wrap items-center justify-center gap-2'>
-            <button className='bg-[#ffa41c] font-bold p-2 w-24 rounded-lg' onClick={() => addProduct(reduceState.numberOfProducts, productId)}>Add to cart</button>
+            <button className='bg-[#ffa41c] font-bold p-2 w-24 rounded-lg' onClick={() => handleProductAddition()}>Add to cart</button>
             <Link className='bg-[var(--blue-color)] font-bold rounded-lg text-white w-24 p-2 text-center' to={`/${productId}/review`}>Review</Link>
           </div>
           :

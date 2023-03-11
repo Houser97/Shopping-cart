9f2fdb8a3b07268a668f2ce6ae28147b3ea5d55c
Arturo Rivera <arturo.riverar97@gmail.com>
2023-03-10 22:03:12 -0600

    Set toggleNavbar function to close navbar
    The toggleNavbar function checks that the setToggleNavbar prop is not
    undefined prior to using it when clicking any button. The reason of the
    toggleNavbar function is that these buttons are also placed in the
    Header for large devices; thus, when located these buttons there then
    they should not command the navbar state.

--- client/src/components/AuthButtons.js
+++ client/src/components/AuthButtons.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const AuthButtons = () => {
+const AuthButtons = ({setToggleNavbar}) => {
+  const toggleNavbar = () => {
+    if(setToggleNavbar){
+      setToggleNavbar(false)
+    }
+  }
   return (
     <div className='flex flex-row flex-wrap text-base gap-5 items-center justify-center'>
-        <Link to='/sign-up' className='bg-white text-black px-3 py-1 rounded-lg'>Sign up</Link>
-        <Link to='/login' className='bg-[var(--yellow-color)] text-black px-3 py-1 rounded-lg'>Sign in</Link>
+        <Link to='/sign-up' className='bg-white text-black px-3 py-1 rounded-lg' onClick={() => toggleNavbar()}>Sign up</Link>
+        <Link to='/login' className='bg-[var(--yellow-color)] text-black px-3 py-1 rounded-lg' onClick={() => toggleNavbar()}>Sign in</Link>
     </div>
   )
 }

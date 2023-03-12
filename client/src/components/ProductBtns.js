3bdc1eec409dc9b20fb89f8cac9a3e174106d9b8
Arturo Rivera <arturo.riverar97@gmail.com>
2023-03-12 17:04:31 -0600

    Set the edit parameter to 0 when calling ReviewForm
    Since the Review button is used to allow users to create a review, the
    edit parameter is set to 0 so the ReviewForm component does not allow
    editing.

--- client/src/components/ProductBtns.js
+++ client/src/components/ProductBtns.js
@@ -16,7 +16,7 @@ const ProductBtns = ({productId, reduceState = {numberOfProducts: 1}}) => {
       {user ? 
           <div className='flex flex-row flex-wrap items-center justify-center gap-2'>
             <button className='bg-[#ffa41c] font-bold p-2 w-24 rounded-lg' onClick={() => handleProductAddition()}>Add to cart</button>
-            <Link className='bg-[var(--blue-color)] font-bold rounded-lg text-white w-24 p-2 text-center' to={`/${productId}/review`}>Review</Link>
+            <Link className='bg-[var(--blue-color)] font-bold rounded-lg text-white w-24 p-2 text-center' to={`/${productId}/0/review`}>Review</Link>
           </div>
           :
           <div className='text-lg font-bold text-center w-full 2sm:text-2xl'>

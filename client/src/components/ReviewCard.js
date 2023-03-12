2206b4a9068357d2e71e99566f31e1e1dfed9381
Arturo Rivera <arturo.riverar97@gmail.com>
2023-03-12 17:02:36 -0600

    Set button open ReviewForm and edit review
    The button is created using the LINK component from React-Router. It
    is visible only when a user is logged in and the author id matches the
    current user id.

--- client/src/components/ReviewCard.js
+++ client/src/components/ReviewCard.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from 'react'
+import { Link } from 'react-router-dom'
 import { CartContext } from '../App'
 import StarRate from './StarRate'
 
@@ -71,7 +72,7 @@ const ReviewCard = ({likes, dislikes, comment, author, rating, date ,formatted_d
             </div>
         </div>
         <div className='w-full my-4 text-base text-justify sm:text-xl'>{comment}</div>
-        <div className='flex flex-row w-full my-3'>
+        <div className='flex flex-row w-full my-3 mb-5 relative'>
             <div data = 'like' className={`flex flex-row items-center mr-3 ${user ? `cursor-pointer`:''}`} onClick={(e) => updateReviewLikes(e)}>
                 <svg className={`w-5 h-5 mr-2 pointer-events-none
                 ${user ? `${localLikes.has(user.id) ? 'fill-blue-600':''}`:''}`} viewBox="0 0 24 24"><path d="M23,10C23,8.89 22.1,8 21,8H14.68L15.64,3.43C15.66,3.33 15.67,3.22 15.67,3.11C15.67,2.7 15.5,2.32 15.23,2.05L14.17,1L7.59,7.58C7.22,7.95 7,8.45 7,9V19A2,2 0 0,0 9,21H18C18.83,21 19.54,20.5 19.84,19.78L22.86,12.73C22.95,12.5 23,12.26 23,12V10M1,21H5V9H1V21Z" /></svg>
@@ -82,6 +83,9 @@ const ReviewCard = ({likes, dislikes, comment, author, rating, date ,formatted_d
                 ${user ? `${localDislikes.has(user.id) ? 'fill-orange-600':''}`:''}`} viewBox="0 0 24 24"><path d="M19,15H23V3H19M15,3H6C5.17,3 4.46,3.5 4.16,4.22L1.14,11.27C1.05,11.5 1,11.74 1,12V14A2,2 0 0,0 3,16H9.31L8.36,20.57C8.34,20.67 8.33,20.77 8.33,20.88C8.33,21.3 8.5,21.67 8.77,21.94L9.83,23L16.41,16.41C16.78,16.05 17,15.55 17,15V5C17,3.89 16.1,3 15,3Z" /></svg>
                 <div className='text-lg'>{localDislikes.size}</div>
             </div>
+            <Link className={`rounded-lg font-bold text-lg bg-blue-600 text-white px-5 
+            py-1 absolute right-0 top-0 tracking-wider cursor-pointer ${user ? author._id === user.id ? 'flex':'hidden':'hidden'}`
+            } to={`/${productId}/1/review`}>Edit</Link>
         </div>
         <div className='w-full text-end text-xl font-bold'>{formatted_date}</div>
     </div>

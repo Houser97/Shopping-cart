3078a36edc01160ed1b6786c4f19eb795cf99104
Arturo Rivera <arturo.riverar97@gmail.com>
2023-03-12 16:54:11 -0600

    Add logic to edit reviews in ReviewForm component
    With the help of the 'edit' parameter it is defined when the component
    should allow editing or creating a review. For this, the updateReview
    function is defined, which is in charge of calling the API endpoint to
    modify the rating and the user's comment. Also, the function
    handleReviewSubmit is defined to call either the createReview or
    updateReview function depending on the context when the component form
    is submitted.

--- client/src/components/ReviewForm.js
+++ client/src/components/ReviewForm.js
@@ -14,28 +14,31 @@ const ReviewForm = () => {
     const [localReviews, setLocalReviews] = useState([])
     const [alreadyReviewed, setAlreadyReviewed] = useState(false)
 
-    const {id} = useParams()
+    const {id, edit} = useParams()
     const API = useContext(CartContext).API;
     const user = useContext(CartContext).user;
     const reviews = useContext(CartContext).globalReviews;
     /*Permite llamar a la base de datos para actualizar reviews después de publicar una. */
     const setUpdateReviews = useContext(CartContext).setUpdateReviews;
+    /*Estado que guardará los datos de la review del usuario para poder hacer edit*/
+    const [userReview, setUserReview] = useState(null)
 
     useEffect(() => {
         setItem(productsData.filter(product => product.id === parseInt(id))[0])
         setLocalReviews(reviews.filter(review => review.item === parseInt(id)))
     }, [])
 
+    /*useEffect que asigna si ya se hizo review o recupera datos de review para poder hacer edit */
     useEffect(() => {
-        localReviews.forEach((review) => {
-            if(review.author._id === user.id){
-                setAlreadyReviewed(true)
-            }
-        })
+        const review = localReviews.filter((review) => review.author._id === user.id)
+        if(review.length && parseInt(edit)){
+            setUserReview(review[0])
+        } else if (review.length){
+            setAlreadyReviewed(true)
+        }
     }, [localReviews])  
 
-    const createReview = (e) => {
-        e.preventDefault()
+    const createReview = () => {
         fetch(`${API}/create_review`, {
             method: 'POST',
             headers: {
@@ -51,6 +54,42 @@ const ReviewForm = () => {
             }
         })
     }
+
+    const updateReview = () => {
+        const {date, dislikes, item, _id, likes} = userReview
+        fetch(`${API}/update_review`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: _id,
+                author: user.id,
+                item: parseInt(item),
+                rating: parseFloat(rating),
+                date: new Date(date),
+                comment,
+                dislikes,
+                likes
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            setUpdateReviews(prev => !prev)
+            if(data){
+                navigate('/')
+            }
+        })
+    }
+
+    const handleReviewSubmit = (e) => {
+        e.preventDefault()
+        if(userReview && parseInt(edit)){
+            updateReview()
+        } else {
+            createReview()
+        }
+    }
     
   return (
     <div className='flex justify-center items-center w-full min-h-screen px-3 py-20 bg-[#091F44] mt-10 xs:px-10'>
@@ -58,13 +97,14 @@ const ReviewForm = () => {
         items-center gap-5 p-5 px-6 w-full bg-white rounded-2xl md:grid-cols-[minmax(0,350px),minmax(350px,1fr)] md:pl-20 md:p-10'>
             <img className='w-full max-w-[350px] justify-self-center h-auto md:mr-10' src={item ? item.image : ''} alt='product'></img>
             { !alreadyReviewed ?
-            <form className='flex flex-col w-full h-full text-3xl justify-around' onSubmit={(e) => createReview(e)}>
+            <form className='flex flex-col w-full h-full text-3xl justify-around' onSubmit={(e) => handleReviewSubmit(e)}>
                 <h1 className='w-full text-center md:text-5xl'>{item ? item.name : ''}</h1>
                 <div className='flex w-full px-10 justify-center text-3xl my-5 md:text-4xl'>
-                    <StarRate product={item ? item.name : ''} isCustomizable={true} setRating={setRating} />
+                    <StarRate key={`update-Review-${userReview ? userReview._id : id}`} product={item ? item.name : ''} isCustomizable={true} setRating={setRating} rating = {userReview ? userReview.rating:0} />
                 </div>
                 <textarea className='max-h-60 h-56 text-xl p-2 w-full border-solid border-gray-400 border-2 
-                outline-none rounded-md' minLength='4' required onChange={(e) => setComment(e.target.value)}></textarea>
+                outline-none rounded-md' minLength='4' required onChange={(e) => setComment(e.target.value)}
+                defaultValue = {userReview ? userReview.comment:''}></textarea>
                 <button className='flex text-2xl bg-[#fcc902] rounded-lg w-[min-content] mt-5
                 px-4 py-2 font-bold self-center transition-transform hover:bg-[#fcd01f] sm:px-5 sm:py-3'>Submit</button>
             </form>

c4cd0ada55cc3fd58748e3efc7b6821ab04b9d9e
Arturo Rivera <arturo.riverar97@gmail.com>
2023-03-14 08:06:28 -0600

    Change TestimonialCard layout and style

--- client/src/components/TestimonialCard.js
+++ client/src/components/TestimonialCard.js
@@ -1,25 +1,21 @@
 import React from 'react'
 import '../styles/TestimonialCard.css'
 
-const TestimonialCard = () => {
+const TestimonialCard = ({userPicture, country, username, mainSentence, testimonial}) => {
   return (
-    <div className='testimonial-card'>
-
-        <div className='testimonial-border'></div>
-
-        <svg className='testimonial-quote quote-up' viewBox="0 0 24 24">
-            <path fill="currentColor" d="M10,7L8,11H11V17H5V11L7,7H10M18,7L16,11H19V17H13V11L15,7H18Z" />
-        </svg>
-
-        <svg className='testimonial-quote quote-down' viewBox="0 0 24 24">
-            <path fill="currentColor" d="M10,7L8,11H11V17H5V11L7,7H10M18,7L16,11H19V17H13V11L15,7H18Z" />
-        </svg>
-
-
-        <h3>Lorem</h3>
-        <div className='testimonial-text'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lacinia nunc eget felis volutpat pretium. 
+    <div className='flex flex-col items-center justify-between relative px-6 pb-5 bg-white text-black rounded-xl max-w-2xl'>
+      <div className='flex flex-row w-full justify-start relative text-lg font-bold'>
+        <div className='flex text-center items-center justify-center rounded-full relative -top-10 -left-3 w-20 h-20 border-[var(--yellow-color)] border-solid border-4 bg-white'>Image</div>
+        <div className='flex flex-col items-start justify-center ml-2'>
+          <div>Country</div>
+          <div>Username</div>
         </div>
+      </div>
+      <div className='font-bold text-xl text-center'>flex flex-col items-center justify-between relative bg-green-400 w-96 h-40 mt-40
+      flex flex-col items-center justify-between relative bg-green-400 w-96 h-40 mt-40</div>
+      <div className='text-justify text-lg'>Testimonial flex flex-col items-center justify-between relative bg-green-400 w-96 h-40 mt-40
+      flex flex-col items-center justify-between relative bg-green-400 w-96 h-40 mt-40 flex flex-col items-center justify-between relative bg-green-400 w-96 h-40 mt-40
+      </div>
     </div>
   )
 }

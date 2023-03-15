612d7d05ba14f0b0a29713cd09d0eb3982f918d3
Arturo Rivera <arturo.riverar97@gmail.com>
2023-03-15 08:15:46 -0600

    Set single row for large devices to present HommieLotie and Testimonials

--- client/src/components/Testimonials.js
+++ client/src/components/Testimonials.js
@@ -8,11 +8,11 @@ const Testimonials = () => {
     grid-rows-[minmax(200px,1fr)_minmax(200px,400px)] gap-3 py-7 px-6
     bg-[var(--blue-color)] text-[var(--yellow-color)] w-full lg:px-14
     lg:grid-cols-[minmax(200px,400px)_minmax(200px,1fr)]
-    lg:grid-rows-[repeat(auto-fit,minmax(200px,500px))]'>
+    lg:grid-rows-1'>
         <HomeLottie />
         <div className='self-center h-full row-start-1 lg:row-auto'>
             <h2 className='mb-7 text-center'>Testimonials</h2>
-            <div className='flex flex-row justify-center items-center w-full py-12 px-5'>
+            <div className='flex flex-row justify-center items-center w-full py-12 px-5 bg-green-400'>
               <TestimonialCard />
             </div>
         </div>

90d2b64ef8736d6b415d028946dccefc883aa4c5
Arturo Rivera <arturo.riverar97@gmail.com>
2023-03-16 21:11:20 -0600

    Set picture and user data containers with breakpoints

--- client/src/components/TestimonialCard.js
+++ client/src/components/TestimonialCard.js
@@ -3,17 +3,26 @@ import '../styles/TestimonialCard.css'
 
 const TestimonialCard = ({userPicture, country, username, mainSentence, testimonial}) => {
   return (
-    <div className='flex flex-col items-start justify-between relative px-6 pb-5 bg-white text-black rounded-xl max-w-2xl'>
-      <svg className='testimonial-quote quote-down' viewBox="0 0 24 24">
+    <div className='flex flex-col items-start justify-between relative px-6 pb-5 bg-white text-black rounded-xl w-full max-w-2xl'>
+      <svg className='testimonial-quote rotate-180 top-2 right-0 sm:top-10 sm:right-10' viewBox="0 0 24 24">
           <path fill="currentColor" d="M10,7L8,11H11V17H5V11L7,7H10M18,7L16,11H19V17H13V11L15,7H18Z" />
       </svg>
-      <div className='flex flex-row relative -left-20 -top-10 text-lg font-bold py-2 px-20 bg-blue-900 rounded-tr-[50px]'>
-        <div className='flex text-center items-center justify-center rounded-full w-24 h-24 border-[var(--yellow-color)] border-solid border-4 bg-white'>Image</div>
-        <div className='flex flex-col items-start justify-center ml-2'>
+      <div className='hidden flex-row justify-between relative -left-16 -top-10 text-lg font-bold py-2 px-12 w-80 bg-blue-600 rounded-tr-[50px] sm:flex'>
+        <div className='flex text-center items-center justify-center rounded-full w-24 h-24 border-[var(--yellow-color)] border-solid border-4 bg-white mr-6'>Image</div>
+        <div className='flex flex-col items-start justify-center'>
           <div>Country</div>
           <div>Username</div>
         </div>
-        <div className='absolute w-14 h-20 bg-black top-full left-0'></div>
+        <div className='absolute w-10 h-40 top-full left-0  overflow-hidden'>
+          <div className='w-24 h-52 -rotate-[20deg] bg-blue-900 -right-[79px] -top-12 absolute'></div>
+        </div>
+      </div>
+      <div className='flex flex-row text-lg font-bold w-full justify-center sm:hidden py-10'>
+          <div className='flex text-center items-center justify-center rounded-full w-24 h-24 border-[var(--yellow-color)] border-solid border-4 bg-white mr-6'>Image</div>
+          <div className='flex flex-col items-start justify-center'>
+            <div>Country</div>
+            <div>Username</div>
+          </div>
       </div>
       <div className='font-bold text-xl text-center mb-5'>flex flex-col items-center justify-between relative bg-green-400 w-96 h-40 mt-40
       flex flex-col items-center justify-between relative bg-green-400 w-96 h-40 mt-40</div>

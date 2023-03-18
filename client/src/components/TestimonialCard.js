9541c9948dbe147594ef0e7d8c43252b0c84a441
Arturo Rivera <arturo.riverar97@gmail.com>
2023-03-18 07:44:47 -0600

    Set props to fill TestimonialCard fields

--- client/src/components/TestimonialCard.js
+++ client/src/components/TestimonialCard.js
@@ -8,10 +8,10 @@ const TestimonialCard = ({userPicture, country, username, mainSentence, testimon
           <path fill="currentColor" d="M10,7L8,11H11V17H5V11L7,7H10M18,7L16,11H19V17H13V11L15,7H18Z" />
       </svg>
       <div className='hidden flex-row justify-between relative -left-16 -top-10 text-lg font-bold py-2 px-12 w-80 bg-blue-600 rounded-tr-[50px] sm:flex'>
-        <div className='flex text-center items-center justify-center rounded-full w-24 h-24 border-[var(--yellow-color)] border-solid border-4 bg-white mr-6'>Image</div>
+        <img src={userPicture} className='flex text-center items-center justify-center rounded-full w-24 h-24 border-[var(--yellow-color)] border-solid border-4 bg-white mr-6'></img>
         <div className='flex flex-col items-start justify-center'>
-          <div>Country</div>
-          <div>Username</div>
+          <div>{country}</div>
+          <div>{username}</div>
         </div>
         <div className='absolute w-10 h-40 top-full left-0  overflow-hidden'>
           <div className='w-24 h-52 -rotate-[20deg] bg-blue-900 -right-[79px] -top-12 absolute'></div>
@@ -20,15 +20,12 @@ const TestimonialCard = ({userPicture, country, username, mainSentence, testimon
       <div className='flex flex-row text-lg font-bold w-full justify-center sm:hidden py-10'>
           <div className='flex text-center items-center justify-center rounded-full w-24 h-24 border-[var(--yellow-color)] border-solid border-4 bg-white mr-6'>Image</div>
           <div className='flex flex-col items-start justify-center'>
-            <div>Country</div>
-            <div>Username</div>
+            <div>{country}</div>
+            <div>{username}</div>
           </div>
       </div>
-      <div className='font-bold text-xl text-center mb-5'>flex flex-col items-center justify-between relative bg-green-400 w-96 h-40 mt-40
-      flex flex-col items-center justify-between relative bg-green-400 w-96 h-40 mt-40</div>
-      <div className='text-justify text-lg'>Testimonial flex flex-col items-center justify-between relative bg-green-400 w-96 h-40 mt-40
-      flex flex-col items-center justify-between relative bg-green-400 w-96 h-40 mt-40 flex flex-col items-center justify-between relative bg-green-400 w-96 h-40 mt-40
-      </div>
+      <div className='font-bold text-xl text-center mb-5'>{mainSentence}</div>
+      <div className='text-justify text-lg'>{testimonial}</div>
     </div>
   )
 }

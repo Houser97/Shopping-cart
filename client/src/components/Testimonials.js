fd7a473f0258ca28be8d0cac06c35d07b46501d0
Arturo Rivera <arturo.riverar97@gmail.com>
2023-03-18 07:43:30 -0600

    Tune grid and map TestimonialsData array to render TestimonialCard

--- client/src/components/Testimonials.js
+++ client/src/components/Testimonials.js
@@ -1,19 +1,26 @@
 import React from 'react'
+import { TestimonialData } from '../assets/constants'
 import HomeLottie from './HomeLottie'
 import TestimonialCard from './TestimonialCard'
 
 const Testimonials = () => {
   return (
-    <section className='grid grid-cols-[minmax(200px,1fr)] 
-    grid-rows-[minmax(200px,1fr)_minmax(200px,400px)] gap-3 py-7 px-6
+    <section className='grid grid-cols-[minmax(100px,1fr)] 
+    grid-rows-[minmax(0px,1fr)_minmax(200px,400px)] gap-3 py-7 px-2
     bg-[var(--blue-color)] text-[var(--yellow-color)] w-full lg:px-14
     lg:grid-cols-[minmax(200px,400px)_minmax(200px,1fr)]
     lg:grid-rows-1'>
         <HomeLottie />
         <div className='self-center h-full row-start-1 lg:row-auto'>
             <h2 className='mb-7 text-center'>Testimonials</h2>
-            <div className='flex flex-row justify-center items-center w-full py-12 px-5 bg-green-400'>
-              <TestimonialCard />
+            <div className='flex flex-row justify-center items-center w-full px-8'>
+              {
+                TestimonialData.map((testimonial, index) => {
+                  return(
+                    <TestimonialCard {...testimonial} key={`testimonial-card-${index}`} />
+                  )
+                })
+              }
             </div>
         </div>
     </section>

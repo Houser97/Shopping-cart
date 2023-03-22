d7f1b6159163e709f610d9871409f33003a5d3a4
Arturo Rivera <arturo.riverar97@gmail.com>
2023-03-21 20:42:02 -0600

    Set up logic to delete user review
    A new button was added to allow users to delete their review. This
    button will invoke the newly created deleteReview() function to delete
    the review by its id.

--- client/src/components/ReviewCard.js
+++ client/src/components/ReviewCard.js
@@ -7,6 +7,7 @@ const ReviewCard = ({likes, dislikes, comment, author, rating, date ,formatted_d
 
     const API = useContext(CartContext).API
     const user = useContext(CartContext).user
+    const setUpdateReviews = useContext(CartContext).setUpdateReviews
     /*Estados LOCAL ayudan a actualizar interfaz sin tener que refrescar página para recuperar REVIEWS de DB. */
     const [localLikes, setLocalLikes] = useState(new Set(likes))
     const [localDislikes, setLocalDislikes] = useState(new Set(dislikes));
@@ -56,8 +57,25 @@ const ReviewCard = ({likes, dislikes, comment, author, rating, date ,formatted_d
             setLocalDislikes(dislikesSet);
         })
     }
+
+    const deleteReview = () => {
+        fetch(`${API}/delete_review`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({id: _id})
+        })
+        .then(data => data.json())
+        .then(data => {
+            if(data){
+                setUpdateReviews(prev => !prev)
+            }
+        })
+    }
   return (
-    <div className='flex flex-col w-full justify-evenly bg-white mb-3 rounded-lg p-3 sm:px-9'>
+    <div className='flex flex-col w-full justify-evenly bg-white mb-3 rounded-lg p-3 relative sm:px-9'>
         <div className='flex flex-row flex-wrap w-full justify-between'>
             <h3 className='font-bold text-2xl mr-4'>{author.username}</h3>
             <div>
@@ -76,9 +94,16 @@ const ReviewCard = ({likes, dislikes, comment, author, rating, date ,formatted_d
                 ${user ? `${localDislikes.has(user.id) ? 'fill-orange-600':''}`:''}`} viewBox="0 0 24 24"><path d="M19,15H23V3H19M15,3H6C5.17,3 4.46,3.5 4.16,4.22L1.14,11.27C1.05,11.5 1,11.74 1,12V14A2,2 0 0,0 3,16H9.31L8.36,20.57C8.34,20.67 8.33,20.77 8.33,20.88C8.33,21.3 8.5,21.67 8.77,21.94L9.83,23L16.41,16.41C16.78,16.05 17,15.55 17,15V5C17,3.89 16.1,3 15,3Z" /></svg>
                 <div className='text-lg'>{localDislikes.size}</div>
             </div>
-            <Link className={`rounded-lg font-bold text-lg bg-blue-600 text-white px-5 
-            py-1 absolute right-0 top-0 tracking-wider cursor-pointer ${user ? author._id === user.id ? 'flex':'hidden':'hidden'}`
-            } to={`/${productId}/1/review`}>Edit</Link>
+            <div className='flex flex-row absolute right-0 top-0'>
+                <Link className={`rounded-lg font-bold text-lg bg-blue-600 text-white
+                py-1 tracking-wider cursor-pointer mr-1 ${user ? author._id === user.id ? 'flex':'hidden':'hidden'} text-base px-2 sm:px-3 sm:mr-2 sm:text-lg`
+                } to={`/${productId}/1/review`}>Edit</Link>
+                <button className={`rounded-lg font-bold text-lg bg-orange-600 text-white
+                py-1 tracking-wider cursor-pointer ${user ? author._id === user.id ? 'flex':'hidden':'hidden'} text-base px-2 sm:px-3 sm:text-lg`}
+                onClick={() => deleteReview()}>
+                    Delete
+                </button>
+            </div>
         </div>
         <div className='w-full text-end text-xl font-bold'>{formatted_date}</div>
     </div>

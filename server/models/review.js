381753972e904547a90de97c46e9f3ad87c2443b
Arturo Rivera <arturo.riverar97@gmail.com>
2023-03-22 21:06:19 -0600

    Set pre-save middleware to add date
    The date was generated when the schema was defined, not when users
    created a review. Therefore, a pre-save middleware was set to add the
    current date to a newly created review prior to saving the new document
    in the collection.

--- server/models/review.js
+++ server/models/review.js
@@ -6,12 +6,17 @@ const ReviewSchema = new Schema({
     author: {type: Schema.Types.ObjectId, ref: 'User'},
     item: {type: Number, required: true},
     rating: {type: Number, required: true},
-    date: {type: Date, default: Date.now()},
+    date: {type: Date},
     likes: [],
     dislikes: [],
     comment: {type: String, required: true}
 })
 
 ReviewSchema.virtual('formatted_date').get(function(){return DateTime.fromJSDate(this.date).toFormat('d LLL yyyy, T')})
+
+ReviewSchema.pre('save', function (next) {
+    this.date = Date.now();
+    next();
+});
 
 module.exports = mongoose.model('Review', ReviewSchema)

0fcf06f2bd37c427facb946de7cb2a115d96ec4f
Arturo Rivera <arturo.riverar97@gmail.com>
2023-03-26 08:27:49 -0600

    Remove padding left a top

--- client/src/components/Testimonials.js
+++ client/src/components/Testimonials.js
@@ -41,14 +41,14 @@ const Testimonials = () => {
 
   return (
     <section className='grid grid-cols-[minmax(100px,1fr)] 
-    grid-rows-[minmax(0px,1fr)_minmax(200px,400px)] gap-3 py-7 px-2
+    grid-rows-[minmax(0px,1fr)_minmax(200px,400px)] gap-12 py-7 px-2
     bg-[var(--blue-color)] text-[var(--yellow-color)] w-full lg:px-14
     lg:grid-cols-[minmax(200px,400px)_minmax(200px,1200px)]
     lg:grid-rows-1 justify-center'>
         <HomeLottie />
         <div className='flex flex-col self-center items-center justify-center h-full row-start-1 lg:row-auto overflow-y-visible relative'>
             <h2 className='text-center'>Testimonials</h2>
-            <div className='flex flex-row justify-start items-center pl-0 py-2 overflow-x-hidden overflow-y-visible sm:pl-16 h-full w-full sm:py-16'
+            <div className='flex flex-row justify-start items-center pl-0 py-2 overflow-x-hidden overflow-y-visible h-full w-full sm:pb-16'
             onMouseEnter={() => setIsHovering(true)}
             onMouseLeave={() => setIsHovering(false)}>
               <div className='flex flex-row justify-start items-center transition-transform duration-300 ease-in-out transform w-full gap-32 mb-10 sm:mb-0'

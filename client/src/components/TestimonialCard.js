64264d25e2c16964fc41bc538ad80383207b3573
Arturo Rivera <arturo.riverar97@gmail.com>
2023-03-26 08:28:11 -0600

    Set style used in small devices as the default

--- client/src/components/TestimonialCard.js
+++ client/src/components/TestimonialCard.js
@@ -7,16 +7,18 @@ const TestimonialCard = ({userPicture, username, mainSentence, testimonial}) => 
       <svg className='testimonial-quote rotate-180 top-2 right-0 sm:top-10 sm:right-10' viewBox="0 0 24 24">
           <path fill="currentColor" d="M10,7L8,11H11V17H5V11L7,7H10M18,7L16,11H19V17H13V11L15,7H18Z" />
       </svg>
-      <div className='hidden flex-row justify-between relative -left-16 -top-10 text-lg font-bold py-2 px-12 w-80 bg-blue-600 rounded-tr-[50px] sm:flex'>
-        <img src={userPicture} className='flex text-center items-center justify-center rounded-full w-24 h-24 border-[var(--yellow-color)] border-solid border-4 bg-white mr-6'></img>
-        <div className='flex flex-col items-start justify-center'>
-          <div>{username}</div>
+      {/* 
+        <div className='hidden flex-row justify-between relative -left-16 -top-10 text-lg font-bold py-2 px-12 w-80 bg-blue-600 rounded-tr-[50px] sm:flex'>
+          <img src={userPicture} className='flex text-center items-center justify-center rounded-full w-24 h-24 border-[var(--yellow-color)] border-solid border-4 bg-white mr-6'></img>
+          <div className='flex flex-col items-start justify-center'>
+            <div>{username}</div>
+          </div>
+          <div className='absolute w-10 h-40 top-full left-0  overflow-hidden'>
+            <div className='w-24 h-52 -rotate-[20deg] bg-blue-900 -right-[79px] -top-12 absolute'></div>
+          </div>
         </div>
-        <div className='absolute w-10 h-40 top-full left-0  overflow-hidden'>
-          <div className='w-24 h-52 -rotate-[20deg] bg-blue-900 -right-[79px] -top-12 absolute'></div>
-        </div>
-      </div>
-      <div className='flex flex-row text-lg font-bold w-full justify-center sm:hidden py-10'>
+      */}
+      <div className='flex flex-row text-lg font-bold w-full justify-center py-10'>
           <img src = {userPicture} className='flex text-center items-center justify-center rounded-full w-24 h-24 border-[var(--yellow-color)] border-solid border-4 bg-white mr-6'></img>
           <div className='flex flex-col items-start justify-center'>
             <div>{username}</div>

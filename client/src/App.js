cd7fc8f74cb177c3c4faa8c0216c9871fd8c300a
Arturo Rivera <arturo.riverar97@gmail.com>
2023-03-26 08:57:20 -0600

    Change project basename

--- client/src/App.js
+++ client/src/App.js
@@ -151,7 +151,7 @@ function App() {
   }
 
   return (
-    <BrowserRouter basename='Shopping-cart/'>
+    <BrowserRouter basename='/'>
       <CartContext.Provider value = {cartContextValue}>
         <div className="App">
           <BuyAnimation />

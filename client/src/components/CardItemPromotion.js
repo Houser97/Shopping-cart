468e459949fd108981baa228fe8728112f3e5e98
Arturo Rivera <arturo.riverar97@gmail.com>
2023-03-26 08:27:27 -0600

    Center text and add font-bold to price section

--- client/src/components/CardItemPromotion.js
+++ client/src/components/CardItemPromotion.js
@@ -5,9 +5,9 @@ const CardItemPromotion = ({section, image, price}) => {
   return (
     <div className='cardItemPromotion'>
         <div className='promotion_data'>
-            <div className='section_promotion'>{section}</div>
+            <div className='section_promotion font-extrabold'>{section}</div>
             <div className='offer_duration'>Offer available only this week</div>
-            <div className='price_promotion'>Only {price}</div>
+            <div className='price_promotion font-bold'>Only {price}</div>
         </div>
         <img className='item_promotion_img' alt='item_promotion' src = {image}></img>
     </div>

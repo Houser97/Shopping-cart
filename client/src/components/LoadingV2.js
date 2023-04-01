f7657ea3ab9b33b0817f20b54e9054de63d3c71e
Arturo Rivera <arturo.riverar97@gmail.com>
2023-03-31 21:34:14 -0600

    Set up second Loading component

---
+++ client/src/components/LoadingV2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/LoadingV2.css'
+
+const LoadingV2 = () => {
+  return (
+    <span class="loader inline-block w-12 h-12 
+    rounded-full border-t-4 border-green-500 
+    border-r-4 border-r-transparent mt-10"></span>
+  )
+}
+
+export default LoadingV2

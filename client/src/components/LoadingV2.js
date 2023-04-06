cb37305aef3e5fc3babd4d2b03a1bce6a99c2683
Arturo Rivera <arturo.riverar97@gmail.com>
2023-04-05 21:27:47 -0600

    Rename class with className

--- client/src/components/LoadingV2.js
+++ client/src/components/LoadingV2.js
@@ -3,7 +3,7 @@ import '../styles/LoadingV2.css'
 
 const LoadingV2 = () => {
   return (
-    <span class="loader inline-block w-12 h-12 
+    <span className="loader inline-block w-12 h-12 
     rounded-full border-t-4 border-green-600
     border-r-4 border-r-transparent mt-10 self-center"></span>
   )

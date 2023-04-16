96fe0d3b67beb999ab6352e3e330a6d78f42c0f4
Arturo Rivera <arturo.riverar97@gmail.com>
2023-04-16 17:05:47 -0600

    Set cart and products slices

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,12 @@
 import userSlice from './slices/user'
+import cartSlice from './slices/cart'
+import productsSlice from './slices/products'
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 
 const rootReducer = combineReducers({
-    user: userSlice
+    user: userSlice,
+    cart: cartSlice,
+    products: productsSlice
 })
 
 export const store = configureStore({

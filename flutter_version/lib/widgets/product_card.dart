331f5f3a620a7fa365c258aafd606a330ab7e792
Arturo Rivera <arturo.riverar97@gmail.com>
2024-01-09 21:19:24 -0600

    Wrap widget with GestureDetector for navigation

--- flutter_version/lib/widgets/product_card.dart
+++ flutter_version/lib/widgets/product_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_version/pages/product_details.dart';
 
 class ProductCard extends StatelessWidget {
   final String title;
@@ -20,14 +21,22 @@ class ProductCard extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            width: double.infinity,
-            decoration: const BoxDecoration(
-                color: Color.fromRGBO(240, 242, 241, 1),
-                borderRadius: BorderRadius.all(Radius.circular(16))),
-            child: Image.asset(
-              image,
-              height: 150,
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                return ProductDetails(
+                    image: image, price: price, rating: rating, title: title);
+              }));
+            },
+            child: Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                  color: Color.fromRGBO(240, 242, 241, 1),
+                  borderRadius: BorderRadius.all(Radius.circular(16))),
+              child: Image.asset(
+                image,
+                height: 150,
+              ),
             ),
           ),
           Row(

dbf03400ab50c43989498ec154c661e298888da6
Arturo Rivera <arturo.riverar97@gmail.com>
2024-01-13 12:09:18 -0600

    Add Checkout button and wrap widgets in Column

--- flutter_version/lib/pages/cart.dart
+++ flutter_version/lib/pages/cart.dart
@@ -9,17 +9,46 @@ class Cart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.all(10.0),
-        child: ListView.builder(
-            itemCount: products.length,
-            itemBuilder: (context, index) {
-              final title = products[index]['title'];
-              final price = products[index]['price'];
-              final image = products[index]['image'];
-              return ProductCart(
-                image: image as String,
-                title: title as String,
-                price: price as double,
-              );
-            }));
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  itemCount: products.length,
+                  itemBuilder: (context, index) {
+                    final title = products[index]['title'];
+                    final price = products[index]['price'];
+                    final image = products[index]['image'];
+                    return ProductCart(
+                      image: image as String,
+                      title: title as String,
+                      price: price as double,
+                    );
+                  }),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 25,
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                ),
+                child: const Text(
+                  'Checkout',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }

f0cc18f07f5e63dba1594da1d01c7b5e619f5e92
Arturo Rivera <arturo.riverar97@gmail.com>
2024-01-14 19:31:38 -0600

    Create constants file

---
+++ flutter_version/lib/widgets/constants.dart
@@ -0,0 +1,52 @@
+final products = [
+  {
+    'id': 0,
+    'title': 'Xbox',
+    'price': 400.00,
+    'image': 'assets/images/xbox.png',
+    'rating': 4.8,
+    'categories': ['Electronic', 'Videogames']
+  },
+  {
+    'id': 1,
+    'title': 'iPhone',
+    'price': 1000.00,
+    'image': 'assets/images/iphone.png',
+    'rating': 5.0,
+    'categories': ['Smartphones', 'Electronic']
+  },
+  {
+    'id': 2,
+    'title': 'Mac',
+    'price': 950.00,
+    'image': 'assets/images/mac.png',
+    'rating': 5.0,
+    'categories': ['Laptops', 'Electronic']
+  },
+  {
+    'id': 3,
+    'title': 'Air pods',
+    'price': 200.00,
+    'image': 'assets/images/airpods.png',
+    'rating': 4.8,
+    'categories': ['Headphones', 'Laptops', 'Electronic']
+  },
+  {
+    'id': 4,
+    'title': 'Headphones',
+    'price': 180.00,
+    'image': 'assets/images/headphones.png',
+    'rating': 4.7,
+    'categories': ['Headphones', 'Electronic']
+  },
+  {
+    'id': 5,
+    'title': 'Mouse Razer',
+    'price': 400.00,
+    'image': 'assets/images/mouse.png',
+    'rating': 4.8,
+    'categories': ['Laptops', 'Electronic']
+  },
+];
+
+final categories = ['All', 'Smartphones', 'Laptops', 'Headphones'];

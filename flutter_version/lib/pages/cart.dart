a92fc4ad52fee510dd23232beddc3893b777b29b
Arturo Rivera <arturo.riverar97@gmail.com>
2024-01-23 19:50:10 -0600

    Use provider to fill Cart Widget

--- flutter_version/lib/pages/cart.dart
+++ flutter_version/lib/pages/cart.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_version/widgets/constants.dart';
+import 'package:flutter_version/provider/cart_provider.dart';
 import 'package:flutter_version/widgets/product_cart.dart';
+import 'package:provider/provider.dart';
 
 class Cart extends StatelessWidget {
   const Cart({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final cart = context.watch<CartProvider>().cart;
+
     return Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
@@ -14,15 +17,19 @@ class Cart extends StatelessWidget {
           children: [
             Expanded(
               child: ListView.builder(
-                  itemCount: products.length,
+                  itemCount: cart.length,
                   itemBuilder: (context, index) {
-                    final title = products[index]['title'];
-                    final price = products[index]['price'];
-                    final image = products[index]['image'];
+                    final id = cart[index]['id'];
+                    final title = cart[index]['title'];
+                    final price = cart[index]['price'];
+                    final image = cart[index]['image'];
+                    final currentQty = cart[index]['quantity'];
                     return ProductCart(
+                      id: id as int,
                       image: image as String,
                       title: title as String,
                       price: price as double,
+                      currentQty: currentQty,
                     );
                   }),
             ),
@@ -30,7 +37,7 @@ class Cart extends StatelessWidget {
               height: 10,
             ),
             const Text(
-              'Total: \$1304',
+              'Total: \$100',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 17,

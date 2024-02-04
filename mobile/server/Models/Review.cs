fbe5039e10244218d99f795af392331c5249d17e
Arturo Rivera <arturo.riverar97@gmail.com>
2024-02-04 14:24:40 -0600

    Create Review model

---
+++ mobile/server/Models/Review.cs
@@ -0,0 +1,12 @@
+﻿namespace server.Models
+{
+    public record class Review
+    {
+        public string Author { get; init; } = String.Empty;
+        public string Id { get; init; } = String.Empty;
+        public List<string> Like { get; init; } = [];
+        public List<string> Dislikes { get; init; } = [];
+        public DateTime Date { get; init; }
+        public string Comment { get; init; } = String.Empty;
+    }
+}

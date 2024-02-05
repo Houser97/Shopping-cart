dd9c6de57b2eb7c41666eed4da7160b4c3c9e0e2
Arturo Rivera <arturo.riverar97@gmail.com>
2024-02-05 14:46:30 -0600

    Adjust Review model to work with MongoDB

--- mobile/server/Models/Review.cs
+++ mobile/server/Models/Review.cs
@@ -1,12 +1,29 @@
-﻿namespace server.Models
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace server.Models
 {
     public record class Review
     {
-        public string Author { get; init; } = String.Empty;
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string Id { get; init; } = String.Empty;
-        public List<string> Like { get; init; } = [];
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        [BsonElement("author")]
+        public string Author { get; init; } = String.Empty;
+        [BsonElement("item")]
+        public int Item { get; set; }
+        [BsonElement("rating")]
+        public int Rating { get; set; }
+        [BsonElement("likes")]
+        public List<string> Likes { get; init; } = [];
+        [BsonElement("dislikes")]
         public List<string> Dislikes { get; init; } = [];
+        [BsonElement("date")]
         public DateTime Date { get; init; }
+        [BsonElement("comment")]
         public string Comment { get; init; } = String.Empty;
+        [BsonIgnoreIfDefault]
+        [BsonElement("__v")]
+        public int? VersionKey { get; set; }
     }
 }

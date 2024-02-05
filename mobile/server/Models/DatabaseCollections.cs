628b3df79ef408d1d50b4026c567e933d802a0af
Arturo Rivera <arturo.riverar97@gmail.com>
2024-02-05 14:47:06 -0600

    Create DatabaseCollections class

---
+++ mobile/server/Models/DatabaseCollections.cs
@@ -0,0 +1,16 @@
+﻿namespace server.Models
+{
+    public record class DatabaseCollections : IDatabaseCollections
+    {
+        public string ReviewsCollection { get; set; } = "reviews";
+        public string UsersCollection { get; set; } = "users";
+        public string ProductsCollection { get; set; } = "products";
+    }
+
+    public interface IDatabaseCollections
+    {
+        public string ReviewsCollection { get; set; }
+        public string UsersCollection { get; set; }
+        public string ProductsCollection { get; set; }
+    }
+}

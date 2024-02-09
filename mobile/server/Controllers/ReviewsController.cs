7dabce0ce4df5b738a0c75c5f79acbbf93e09fc1
Arturo Rivera <arturo.riverar97@gmail.com>
2024-02-09 17:42:34 -0600

    Create ReviewsController to handle HTTP requests

---
+++ mobile/server/Controllers/ReviewsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using server.Models;
+using server.Services;
+
+namespace server.Controllers
+{
+    [Route("api/reviews")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+
+        private readonly ReviewsService _reviewsService;
+        public ReviewsController(ReviewsService reviewsService)
+        {
+            _reviewsService = reviewsService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetReviews()
+        {
+            // Realiza una consulta para obtener todas las reviews
+            var reviews = await _reviewsService.GetAll();
+
+            return Ok(reviews);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] string id)
+        {
+            var review = await _reviewsService.GetById(id);
+            Console.WriteLine(review);
+            return Ok(review);
+        }
+    }
+
+}

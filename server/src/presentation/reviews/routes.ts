abfd70b4a36f55c5c5a6a97373dfbdf4d46c9491
Arturo Rivera <arturo.riverar97@gmail.com>
2024-04-26 22:24:26 -0600

    Create review routes

---
+++ server/src/presentation/reviews/routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { ReviewController } from "./controller";
+import { ReviewService } from "../services/review.service";
+
+export class ReviewRoutes {
+    static get routes(): Router {
+
+        const router = Router();
+        const reviewService = new ReviewService();
+        const controller = new ReviewController(reviewService);
+
+        router.get('/', controller.getReviews);
+        router.post('/', controller.createReview);
+        router.put('/', controller.updateReview);
+        router.delete('/', controller.deleteReview);
+
+        return router;
+    }
+}

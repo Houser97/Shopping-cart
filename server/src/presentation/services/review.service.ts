daf0705df3bebbacaa0f7356eb9693c63282c023
Arturo Rivera <arturo.riverar97@gmail.com>
2024-04-26 22:23:03 -0600

    Create ReviewService with unimplemented methods

---
+++ server/src/presentation/services/review.service.ts
@@ -0,0 +1,19 @@
+export class ReviewService {
+    constructor() { }
+
+    async getReviews() {
+        throw 'Unimplemented method';
+    }
+
+    async createReview() {
+        throw 'Unimplemented method';
+    }
+
+    async updateReview() {
+        throw 'Unimplemented method';
+    }
+
+    async deleteReview() {
+        throw 'Unimplemented method';
+    }
+}

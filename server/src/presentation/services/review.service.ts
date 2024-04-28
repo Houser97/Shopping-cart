db16178411dd6a57bd5fe4293eab0c29c224f8fb
Arturo Rivera <arturo.riverar97@gmail.com>
2024-04-27 22:09:57 -0600

    Implement getReviews method

--- server/src/presentation/services/review.service.ts
+++ server/src/presentation/services/review.service.ts
@@ -1,8 +1,32 @@
+import { ReviewModel } from "../../data/mongo/models/review.model";
+import { PaginationDto } from "../../domain/dtos/shared/pagination.dto";
+import { CustomError } from "../../domain/errors/custom.error";
+
 export class ReviewService {
     constructor() { }
 
-    async getReviews() {
-        throw 'Unimplemented method';
+    async getReviews(productId: string, paginationDto: PaginationDto) {
+        const { page, limit } = paginationDto;
+
+        try {
+            const reviews = await ReviewModel.find({ item: productId })
+                .sort({ date: -1 })
+                .populate('author', 'username')
+                .skip((page - 1) * limit)
+                .limit(limit);
+
+            return {
+                page,
+                limit,
+                total: reviews.length,
+                next: `/api/reviews/${productId}?page=${page + 1}&limit=${limit}`,
+                prev: ((page - 1) > 0) ? `/api/reviews/${productId}?page=${page - 1}&limit=${limit}` : null,
+                reviews
+            }
+
+        } catch (error) {
+            throw CustomError.internalServer(`${error}`);
+        }
     }
 
     async createReview() {

e3396d0b4f5dbde72c838bb13391ed51ffc90ddc
Arturo Rivera <arturo.riverar97@gmail.com>
2024-04-27 22:09:06 -0600

    Create Pagination DTO

---
+++ server/src/domain/dtos/shared/pagination.dto.ts
@@ -0,0 +1,14 @@
+export class PaginationDto {
+    constructor(
+        public readonly page: number,
+        public readonly limit: number,
+    ) { }
+
+    static create(page: number = 1, limit: number = 10): [string?, PaginationDto?] {
+        if (isNaN(page) || isNaN(limit)) return ['Page and Limit must be numbers'];
+        if (page <= 0) return ['Page must be greater than 0'];
+        if (limit <= 0) return ['Limit must be greater than 0'];
+
+        return [undefined, new PaginationDto(page, limit)];
+    }
+}

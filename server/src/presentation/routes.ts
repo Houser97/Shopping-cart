5f8bc8e339386f5f076f8a7d5e4408dc5e6daf4f
Arturo Rivera <arturo.riverar97@gmail.com>
2024-05-05 21:53:34 -0600

    Add Review routes and comment remaining ones

--- server/src/presentation/routes.ts
+++ server/src/presentation/routes.ts
@@ -7,9 +7,9 @@ export class AppRoutes {
 
         const router = Router();
 
-        router.use('/api/reviews', ReviewRoutes.routes);
-        router.use('/api/products',);
-        router.use('/api/auth',);
+        router.use('/api/reviews/', ReviewRoutes.routes);
+        //router.use('/api/products',);
+        //router.use('/api/auth',);
         //router.use('/api/upload',);
         //router.use('/api/images',);
 

f047848b9427891cb94e0a389624ed582b40ba06
Arturo Rivera <arturo.riverar97@gmail.com>
2024-06-09 14:20:00 -0600

    Create UserEntity instance with fromObject method

--- server/src/domain/entities/user.entity.ts
+++ server/src/domain/entities/user.entity.ts
@@ -1,3 +1,5 @@
+import { CustomError } from "../errors/custom.error";
+
 export class UserEntity {
     constructor(
         public id: string,
@@ -6,4 +8,19 @@ export class UserEntity {
         public password: string,
         public cart: [string],
     ) { }
+
+    static fromObject(object: { [key: string]: any }) {
+        const { _id, email, username, cart, password } = object;
+
+        if (!_id) {
+            throw CustomError.badRequest('Missing id');
+        }
+
+        if (!email) throw CustomError.badRequest('Missing email');
+        if (!username) throw CustomError.badRequest('Missing username');
+        if (!cart) throw CustomError.badRequest('Missing cart');
+        if (!password) throw CustomError.badRequest('Missing password');
+
+        return new UserEntity(_id, username, email, password, cart);
+    }
 }
---
+++ server/src/domain/interfaces/user.interface.ts
@@ -0,0 +1,7 @@
+export interface User {
+    email: string,
+    cart: string[],
+    username: string,
+    _id: string,
+    password: string,
+}

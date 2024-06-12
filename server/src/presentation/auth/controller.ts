f44c60a23363897b1e023430236a1b72e8af7b23
Arturo Rivera <arturo.riverar97@gmail.com>
2024-06-11 21:21:57 -0600

    Set register method to respond register route

--- server/src/presentation/auth/controller.ts
+++ server/src/presentation/auth/controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { AuthService } from "../services/auth.service";
 import { CustomError } from "../../domain/errors/custom.error";
 import { LoginUserDto } from "../../domain/dtos/auth/login-user.dto";
+import { RegisterUserDto } from "../../domain/dtos/auth/register-user.dto";
 
 export class AuthController {
     constructor(
@@ -18,11 +19,21 @@ export class AuthController {
     }
 
     login = async (req: Request, res: Response) => {
-        if (req.user) {
-            return res.status(200).json({ user: req.user })
-        }
+        const [error,] = LoginUserDto.login(req.body)
+        if (error) return CustomError.badRequest(error);
 
-        return res.status(401).json({ error: 'Email or Password incorrect' })
+        this.authService.login(req, res);
+    }
+
+    register = async (req: Request, res: Response) => {
+        const [_, registerUserDto] = RegisterUserDto.create(req.body);
+
+        this.authService.register(registerUserDto!)
+            .then(user => res.json(user))
+            .catch(error => this.handleError(error, res));
+    }
+
+    logout = (req: Request, res: Response) => {
 
     }
 }

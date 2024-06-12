589d290edc171f7c9f46b9044d734d7ced839d48
Arturo Rivera <arturo.riverar97@gmail.com>
2024-06-11 19:57:51 -0600

    Use Passport static method to perform login

--- server/src/presentation/services/auth.service.ts
+++ server/src/presentation/services/auth.service.ts
@@ -1,23 +1,11 @@
-import passport from "passport";
 import { Request, Response } from "express";
-import { User } from "../../domain/interfaces/user.interfaces";
-import { UserEntity } from "../../domain/entities/user.entity";
+import { Passport } from "../../config/passport";
 
 export class AuthService {
     constructor() { }
 
     public login(req: Request, res: Response) {
-        passport.authenticate('local',
-            function (
-                err: unknown,
-                user: User | undefined,
-                info: { [key: string]: string } | undefined) {
-                if (err) return res.status(500).json({ error: 'Internal server error' })
-                if (!user && info) return res.status(401).json({ error: info.message })
-
-                const { password, ...rest } = UserEntity.fromObject(user!);
-                return res.status(200).json({ user: rest })
-            })(req, res);
+        Passport.login(req, res);
     }
 }
 

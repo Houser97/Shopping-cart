e5fef9bf21429b4b31a37d65dec2e4cb96357af9
Arturo Rivera <arturo.riverar97@gmail.com>
2024-06-16 16:58:37 -0600

    Move passport file to folder and create jwt validation

---
+++ server/src/config/passport/passport.ts
@@ -0,0 +1,73 @@
+import passport from 'passport'
+import { UserModel } from '../../data/mongo/models/user.model';
+import { NextFunction, Request, Response } from 'express';
+import { UserEntity } from '../../domain/entities/user.entity';
+import { PassportStrategies } from './strategies';
+
+interface User {
+    id: string;
+}
+
+export class Passport {
+
+    constructor(
+        private readonly usernameField: string = '',
+    ) { }
+
+    public configure() {
+        passport.use(
+            //PassportStrategies.LocalStrategy({usernameField: this.usernameField})
+            PassportStrategies.JwtStrategy()
+        );
+
+        //Creación de Cookie para mantener usuario en sesión.
+        passport.serializeUser((user: any, done) => {
+            done(null, user.id);
+        });
+
+        passport.deserializeUser((id, done) => {
+            UserModel.findById(id, (err: string, user: User) => {
+                done(err, user);
+            })
+        });
+    }
+
+    public initialize() {
+        // init passport on every route call.
+        return passport.initialize();
+    }
+
+    public createSession() {
+        // allow passport to use "express-session".
+        return passport.session();
+    }
+
+    static validateLocalAuth(req: Request, res: Response, next: NextFunction) {
+        passport.authenticate('local',
+            function (
+                err: unknown,
+                user: { [key: string]: any } | undefined,
+                info: { [key: string]: string } | undefined) {
+                if (err) return res.status(500).json({ error: 'Internal server error' })
+                if (!user && info) return res.status(401).json({ error: info.message })
+
+                const { password, ...rest } = UserEntity.fromObject(user!);
+                req.login(user!, next)
+                return res.status(200).json({ user: rest })
+            })(req, res, next);
+    }
+
+    static validateJwtAuth(req: Request, res: Response, next: NextFunction) {
+        passport.authenticate('jwt',
+            function (
+                err: unknown,
+                user: { [key: string]: any } | undefined,
+                info: { [key: string]: string } | undefined) {
+
+                if (err) return res.status(500).json({ error: 'Internal server error' })
+                if (!user && info) return res.status(401).json({ error: info.message })
+
+                next()
+            })(req, res, next);
+    }
+}

f14c71f0789b0ac53c54fa6534ee2a31a8b280c5
Arturo Rivera <arturo.riverar97@gmail.com>
2024-06-16 16:59:53 -0600

    Add JWT_SECRET in envs object

--- server/src/config/envs.ts
+++ server/src/config/envs.ts
@@ -6,4 +6,5 @@ export const envs = {
     PORT: get('PORT').required().asPortNumber(),
     MONGO_URL: get('MONGO_URL').required().asString(),
     MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
+    JWT_SECRET: get('JWT_SECRET').required().asString(),
 }

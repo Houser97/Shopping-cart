63a5ecbc521542af82a5263035a3b76cf49faca2
Arturo Rivera <arturo.riverar97@gmail.com>
2024-06-17 20:06:06 -0600

    Create ProductController with create method

---
+++ server/src/presentation/products/controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { ProductService } from "../services/product.service";
+import { CustomError } from "../../domain/errors/custom.error";
+import { CreateProductDto } from "../../domain/dtos/products/create-product.dto";
+
+export class ProductController {
+    constructor(
+        private readonly productService: ProductService
+    ) { }
+
+    private handleError = (error: unknown, res: Response) => {
+        if (error instanceof CustomError) {
+            return res.status(error.statusCode).json({ error: error.message });
+        }
+
+        console.log(`${error}`);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+
+    createProduct = (req: Request, res: Response) => {
+        const [error, createProductDto] = CreateProductDto.create({ ...req.body });
+        if (error) return res.status(400).json({ error });
+
+        this.productService.create(createProductDto!)
+            .then(product => res.json(product))
+            .catch(error => this.handleError(error, res));
+    }
+
+}

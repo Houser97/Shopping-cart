74977c83eea40fcd0ea429a6968480e9b001d829
Arturo Rivera <arturo.riverar97@gmail.com>
2024-06-18 21:29:24 -0600

    Add delete route for products

--- server/src/presentation/products/routes.ts
+++ server/src/presentation/products/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthMiddleware } from "../middlewares/auth.middleware";
 import { ProductService } from "../services/product.service";
 import { ProductController } from "./controller";
+import { ValidatorsMiddleware } from "../middlewares/validators.middleware";
 
 export class ProductRoutes {
     static get routes(): Router {
@@ -12,6 +13,10 @@ export class ProductRoutes {
 
         router.post('/', [AuthMiddleware.validateAuth], controller.createProduct);
         router.put('/', [AuthMiddleware.validateAuth], controller.updateProduct);
+        router.delete('/:productId', [
+            AuthMiddleware.validateAuth,
+            ValidatorsMiddleware.validateMongoId
+        ], controller.deleteProduct);
 
         return router;
     }

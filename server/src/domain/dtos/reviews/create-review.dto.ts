17055e0ae3313b7c6f566ec31f8bf1449fe4d103
Arturo Rivera <arturo.riverar97@gmail.com>
2024-06-20 21:40:45 -0600

    Include productId in CreateReviewDto class

--- server/src/domain/dtos/reviews/create-review.dto.ts
+++ server/src/domain/dtos/reviews/create-review.dto.ts
@@ -2,20 +2,24 @@ import { Validators } from "../../../config/validators";
 
 export class CreateReviewDto {
     private constructor(
+        public readonly productId: string,
         public readonly authorId: string,
         public readonly comment: string,
     ) { };
 
     static create(props: { [key: string]: any }): [string?, CreateReviewDto?] {
         const {
+            productId,
             authorId,
             comment,
         } = props;
 
+        if (!Validators.isMongoID(productId)) return ['Invalid Product ID'];
         if (!Validators.isMongoID(authorId)) return ['Invalid User ID'];
         if (!comment && comment.length < 5) return ['Comment must be greater than 5 characters'];
 
         return [undefined, new CreateReviewDto(
+            productId,
             authorId,
             comment,
         )]

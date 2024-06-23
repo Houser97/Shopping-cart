f9ec037b9dc0a7eb2ccf48b0e83ee62ffe71ffc8
Arturo Rivera <arturo.riverar97@gmail.com>
2024-06-22 22:51:08 -0600

    Create validateAuthorId middleware

--- server/src/presentation/middlewares/auth.middleware.ts
+++ server/src/presentation/middlewares/auth.middleware.ts
@@ -1,10 +1,19 @@
 import { NextFunction, Request, Response } from "express";
+import { CustomError } from "../../domain/errors/custom.error";
+import { Passport } from "../../config/passport/passport";
+import { RequestWithUser } from "../../domain/interfaces/request-with-user.interface";
 
 export class AuthMiddleware {
-    static validatePassportAuth(req: Request, res: Response, next: NextFunction) {
-        const isAuthenticated = req.isAuthenticated();
-        if (!isAuthenticated) return res.status(401).json({ error: 'Login to create reviews' });
+    static validateAuthorId(req: RequestWithUser, res: Response, next: NextFunction) {
+        const { _id } = req.user;
+        const { authorId: userInBody } = req.body;
+
+        if (_id as any !== userInBody) throw CustomError.unauthorized('User Id does not match the Author Id');
 
         next();
+    }
+
+    static validateAuth(req: Request, res: Response, next: NextFunction) {
+        Passport.validateJwtAuth(req, res, next);
     }
 }

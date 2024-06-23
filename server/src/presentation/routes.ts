b9b3fd6aee910c16c38bac3b065e324f2004daaa
Arturo Rivera <arturo.riverar97@gmail.com>
2024-06-22 22:53:28 -0600

    Set api endpoints: products, ratings and reactions

--- server/src/presentation/routes.ts
+++ server/src/presentation/routes.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { ReviewRoutes } from "./reviews/routes";
 import { AuthRoutes } from "./auth/routes";
+import { RatingRoutes } from "./ratings/routes";
+import { ReactionRoutes } from "./reactions/routes";
+import { ProductRoutes } from "./products/routes";
 
 export class AppRoutes {
 
@@ -10,9 +13,9 @@ export class AppRoutes {
 
         router.use('/api/auth', AuthRoutes.routes);
         router.use('/api/reviews', ReviewRoutes.routes);
-        //router.use('/api/products',);
-        //router.use('/api/upload',);
-        //router.use('/api/images',);
+        router.use('/api/ratings', RatingRoutes.routes);
+        router.use('/api/reactions', ReactionRoutes.routes);
+        router.use('/api/products', ProductRoutes.routes);
 
         return router;
     }

135386175c362219ee8ae0e583a9faa24813184b
Arturo Rivera <arturo.riverar97@gmail.com>
2024-06-23 11:18:01 -0600

    Use JwtPayload instead of generic in validateToken

--- server/src/config/jwt.adapter.ts
+++ server/src/config/jwt.adapter.ts
@@ -1,11 +1,12 @@
 import jwt from 'jsonwebtoken';
 import { envs } from "./envs";
+import { JwtPayload } from '../domain/interfaces/jwt-payload.interface';
 
 
 const JWT_SECRET = envs.JWT_SECRET;
 
 export class JwtAdapter {
-    static generateToken(payload: any, duration: string = '2h') {
+    static generateToken(payload: JwtPayload, duration: string = '2h') {
         return new Promise((resolve) => {
             jwt.sign(payload, JWT_SECRET, { expiresIn: duration }, (err, token) => {
                 if (err) return resolve(null);
@@ -14,11 +15,11 @@ export class JwtAdapter {
         })
     }
 
-    static validateToken<T>(token: string): Promise<T | null> {
+    static validateToken(token: string): Promise<JwtPayload | null> {
         return new Promise((resolve) => {
             jwt.verify(token, JWT_SECRET, (err, decoded) => {
                 if (err) return resolve(null);
-                resolve(decoded as T);
+                resolve(decoded as JwtPayload);
             })
         })
     }

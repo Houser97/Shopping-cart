4e58ff8081d51764820161eb14ef258511f0684a
Arturo Rivera <arturo.riverar97@gmail.com>
2024-06-24 20:16:08 -0600

    Add rating to CreateReviewDto class

--- server/src/domain/dtos/reviews/create-review.dto.ts
+++ server/src/domain/dtos/reviews/create-review.dto.ts
@@ -5,6 +5,7 @@ export class CreateReviewDto {
         public readonly productId: string,
         public readonly authorId: string,
         public readonly comment: string,
+        public readonly rating: number,
     ) { };
 
     static create(props: { [key: string]: any }): [string?, CreateReviewDto?] {
@@ -12,16 +13,24 @@ export class CreateReviewDto {
             productId,
             authorId,
             comment,
+            rating,
         } = props;
+
+        const ratingParsed = +rating;
 
         if (!Validators.isMongoID(productId)) return ['Invalid Product ID'];
         if (!Validators.isMongoID(authorId)) return ['Invalid User ID'];
         if (!comment && comment.length < 5) return ['Comment must be greater than 5 characters'];
 
+        if (isNaN(ratingParsed)) return ['Rating must be a number'];
+        if (+ratingParsed < 0) return ['Rating must be greater or equal to 0'];
+        if (+ratingParsed > 5) return ['Rating must be less or equal to 5'];
+
         return [undefined, new CreateReviewDto(
             productId,
             authorId,
             comment,
+            rating
         )]
     }
 }

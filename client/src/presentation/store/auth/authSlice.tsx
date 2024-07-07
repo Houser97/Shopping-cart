1f6ffb4b7486eecacf5600ee1ada09ab78f57c1f
Arturo Rivera <arturo.riverar97@gmail.com>
2024-07-06 23:42:53 -0600

    Create AuthSlice

---
+++ client/src/presentation/store/auth/authSlice.tsx
@@ -0,0 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export interface AuthState {
+    user: Record<string, any>,
+    errorMessage: string | undefined,
+    status: string
+}
+
+const initialState: AuthState = {
+    status: 'checking', // 'authenticated','not-authenticated',
+    user: {},
+    errorMessage: undefined,
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        onChecking: (state) => {
+            state.status = 'checking';
+            state.user = {};
+            state.errorMessage = undefined;
+        },
+        onLogin: (state, { payload }) => {
+            state.status = 'authenticated';
+            state.user = payload;
+            state.errorMessage = undefined;
+        },
+        onLogout: (state, { payload }) => {
+            state.status = 'not-authenticated';
+            state.user = {};
+            state.errorMessage = payload;
+        },
+        clearErrorMessage: (state) => {
+            state.errorMessage = undefined;
+        }
+    }
+});
+
+
+// Action creators are generated for each case reducer function
+export const { onChecking, onLogin, onLogout, clearErrorMessage } = authSlice.actions;

83205fa89bf66d5ea0f55c8f7f9a99f798526153
Arturo Rivera <arturo.riverar97@gmail.com>
2024-07-07 20:02:38 -0600

    Create uploadImages static method

--- server/src/config/cloudinary/cloudinary.adapter.ts
+++ server/src/config/cloudinary/cloudinary.adapter.ts
@@ -1,8 +1,9 @@
+import { UploadedFile } from "express-fileupload";
 import { CustomError } from "../../domain/errors/custom.error";
 import { envs } from "../envs";
 import { cloudinary } from "./cloudinary.config";
 
-export class Cloudinary {
+export class CloudinaryAdapter {
     static async uploadImage(image: string) {
         try {
             const data = await cloudinary.uploader.upload(image, {
@@ -13,4 +14,15 @@ export class Cloudinary {
             CustomError.internalServer(`${error}`);
         }
     }
+
+
+    static async uploadImages(file: UploadedFile): Promise<string> {
+        return new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream({ resource_type: 'image', upload_preset: envs.CLOUDINARY_PRESET }, (error, result) => {
+                if (error) return reject(error);
+                resolve(result!.secure_url);
+            }).end(file.data);
+        });
+    }
+
 }

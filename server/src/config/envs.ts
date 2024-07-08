b523ec590264f8a7f60fb9a34b7135fd4e3c71ba
Arturo Rivera <arturo.riverar97@gmail.com>
2024-07-07 20:03:04 -0600

    Include cloudinary environment variables

--- server/src/config/envs.ts
+++ server/src/config/envs.ts
@@ -7,4 +7,8 @@ export const envs = {
     MONGO_URL: get('MONGO_URL').required().asString(),
     MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
     JWT_SECRET: get('JWT_SECRET').required().asString(),
+    CLOUD_NAME: get('CLOUD_NAME').required().asString(),
+    API_KEY_CLOUDINARY: get('API_KEY_CLOUDINARY').required().asString(),
+    API_SECRET_CLOUDINARY: get('API_SECRET_CLOUDINARY').required().asString(),
+    CLOUDINARY_PRESET: get('CLOUDINARY_PRESET').required().asString(),
 }

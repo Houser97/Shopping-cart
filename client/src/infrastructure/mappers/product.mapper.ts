ede6191db76f7a3f0d126006ca41c2261e47c3f1
Arturo Rivera <arturo.riverar97@gmail.com>
2024-07-10 20:35:01 -0600

    Create ProductMapper class

---
+++ client/src/infrastructure/mappers/product.mapper.ts
@@ -0,0 +1,16 @@
+import { Product } from "../../domain/entities/product";
+import { ProductDB } from "../interfaces/product-db.response";
+
+export class ProductMapper {
+    static fromDbCastToEntity(product: ProductDB): Product {
+        return {
+            id: product.id,
+            title: product.title,
+            price: product.price,
+            authorId: product.authorId,
+            images: product.images,
+            description: product.description,
+            rating: product.rating
+        }
+    }
+}

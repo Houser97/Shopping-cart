f398ed02341a75969343bad17ab85f25465714b9
Arturo Rivera <arturo.riverar97@gmail.com>
2024-07-12 23:06:03 -0600

    Create IncentiveCard component

---
+++ client/src/presentation/components/home/cards/IncentiveCard.tsx
@@ -0,0 +1,11 @@
+
+export const IncentiveCard = ({ svg, text1, text2 }) => {
+  return (
+    <div className='flex flex-col justify-center items-center group/card w-full h-full
+    font-bold text-xl p-2 bg-[var(--blue-color)] text-white text-center'>
+      <div className='text-2xl mb-2 md:text-3xl'>{text1}</div>
+      <div className='text-[17px] font-light md:text-[22px]'>{text2}</div>
+      <div className='group-hover/card:animate-[svgAnimation_700ms_ease-in-out_1]'>{svg}</div>
+    </div>
+  )
+}

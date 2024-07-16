6e665bf61746f8b8efed6e4148fdb6224d6596ce
Arturo Rivera <arturo.riverar97@gmail.com>
2024-07-15 20:09:45 -0600

    Remove raiting route

--- server/src/presentation/routes.ts
+++ server/src/presentation/routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import { ReviewRoutes } from "./reviews/routes";
 import { AuthRoutes } from "./auth/routes";
-import { RatingRoutes } from "./ratings/routes";
 import { ReactionRoutes } from "./reactions/routes";
 import { ProductRoutes } from "./products/routes";
 
@@ -13,7 +12,6 @@ export class AppRoutes {
 
         router.use('/api/auth', AuthRoutes.routes);
         router.use('/api/reviews', ReviewRoutes.routes);
-        router.use('/api/ratings', RatingRoutes.routes);
         router.use('/api/reactions', ReactionRoutes.routes);
         router.use('/api/products', ProductRoutes.routes);
 

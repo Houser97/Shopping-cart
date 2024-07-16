cf1b1b88549999c44d2ea88f12b044b412062ff0
Arturo Rivera <arturo.riverar97@gmail.com>
2024-07-15 20:24:21 -0600

    Remove id from UpdateReviewDto

--- server/src/domain/dtos/reviews/update-review.dto.ts
+++ server/src/domain/dtos/reviews/update-review.dto.ts
@@ -2,20 +2,15 @@ import { Validators } from "../../../config/validators";
 
 export class UpdateReviewDto {
     private constructor(
-        public readonly id: string,
         public readonly authorId: string,
         public readonly comment: string,
     ) { };
 
     static create(props: { [key: string]: any }): [string?, UpdateReviewDto?] {
         const {
-            id,
             authorId,
             comment,
         } = props;
-
-        if (!id) return ['ProductId is empty'];
-        if (!Validators.isMongoID(id)) return ['Invalid Review Id'];
 
         if (!authorId) return ['AuthorId is empty'];
         if (!Validators.isMongoID(authorId)) return ['Invalid Author Id'];
@@ -23,7 +18,6 @@ export class UpdateReviewDto {
         if (comment !== undefined && (comment.length < 5)) return ['Comment is less than 6 characters'];
 
         return [undefined, new UpdateReviewDto(
-            id,
             authorId,
             comment
         )]

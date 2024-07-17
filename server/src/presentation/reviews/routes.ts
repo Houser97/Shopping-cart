16a88ea84662e7b53e242afa315b695ba4c64a65
Arturo Rivera <arturo.riverar97@gmail.com>
2024-07-16 22:01:46 -0600

    Add validations to review routes

--- server/src/presentation/reviews/routes.ts
+++ server/src/presentation/reviews/routes.ts
@@ -2,18 +2,25 @@ import { Router } from "express";
 import { ReviewController } from "./controller";
 import { ReviewService } from "../services/review.service";
 import { AuthMiddleware } from "../middlewares/auth.middleware";
+import { ValidatorsMiddleware } from "../middlewares/validators.middleware";
+import { ReactionService } from "../services/reaction.service";
 
 export class ReviewRoutes {
     static get routes(): Router {
 
         const router = Router();
-        const reviewService = new ReviewService();
+        const reactionService = new ReactionService();
+
+        const reviewService = new ReviewService(reactionService);
         const controller = new ReviewController(reviewService);
 
-        router.get('/:productId', controller.getReviews);
-        router.post('/', [AuthMiddleware.validatePassportAuth], controller.createReview);
-        router.put('/', [AuthMiddleware.validatePassportAuth, AuthMiddleware.validateAuthorId], controller.updateReview);
-        router.delete('/', [AuthMiddleware.validatePassportAuth, AuthMiddleware.validateAuthorId], controller.deleteReview);
+        router.get('/:productId', AuthMiddleware.optionalAuth, controller.getReviews);
+        router.post('/', [
+            AuthMiddleware.validateAuth,
+            AuthMiddleware.validateAuthorId,
+        ], controller.createReview);
+        router.put('/:id', [AuthMiddleware.validateAuth, AuthMiddleware.validateAuthorId], controller.updateReview);
+        router.delete('/', [AuthMiddleware.validateAuth, AuthMiddleware.validateAuthorId], controller.deleteReview);
 
         return router;
     }

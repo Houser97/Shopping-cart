c145ce556ebe6c03aab08d6f3bd0fa3b8b8b9c26
Arturo Rivera <arturo.riverar97@gmail.com>
2024-07-19 23:16:31 -0600

    Create static methods to get object

--- server/src/domain/entities/reaction.entity.ts
+++ server/src/domain/entities/reaction.entity.ts
@@ -23,4 +23,38 @@ export class ReactionEntity {
 
         return new ReactionEntity(_id, productId, reviewId, authorId, reaction);
     }
+
+    static reactionsToReviewIdObject(object: { [key: string]: any }) {
+        const { _id, reviewId, authorId, reaction, productId } = object;
+
+        if (!_id) {
+            throw CustomError.badRequest('Missing id');
+        }
+
+        if (!productId) throw CustomError.badRequest('Missing Product Id');
+        if (!reviewId) throw CustomError.badRequest('Missing Review Id');
+        if (!authorId) throw CustomError.badRequest('Missing Author Id');
+        if (!reaction) throw CustomError.badRequest('Missing Reaction');
+
+        const key = reviewId.toString();
+
+        return { [key]: object };
+    }
+
+    static reactionsCountToReviewIdObject(array: { [key: string]: any }[]) {
+        const result: { [key: string]: any } = {};
+        array.forEach(object => {
+            const { total, reviewId, reaction } = object;
+            const reviewIdKey = reviewId.toString();
+            if (!(reviewIdKey in result)) {
+                result[reviewIdKey] = {}
+            }
+
+            result[reviewIdKey][reaction] = total
+
+
+        });
+
+        return result;
+    }
 }

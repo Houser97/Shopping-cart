ec42efce26d4fa9964681a5020d43b454f42e58c
Arturo Rivera <arturo.riverar97@gmail.com>
2024-07-19 23:18:19 -0600

    Set getReviews to get reviews data

--- server/src/presentation/services/review.service.ts
+++ server/src/presentation/services/review.service.ts
@@ -1,37 +1,87 @@
+import mongoose from "mongoose";
+import { ProductModel } from "../../data/mongo/models/product.model";
 import { ReviewModel } from "../../data/mongo/models/review.model";
 import { CreateReviewDto } from "../../domain/dtos/reviews/create-review.dto";
 import { UpdateReviewDto } from "../../domain/dtos/reviews/update-review.dto";
 import { PaginationDto } from "../../domain/dtos/shared/pagination.dto";
 import { CustomError } from "../../domain/errors/custom.error";
+import { ReactionService } from "./reaction.service";
 
 export class ReviewService {
-    constructor() { }
+    constructor(
+        private reactionService: ReactionService
+    ) { }
 
-    async getReviews(productId: string, paginationDto: PaginationDto) {
+    async getReviews(productId: string, paginationDto: PaginationDto, authorId: string) {
         const { page, limit } = paginationDto;
 
         try {
-            const reviews = await ReviewModel.find({ item: productId })
-                .sort({ date: -1 })
-                .populate('author', 'username')
-                .skip((page - 1) * limit)
-                .limit(limit);
+            const totalReviewsPromise = ReviewModel.countDocuments({ productId });
+
+            const reviewsPromise = ReviewModel.aggregate([
+                { $match: { productId: new mongoose.Types.ObjectId(productId) } },
+                { $sort: { createdAt: -1 } },
+                { $skip: (page - 1) * limit },
+                { $limit: limit },
+                { $lookup: { from: "ratings", localField: "_id", foreignField: "reviewId", as: "ratings" } },
+                { $lookup: { from: "reactions", localField: "_id", foreignField: "reviewId", as: "reactions" } },
+                { $lookup: { from: "users", localField: "authorId", foreignField: "_id", as: "author" } },
+                { $unwind: { path: "$author", preserveNullAndEmptyArrays: true } },
+                {
+                    $project: {
+                        "author.password": 0,
+                        "author.email": 0,
+                        "author.cart": 0,
+                        "__v": 0,
+                        "author.__v": 0,
+                    }
+                }
+            ]);
+
+            const reactionsPromise = this.reactionService.get(productId, authorId);
+
+            const [totalReviews, reviews, reactions] = await Promise.all([totalReviewsPromise, reviewsPromise, reactionsPromise])
+
+            const reviewsId = reviews.reduce((prev, current) => {
+                return [...prev, current._id];
+            }, []);
+
+            const reactionsTotal = await this.reactionService.getTotalReactions(reviewsId);
 
             return {
                 page,
                 limit,
-                total: reviews.length,
-                next: `/api/reviews/${productId}?page=${page + 1}&limit=${limit}`,
-                prev: ((page - 1) > 0) ? `/api/reviews/${productId}?page=${page - 1}&limit=${limit}` : null,
-                reviews
-            }
-
+                total: totalReviews,
+                totalPages: Math.ceil(totalReviews / limit),
+                next: (page * limit < totalReviews) ? `/api/reviews/${productId}?page=${page + 1}&limit=${limit}` : null,
+                prev: (page > 1) ? `/api/reviews/${productId}?page=${page - 1}&limit=${limit}` : null,
+                reviews: reviews,
+                userReactions: reactions,
+                reactionTotal: reactionsTotal
+            };
         } catch (error) {
             throw CustomError.internalServer(`${error}`);
         }
     }
 
     async create(createReviewDto: CreateReviewDto) {
+        const { productId, authorId } = createReviewDto;
+
+        const productExistsPromise = ProductModel.findById(productId);
+        const reviewExistsPromise = ReviewModel.findOne({ authorId, productId })
+
+        const [
+            productExists,
+            reviewExists
+        ] = await Promise.all([
+            productExistsPromise,
+            reviewExistsPromise
+        ])
+
+
+        if (!productExists) throw CustomError.badRequest('Product does not exists');
+        if (reviewExists) throw CustomError.badRequest('User has already reviewed the product');
+
         try {
             const review = new ReviewModel(createReviewDto);
 
@@ -43,8 +93,8 @@ export class ReviewService {
         }
     }
 
-    async update(updateReviewDto: UpdateReviewDto) {
-        const { id, comment } = updateReviewDto;
+    async update(id: string, updateReviewDto: UpdateReviewDto) {
+        const { comment } = updateReviewDto;
 
         try {
             const updatedReview = await ReviewModel.findByIdAndUpdate(id, { comment });

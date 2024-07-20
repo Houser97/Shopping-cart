1087316c58f70c04af6dc84cce29158bdeb28afa
Arturo Rivera <arturo.riverar97@gmail.com>
2024-07-20 12:13:00 -0600

    Create FileUploadMiddleware

--- server/src/presentation/middlewares/file-upload.middleware.ts
+++ server/src/presentation/middlewares/file-upload.middleware.ts
@@ -4,11 +4,7 @@ export class FileUploadMiddleware {
     static containFiles(req: Request, res: Response, next: NextFunction) {
         if (!req.files || Object.keys(req.files).length === 0) return res.status(400).json({ error: 'No files were selected' });
 
-        if (Array.isArray(req.files.file)) {
-            req.body.images = [req.files.images];
-        } else {
-            req.body.images = req.files.images;
-        }
+        req.body.images = [req.files.images];
 
         next();
     }

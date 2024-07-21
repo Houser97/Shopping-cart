1232b8a11c7b2ad741875c9f26d4cb0b37a21ec9
Arturo Rivera <arturo.riverar97@gmail.com>
2024-07-21 14:52:34 -0600

    Remove productId from update reaction dto

--- server/src/domain/dtos/reactions/update-reaction.dto.ts
+++ server/src/domain/dtos/reactions/update-reaction.dto.ts
@@ -3,7 +3,6 @@ import { ReactionType } from "../../../data/mongo/models/reaction.model";
 
 export class UpdateReactionDto {
     private constructor(
-        public readonly productId: string,
         public readonly reviewId: string,
         public readonly authorId: string,
         public readonly reaction: string,
@@ -12,19 +11,17 @@ export class UpdateReactionDto {
 
     static create(props: { [key: string]: any }): [string?, UpdateReactionDto?] {
         const {
-            productId,
             reviewId,
             authorId,
             reaction
         } = props
 
-        if (!Validators.isMongoID(productId)) return ['Invalid Product ID'];
         if (!Validators.isMongoID(authorId)) return ['Invalid User ID'];
         if (!Validators.isMongoID(reviewId)) return ['Invalid Review ID'];
 
         if (!reaction) return ['Missing reaction'];
         if (!Object.values(ReactionType).includes(reaction)) return ['Reaction is not valid'];
 
-        return [undefined, new UpdateReactionDto(productId, reviewId, authorId, reaction)];
+        return [undefined, new UpdateReactionDto(reviewId, authorId, reaction)];
     }
 }

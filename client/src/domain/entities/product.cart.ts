d7aec1939eb006a5285aeae2eae4aae02f77b452
Arturo Rivera <arturo.riverar97@gmail.com>
2024-08-10 19:24:56 -0600

    Create product cart entities

--- client/src/domain/entities/product.cart.ts
+++ client/src/domain/entities/product.cart.ts
@@ -1,7 +1,13 @@
 export interface ProductCart {
     id: string,
+    userId: string,
+    productId: string,
     price: number,
     image: string,
     name: string,
     quantity: number
+}
+
+export interface ProductCartObject {
+    [productId: string]: ProductCart
 }

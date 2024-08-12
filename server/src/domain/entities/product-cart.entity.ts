40d1630eba70e4608d58f11ea3b23daa03b2583e
Arturo Rivera <arturo.riverar97@gmail.com>
2024-08-12 17:31:40 -0600

    Set image and name properties

--- server/src/domain/entities/product-cart.entity.ts
+++ server/src/domain/entities/product-cart.entity.ts
@@ -5,20 +5,24 @@ export class ProductCartEntity {
         public id: string,
         public userId: string,
         public productId: string,
-        public quantity: string,
+        public quantity: number,
         public price: number,
+        public image: string,
+        public name: string,
     ) { }
 
     static fromObject(object: { [key: string]: any }) {
-        const { _id, userId, productId, quantity, price } = object;
-
+        const { _id, userId, productId: product, quantity } = object;
+        const { price, title, images, _id: productId } = product;
 
         if (!_id) throw CustomError.badRequest('Missing id');
-        if (!userId) throw CustomError.badRequest('Missing title');
-        if (!productId) throw CustomError.badRequest('Missing product id');
+        if (!userId) throw CustomError.badRequest('Missing user id');
+        if (!product) throw CustomError.badRequest('Missing product');
         if (!quantity) throw CustomError.badRequest('Missing quantity');
-        if (!price) throw CustomError.badRequest('Price quantity');
+        if (!title) throw CustomError.badRequest('Missing title');
+        if (!Array.isArray(images)) throw CustomError.badRequest('Missing image');
+        if (!price) throw CustomError.badRequest('Missing price');
 
-        return new ProductCartEntity(_id, userId, productId, quantity, price);
+        return new ProductCartEntity(_id.toString(), userId.toString(), productId.toString(), +quantity, +price, images[0], title);
     }
 }

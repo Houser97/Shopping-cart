eff21a1974aeb1d2438255a2be4e7f6971bae6b3
Arturo Rivera <arturo.riverar97@gmail.com>
2024-08-17 12:09:14 -0600

    Create cart provider

--- client/src/presentation/providers/index.ts
+++ client/src/presentation/providers/index.ts
@@ -1,3 +1,4 @@
+export { cartRepositoryProvider } from "./cart-repository.provider";
 export { reactionRepositoryProvider } from "./reaction-repository.provider";
 export { reviewRepositoryProvider } from "./review-repository.provider";
 export { productRepositoryProvider } from "./product-repository.provider";

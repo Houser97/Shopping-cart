3518c8147d8084da28b72a0616fb3011e7943906
Arturo Rivera <arturo.riverar97@gmail.com>
2024-08-18 14:18:03 -0600

    Set abstract classes for review datasource/repository

---
+++ client/src/domain/datasources/reviews.datasource.ts
@@ -0,0 +1,15 @@
+import { TotalReactions } from "../entities/reaction";
+import { Review } from "../entities/review";
+
+export interface GetReviewsType {
+    reviews: Review[];
+    totalReactions: TotalReactions;
+}
+
+export abstract class ReviewDatasource {
+    abstract getReviews(productId: string): Promise<GetReviewsType>;
+    abstract getReviewByProductIdAndUserId(productId: string): Promise<Review>;
+    abstract createReview(userId: string, comment: string, rating: number, productId: string): Promise<Review>;
+    abstract updateReview(id: string, userId: string, comment: string, rating: number): Promise<Review>;
+    abstract deleteReview(id: string): Promise<Review>;
+}

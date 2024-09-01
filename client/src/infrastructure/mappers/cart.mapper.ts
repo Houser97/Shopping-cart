38dad4ca4b90b51a3d95f2d6eebc603327960ee6
Arturo Rivera <arturo.riverar97@gmail.com>
2024-08-31 18:31:06 -0600

    Create cartmapper methods to create objects

--- client/src/infrastructure/mappers/cart.mapper.ts
+++ client/src/infrastructure/mappers/cart.mapper.ts
@@ -1,14 +1,38 @@
-import { ProductCart } from "../../domain/entities/product.cart";
 import { Product } from "../../domain/entities/product";
+import { ProductCart, ProductCartObject } from "../../domain/entities/product.cart";
+import { ProductCartDBResponse } from "../interfaces/cart-db.response";
 
 export class CartMapper {
-    static toProductCart(product: Product, quantity: number): ProductCart {
+    static toProductCart(product: ProductCartDBResponse): ProductCart {
         return {
             id: product.id,
+            userId: product.userId,
+            productId: product.productId,
+            price: +product.price,
+            image: product.image,
+            name: product.name,
+            quantity: +product.quantity
+        }
+    }
+
+    static toProductCartObject(products: ProductCartDBResponse[]): ProductCartObject {
+        return products.reduce((prev, product) => {
+            const { productId } = product;
+            const key = productId.toString();
+
+            return { ...prev, [key]: product };
+        }, {});
+    }
+
+    static productToProductCart(product: Product, quantity: number, userId: string): ProductCart {
+        return {
+            id: '',
+            userId: userId,
+            productId: product.id,
             price: product.price,
             image: product.images[0],
             name: product.title,
-            quantity
+            quantity: quantity
         }
     }
 }

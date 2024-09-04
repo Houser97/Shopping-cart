377b1877ea2704cf5dbb8802cdcb16fe8a68e9cc
Arturo Rivera <arturo.riverar97@gmail.com>
2024-09-03 19:51:39 -0600

    Create LoginScreen component

---
+++ client/src/presentation/auth/pages/LoginScreen.tsx
@@ -0,0 +1,42 @@
+import { FormEvent } from 'react'
+import { useAuthStore } from '../../hooks/useAuthStore';
+import { useTypedSelector } from '../../store/config/typed-selector';
+import { AuthLayout } from '../layouts/AuthLayout';
+import { useForm } from '../../hooks/useForm';
+
+
+export const LoginScreen = () => {
+
+    const { email, password, onChange } = useForm({
+        email: '',
+        password: ''
+    })
+
+    const { errorMessage } = useTypedSelector(state => state.auth)
+
+    const { startLogin } = useAuthStore();
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        await startLogin({ email, password });
+    }
+
+    return (
+        <AuthLayout handleSubmit={handleSubmit} errorMessage={errorMessage}>
+            <div className='flex flex-col w-full'>
+                <label htmlFor='email' className='font-bold'>E-mail</label>
+                <input type='email' id='email' name='email'
+                    className='border-slate-500 border-2 rounded-md p-1 px-3 
+                outline-[var(--blue-color)]' onChange={onChange} required></input>
+            </div>
+            <div className='flex flex-col mt-4 w-full'>
+                <label htmlFor='password' className='font-bold'>Password</label>
+                <input type='password' id='password' name='password'
+                    className='border-slate-500 border-2 rounded-md p-1 px-3 
+                outline-[var(--blue-color)]' onChange={onChange} required></input>
+            </div>
+
+            <button className='bg-[var(--blue-color)] text-white px-3 py-1 my-4 rounded-md font-bold'>Sign in</button>
+        </AuthLayout>
+    )
+}

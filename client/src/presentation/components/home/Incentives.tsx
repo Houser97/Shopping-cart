30589d7227b65fed44dc5218f365d85505a5cf7a
Arturo Rivera <arturo.riverar97@gmail.com>
2024-09-07 09:58:39 -0600

    Fine tune grid size according to screen size

--- client/src/presentation/components/home/Incentives.tsx
+++ client/src/presentation/components/home/Incentives.tsx
@@ -4,10 +4,10 @@ import { IncentiveCard } from './cards';
 export const Incentives = () => {
   return (
     <section className='grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] 
-    grid-rows-[repeat(auto-fit,minmax(250px,1fr))] auto-rows-[minmax(250px,1fr)] 
-    w-full gap-10 py-10 px-6 bg-white md:px-24 md:grid-cols-[repeat(auto-fit,minmax(300px,1fr))] 
+    grid-rows-[repeat(auto-fit,minmax(250px,1fr))] auto-rows-[minmax(250px,1fr)] justify-center
+    w-full gap-12 py-12 px-6 h-full bg-white md:px-24 md:grid-cols-[repeat(auto-fit,minmax(300px,1fr))] 
     lg:grid-cols-[repeat(auto-fit,minmax(380px,1fr))]
-    items-center'>
+    items-center xl:grid-cols-[repeat(2,minmax(300px,0.5fr))] xl:auto-rows-[minmax(400px,1fr)] xl:gap-16 xl:py-20'>
       {svgIncentives.map((incentive, index) => {
         return (
           <IncentiveCard

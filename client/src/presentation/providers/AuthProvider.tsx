5ea7ddfc365c4c790c9dfae827dd6642eb51af10
Arturo Rivera <arturo.riverar97@gmail.com>
2024-09-14 09:42:37 -0600

    Trigger useEffect with status to update authenticated variable

--- client/src/presentation/providers/AuthProvider.tsx
+++ client/src/presentation/providers/AuthProvider.tsx
@@ -1,14 +1,22 @@
 import { PropsWithChildren, useEffect } from "react"
 import { useAuthStore } from "../hooks/useAuthStore";
+import { useCartStore } from "../hooks/useCartStore";
 
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
 
-    const { checkAuthToken } = useAuthStore();
+    const { status, checkAuthToken } = useAuthStore();
+    const { loadDbProducts } = useCartStore();
 
     useEffect(() => {
         checkAuthToken();
     }, [])
+
+    useEffect(() => {
+        if (status === 'authenticated') {
+            loadDbProducts();
+        }
+    }, [status])
 
     return (
         <>{children}</>

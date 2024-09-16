f13761a0d6bebee99033423b9222def125867474
Arturo Rivera <arturo.riverar97@gmail.com>
2024-09-16 10:47:36 -0600

    Adjust useCartStore to use repository

--- client/src/presentation/hooks/useCartStore.tsx
+++ client/src/presentation/hooks/useCartStore.tsx
@@ -1,8 +1,10 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store/store";
 import { onClear, onUpdate } from "../store";
-import { CartMapper } from "../../infrastructure/mappers/cart.mapper";
 import { ProductCart } from "../../domain/entities/product.cart";
+import { cartRepositoryProvider } from "../providers";
+import { ToastTypes, toast } from "../../config/helpers/Toaster/Toaster";
+
 
 const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
 
@@ -11,14 +13,63 @@ export const useCartStore = () => {
     const { productsInCart, totalPrice, totalProducts } = useTypedSelector(state => state.cart);
     const dispatch = useDispatch();
 
+    const getProducts = async () => {
+        try {
+            return await cartRepositoryProvider.getProducts();
+        } catch (error) {
+            toast('Login to make this operation', ToastTypes.ERROR);
+            return {};
+        }
+    }
 
-    const updateProductInCart = ({ id, quantity, product }) => {
+    const createProduct = async (userId: string, productId: string, quantity: number) => {
+        try {
+            return await cartRepositoryProvider.createProduct(userId, productId, quantity);
+        } catch (error) {
+            toast(`${error}`, ToastTypes.ERROR);
+            return {}
+        }
 
-        const currentQuantity = productsInCart[id]?.quantity ?? 0;
-        const updatedQuantity = currentQuantity + quantity
+    }
 
-        const productCartEntity = CartMapper.toProductCart(product, updatedQuantity);
-        const updatedCart = { ...productsInCart, [id]: productCartEntity }
+    const updateProduct = async (id: string, quantity: number) => {
+        try {
+            return await cartRepositoryProvider.updateProduct(id, quantity);
+        } catch (error) {
+            toast(`${error}`, ToastTypes.ERROR);
+            return {}
+        }
+    }
+
+    const addProducts = (products: ProductCart[]) => {
+
+        const cart = products.reduce((prev, product) => {
+            const { productId } = product;
+            return { ...prev, [productId]: product }
+        }, {})
+
+        const productsData = getProductsData(cart);
+
+        const payload = {
+            productsInCart: cart,
+            totalPrice: productsData.price,
+            totalProducts: productsData.count,
+        }
+
+        dispatch(onUpdate(payload))
+    }
+
+    const loadDbProducts = async () => {
+        const products = await getProducts()
+        if (Object.keys(products).length > 0)
+            addProducts(Object.values(products));
+    }
+
+
+    const updateCart = (product: ProductCart) => {
+        const { productId } = product;
+
+        const updatedCart = { ...productsInCart, [productId]: product }
 
         const productsData = getProductsData(updatedCart);
 
@@ -31,26 +82,29 @@ export const useCartStore = () => {
         dispatch(onUpdate(payload))
     }
 
-    const removeProduct = (id: string) => {
+    const removeProduct = async (id: string) => {
+        try {
+            const { productId } = await cartRepositoryProvider.deleteProduct(id);
+            const { [productId]: product, ...restProducts } = { ...productsInCart };
 
-        const { [id]: productId, ...restProducts } = { ...productsInCart };
+            const productsData = getProductsData(restProducts);
 
+            const payload = {
+                productsInCart: restProducts,
+                totalPrice: productsData.price,
+                totalProducts: productsData.count
+            }
 
-        const productsData = getProductsData(restProducts);
-
-        const payload = {
-            productsInCart: restProducts,
-            totalPrice: productsData.price,
-            totalProducts: productsData.count
+            dispatch(onUpdate(payload));
+        } catch (error) {
+            toast(`${error}`, ToastTypes.ERROR);
         }
-
-        dispatch(onUpdate(payload));
     }
 
     const getProductsData = (products: Record<string, ProductCart>,) =>
         Object.keys(products).reduce((acc, id) => {
             const { quantity, price: productPrice } = products[id]
-            const price = acc.price + quantity * productPrice;
+            const price = (100 * acc.price + quantity * productPrice * 100) / 100;
             const count = acc.count + quantity
 
             return {
@@ -59,8 +113,15 @@ export const useCartStore = () => {
             };
         }, { count: 0, price: 0 });
 
-    const clearCart = () => {
-        dispatch(onClear());
+    const handlePayment = async () => {
+        try {
+            const deleteProducts = await cartRepositoryProvider.handlePayment();
+            dispatch(onClear());
+            return deleteProducts
+        } catch (error) {
+            toast(`${error}`, ToastTypes.ERROR);
+            return []
+        }
     }
 
     return {
@@ -70,9 +131,13 @@ export const useCartStore = () => {
         totalProducts,
 
         // Methods
-        updateProductInCart,
+        updateProduct,
         removeProduct,
-        clearCart
+        handlePayment,
+        getProducts,
+        createProduct,
+        loadDbProducts,
+        updateCart
     }
 
 }

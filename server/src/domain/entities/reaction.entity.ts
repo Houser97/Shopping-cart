dcdee510a57291f4c365e3789d0205ec2480e8e6
Arturo Rivera <arturo.riverar97@gmail.com>
2024-09-28 08:53:48 -0600

    Make productId and authorId optional

--- server/src/domain/entities/reaction.entity.ts
+++ server/src/domain/entities/reaction.entity.ts
@@ -3,10 +3,11 @@ import { CustomError } from "../errors/custom.error";
 export class ReactionEntity {
     private constructor(
         public id: string,
-        public productId: string,
         public reviewId: string,
-        public authorId: string,
-        public reaction: string
+        public reaction: string,
+        public productId?: string,
+        public authorId?: string,
+
     ) { }
 
     static fromObject(object: { [key: string]: any }) {
@@ -21,7 +22,7 @@ export class ReactionEntity {
         if (!authorId) throw CustomError.badRequest('Missing Author Id');
         if (!reaction) throw CustomError.badRequest('Missing Reaction');
 
-        return new ReactionEntity(_id, productId, reviewId, authorId, reaction);
+        return new ReactionEntity(_id, reviewId, reaction, productId, authorId);
     }
 
     static reactionsToReviewIdObject(object: { [key: string]: any }) {
@@ -38,7 +39,7 @@ export class ReactionEntity {
 
         const key = reviewId.toString();
 
-        return { [key]: object };
+        return { [key]: new ReactionEntity(_id, reviewId, reaction) };
     }
 
     static reactionsCountToReviewIdObject(array: { [key: string]: any }[]) {

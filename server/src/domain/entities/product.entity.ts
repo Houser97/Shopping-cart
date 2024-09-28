8d8e66b8fd08e72401ddd1e8f6b9cbd347066096
Arturo Rivera <arturo.riverar97@gmail.com>
2024-09-28 08:53:11 -0600

    Add rating and total reviews validations

--- server/src/domain/entities/product.entity.ts
+++ server/src/domain/entities/product.entity.ts
@@ -8,10 +8,12 @@ export class ProductEntity {
         public authorId: string,
         public images: string[],
         public description: string,
+        public rating?: number,
+        public totalReviews?: number,
     ) { }
 
     static fromObject(object: { [key: string]: any }) {
-        const { _id, title, price, authorId, images, description } = object;
+        const { _id, title, price, authorId, images, description, rating, totalReviews } = object;
 
 
         if (!_id) throw CustomError.badRequest('Missing id');
@@ -20,7 +22,9 @@ export class ProductEntity {
         if (!images) throw CustomError.badRequest('Missing images');
         if (!description) throw CustomError.badRequest('Missing description');
         if (!authorId) throw CustomError.badRequest('Missing Author Id');
+        if (rating && isNaN(rating)) throw CustomError.badRequest('Rating must be a number');
+        if (totalReviews && isNaN(totalReviews)) throw CustomError.badRequest('Total Reviews must be a number');
 
-        return new ProductEntity(_id, title, price, authorId, images, description);
+        return new ProductEntity(_id, title, price, authorId, images, description, rating, totalReviews);
     }
 }

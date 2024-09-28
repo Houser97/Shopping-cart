b4eb5c9dc9d5ce219dc90fdd0308f795152568e4
Arturo Rivera <arturo.riverar97@gmail.com>
2024-09-28 08:52:42 -0600

    Add rating validation

--- server/src/domain/dtos/reviews/update-review.dto.ts
+++ server/src/domain/dtos/reviews/update-review.dto.ts
@@ -4,22 +4,26 @@ export class UpdateReviewDto {
     private constructor(
         public readonly authorId: string,
         public readonly comment: string,
+        public readonly rating: number,
     ) { };
 
     static create(props: { [key: string]: any }): [string?, UpdateReviewDto?] {
         const {
             authorId,
             comment,
+            rating
         } = props;
 
         if (!authorId) return ['AuthorId is empty'];
         if (!Validators.isMongoID(authorId)) return ['Invalid Author Id'];
 
         if (comment !== undefined && (comment.length < 5)) return ['Comment is less than 6 characters'];
+        if (!rating) return ['Rating is missing'];
 
         return [undefined, new UpdateReviewDto(
             authorId,
-            comment
+            comment,
+            rating
         )]
     }
 }

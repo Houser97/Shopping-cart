ccbe858b77b01069d57f422a475f9fcee928f9b9
Arturo Rivera <arturo.riverar97@gmail.com>
2024-11-22 22:11:06 -0600

    Change ProductCard elements

--- client/src/presentation/components/products/cards/ProductCard.tsx
+++ client/src/presentation/components/products/cards/ProductCard.tsx
@@ -38,17 +38,24 @@ export const ProductCard = ({ product, isLazy }: Props) => {
 
     return (
         <div className="grid grid-cols-[repeat(auto-fit,minmax(160px,1fr))] grid-rows-[repeat(auto-fit,minmax(0px,max-content))] 
-        auto-rows-fr w-full h-full gap-2 gap-x-6 p-2 relative bg-white rounded-xl shadow-cardShadow 2sm:grid-cols-[max-content_auto]">
-            <div className='flex flex-row justify-center items-center absolute top-[-15px] left-[-15px] bg-[var(--yellow-color)] w-16 h-16 
-            rounded-full text-xl font-bold text-black'>${price}</div>
+        auto-rows-fr w-full h-full gap-2 gap-x-6 p-2 relative bg-white rounded-xl shadow-cardShadow 2sm:grid-cols-[max-content_auto] animate-fadeIn">
             <Link className='justify-self-center self-center' to={`/product/${id}`}>
                 <img className='max-w-[230px] w-full h-auto self-center justify-self-center 2sm:max-w-auto 2sm:w-auto 2sm:h-[230px] 2sm:justify-self-start' src={images[0]} alt="Apple" loading={`${isLazy ? 'lazy' : 'eager'}`}></img>
             </Link>
             <div className='flex flex-col justify-evenly items-center w-full'>
-                <div className='text-xl text-center font-bold 2sm:text-2xl'>{title}</div>
-                <StarRate product={id} rating={rating} isCustomizable={false} setRating={undefined} />
-                <Link to={`/product/${id}`} className='font-bold text-center text-blue-800 text-lg'>{totalReviewsMessage}</Link>
-                <IncDecProduct dispatch={dispatch} state={state} />
+                <div className='text-xl text-start font-light 2sm:text-2xl'>{title}</div>
+
+                <div className='flex flex-row flex-wrap justify-center gap-x-0 text-2xl'>
+                    <StarRate product={id} rating={rating} isCustomizable={false} setRating={undefined} />
+                    <Link to={`/product/${id}`} className='font-light text-center text-blue-700 text-lg'>{totalReviewsMessage}</Link>
+                </div>
+
+                <div className='flex flex-row-reverse flex-wrap gap-x-10 justify-center'>
+                    <IncDecProduct dispatch={dispatch} state={state} />
+                    <div className='flex flex-row justify-center items-center 
+                    rounded-full text-3xl font-normal text-black'>${price}</div>
+                </div>
+
                 <ProductBtns productId={id} reduceState={state} />
             </div>
         </div>

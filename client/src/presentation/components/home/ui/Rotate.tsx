12c4a7ffdc2f9514becff7003dd2fbcb44b1d223
Arturo Rivera <arturo.riverar97@gmail.com>
2024-11-23 10:52:11 -0600

    Create Rotate component/stylesheet

---
+++ client/src/presentation/components/home/ui/Rotate.tsx
@@ -0,0 +1,19 @@
+import { images } from '../../../../assets/constants'
+import './Rotate.css'
+
+export const Rotate = () => {
+    return (
+        <div className="banner">
+            <div className="product">
+                <div
+                    className="soda"
+                    style={{ "--url": `url(${images.sodaLabel})` } as React.CSSProperties}
+                ></div>
+            </div>
+            <div className="rock">
+                <img src={images.rock} alt=""></img>
+            </div>
+        </div>
+
+    )
+}

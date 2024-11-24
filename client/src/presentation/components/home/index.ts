461fd9cb1a1b475a480ffa7509695108c099a4bc
Arturo Rivera <arturo.riverar97@gmail.com>
2024-11-24 11:26:26 -0600

    Include Carousel

--- client/src/presentation/components/home/index.ts
+++ client/src/presentation/components/home/index.ts
@@ -1,3 +1,4 @@
+export { Carousel } from "./carousel/Carousel";
 export { Incentives } from "./Incentives";
 export { Presentation } from "./Presentation";
 export { Testimonials } from "./Testimonials";
--- client/src/presentation/screens/home/HomeScreen.tsx
+++ client/src/presentation/screens/home/HomeScreen.tsx
@@ -1,10 +1,10 @@
-import { Testimonials, Presentation, Incentives } from '../../components/home';
+import { Testimonials, Presentation, Carousel } from '../../components/home';
 
 export const HomeScreen = () => {
     return (
         <div className='flex flex-col w-full'>
+            <Carousel />
             <Presentation />
-            <Incentives />
             <Testimonials />
         </div>
     )

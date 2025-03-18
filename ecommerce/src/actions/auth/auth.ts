ed9238f2e75c0f46d6dd4917cf3cee7ceaf1cec7
House197 <101058821+Houser97@users.noreply.github.com>
2025-03-18 08:21:50 -0600

    Create register server action

--- ecommerce/src/actions/auth/auth.ts
+++ ecommerce/src/actions/auth/auth.ts
@@ -7,20 +7,38 @@ export const login = async (email: string, password: string) => {
 
     try {
         const { data } = await shoppingApi.post('/auth/login-cookies', { email, password });
-        const token = data.token;
-        setCookie('Authentication',  token, {cookies})
+        const { token, user } = data;
+        await Promise.all([
+            setCookie('Authentication', token, { cookies }),
+            setCookie('UserInfo', JSON.stringify(user), { cookies })
+        ])
         return data;
     } catch (error) {
         console.log(error)
-        return [];
+        return {};
     }
 
+}
+
+export const register = async (email: string, password: string, username: string) => {
+    try {
+        const { data } = await shoppingApi.post('/auth/register-cookies', { email, password, username });
+        const { token, user } = data;
+        await Promise.all([
+            setCookie('Authentication', token, { cookies }),
+            setCookie('UserInfo', JSON.stringify(user), { cookies })
+        ])
+        return data;
+    } catch (error) {
+        console.log(error)
+        return {};
+    }
 }
 
 export const validate = async () => {
 
     try {
-        const cookieHeader = await getCookie('Authentication', {cookies})
+        const cookieHeader = await getCookie('Authentication', { cookies })
         const { data } = await shoppingApi.get('/auth', {
             headers: {
                 Cookie: `Authentication=${cookieHeader}`

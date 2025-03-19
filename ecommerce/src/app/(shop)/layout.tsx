fde98bcd2944179c674daf234767087621db4d4a
House197 <101058821+Houser97@users.noreply.github.com>
2025-03-19 09:00:04 -0600

    Check auth status

--- ecommerce/src/app/(shop)/layout.tsx
+++ ecommerce/src/app/(shop)/layout.tsx
@@ -1,12 +1,22 @@
+'use client'
+
 import Header from "@/components/header/Header";
+import { useAuthStore } from "@/store/auth/auth-store";
+import { useEffect } from "react";
 
 export default function ShopLayout({ children }: {
- children: React.ReactNode;
+  children: React.ReactNode;
 }) {
+  const checkAuthStatus = useAuthStore((state) => state.checkAuthStatus);
+
+  useEffect(() => {
+    checkAuthStatus();
+  }, []);
+
   return (
     <main className="min-h-screen overflow-x-hidden">
-        <Header />
-        { children }
+      <Header />
+      {children}
     </main>
   );
 }

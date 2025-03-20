c1f5b25cf091b4abbdfce5b0ef6e3a0b93ad69b6
House197 <101058821+Houser97@users.noreply.github.com>
2025-03-20 08:48:40 -0600

    Link cart store and update cart

--- ecommerce/src/components/ui/buttons/ProductBtns.tsx
+++ ecommerce/src/components/ui/buttons/ProductBtns.tsx
@@ -1,4 +1,9 @@
+'use client'
 
+import { createProduct, updateCart, updateProduct } from '@/actions/cart/cart';
+import { ProductCart } from '@/domain/entities/product.cart';
+import { useAuthStore } from '@/store/auth/auth-store';
+import { useCartStore } from '@/store/cart/cart-store';
 import Link from 'next/link';
 
 export const ProductBtns = ({ productId, reduceState = { numberOfProducts: 1 } }: {
@@ -8,37 +13,34 @@ export const ProductBtns = ({ productId, reduceState = { numberOfProducts: 1 } }
 
   /*reduceState se inicializa con 1 para el caso en que se agregue producto
   desde ProductData, en donde no se puede indicar el número de productos a agregar. */
-  //const { user, status } = useAuthStore();
-
+  const user = useAuthStore(state => state.user);
+  const status = useAuthStore(state => state.status);
+  const productsInCart = useCartStore(state => state.cart);
+  const updateCartData = useCartStore(state => state.updateCart);
   //const { productsInCart, createProduct, updateProduct, updateCart } = useCartStore();
-  const productsInCart = {}
 
-  let status = 'un'
+
 
   const handleProductAddition = async () => {
-    console.log(productsInCart)
-    console.log(productId)
     let productCartEntity;
+    console.log(productsInCart)
     if (!(productId in productsInCart)) {
 
-      //productCartEntity = await createProduct(user.id, productId, reduceState.numberOfProducts)
+      productCartEntity = await createProduct(user!.id, productId, reduceState.numberOfProducts)
     } else {
-      //const product = productsInCart[productId];
-      //console.log(product)
-      //productCartEntity = await updateProduct(product.id, reduceState.numberOfProducts)
-      console.log(productCartEntity)
+      const product = productsInCart[productId];
+      productCartEntity = await updateProduct(product.id, reduceState.numberOfProducts)
     }
 
-    console.log(productCartEntity)
-    //updateCart(productCartEntity);
-    //dispatch(addProductsToCart({ numberOfProducts: reduceState.numberOfProducts, id: productId }))
+    const cartData = updateCart(productCartEntity as ProductCart, productsInCart);
+    updateCartData(cartData);
   }
 
   return (
     <div>
       {status === 'authenticated' &&
         <div className='flex flex-row flex-wrap items-center justify-center gap-2'>
-          <button className='bg-[#ffa41c] font-bold p-2 w-24 rounded-lg text-whi' onClick={async () => await handleProductAddition()}>Add to cart</button>
+          <button className='bg-[#ffa41c] font-bold p-2 w-34 rounded-lg text-whi' onClick={async () => await handleProductAddition()}>Add to cart</button>
           <Link className={`justify-center bg-[#CDCDCD] font-bold rounded-lg text-black w-24 p-2 text-center ${false ? 'hidden' : 'flex'}`} href={`/review/${productId}`}>Review</Link>
         </div>
       }

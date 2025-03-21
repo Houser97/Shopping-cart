a5a5ed9e5f7a382763e7962f931b18bc394edba7
House197 <101058821+Houser97@users.noreply.github.com>
2025-03-21 08:54:39 -0600

    Persist cart store

--- ecommerce/src/store/cart/cart-store.ts
+++ ecommerce/src/store/cart/cart-store.ts
@@ -1,6 +1,7 @@
 import { CartData } from "@/actions/cart/cart";
 import { ProductCart } from "@/domain/entities/product.cart";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface State {
     cart: Record<string, ProductCart>,
@@ -9,12 +10,16 @@ interface State {
     updateCart: (cartData: CartData) => void
 }
 
-export const useCartStore = create<State>()((set) => ({
-    cart: {},
-    totalPrice: 0,
-    totalProducts: 0,
-    updateCart: (cartData) => {
-        const { productsInCart, totalPrice, totalProducts } = cartData;
-        set({ cart: productsInCart, totalPrice, totalProducts })
-    },
-}));
+export const useCartStore = create<State>()(
+    persist((set) => ({
+        cart: {},
+        totalPrice: 0,
+        totalProducts: 0,
+        updateCart: (cartData) => {
+            const { productsInCart, totalPrice, totalProducts } = cartData;
+            set({ cart: productsInCart, totalPrice, totalProducts })
+        },
+    }), {
+        name: 'shopping-cart'
+    })
+);

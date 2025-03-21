1ef5097b88de6e18a45be36be60595b49dd32556
House197 <101058821+Houser97@users.noreply.github.com>
2025-03-21 08:53:31 -0600

    Set reaction actions

--- ecommerce/src/actions/reactions/reactions.ts
+++ ecommerce/src/actions/reactions/reactions.ts
@@ -9,3 +9,30 @@ export const getReactions = async (productId: string) => {
         return {};
     }
 }
+
+
+export const createReaction = async (productId: string, reviewId: string, authorId: string, reaction: string) => {
+    try {
+        const createdReaction = await reactionRepositoryProvider.createReaction(productId, reviewId, authorId, reaction);
+        return createdReaction;
+    } catch (error) {
+        // if (error.status === 401) {
+        //     toast('Login to make this operation', ToastTypes.ERROR);
+        // } else {
+        //     toast(error.message, ToastTypes.ERROR);
+        // }
+        console.log(error);
+        return false;
+    }
+}
+
+export const updateReaction = async (id: string, reviewId: string, authorId: string, newReaction: string) => {
+    try {
+        const updatedReaction = await reactionRepositoryProvider.updateReaction(id, reviewId, authorId, newReaction);
+        return updatedReaction;
+    } catch (error) {
+        //toast(error.message, ToastTypes.ERROR);
+        return false;
+    }
+}
+

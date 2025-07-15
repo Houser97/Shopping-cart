431046323edae94e900a5b5edf19f76155c294a2
House197 <101058821+Houser97@users.noreply.github.com>
2025-07-15 06:46:15 -0600

    Set up DB Context/Settings

---
+++ ServerNET/Persistence/AppDbContext.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Persistence;
+
+public class AppDbContext
+{
+    private readonly IMongoDatabase _database;
+
+    public AppDbContext(IOptions<AppDbSettings> settings)
+    {
+        var client = new MongoClient(settings.Value.ConnectionString);
+        _database = client.GetDatabase(settings.Value.DatabaseName);
+    }
+
+    public IMongoDatabase Database => _database;
+}
---
+++ ServerNET/Persistence/AppDbSettings.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Persistence;
+
+public class AppDbSettings
+{
+    public string ConnectionString { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+    public string CartCollectionName { get; set; } = null!;
+    public string ProductsCollectionName { get; set; } = null!;
+    public string UsersCollectionName { get; set; } = null!;
+    public string ReviewsCollectionName { get; set; } = null!;
+    public string ReactionsCollectionName { get; set; } = null!;
+
+}

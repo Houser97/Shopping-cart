469a01498dd269cd2dd363dbf9dc285ef414eabf
House197 <101058821+Houser97@users.noreply.github.com>
2025-07-31 07:46:35 -0600

    Update ProductsService.cs
    Create method to get products with reviews.
    Create GetProductById mehod.

--- ServerNET/Application/Services/ProductsService.cs
+++ ServerNET/Application/Services/ProductsService.cs
@@ -23,34 +23,52 @@ public class ProductsService(
     private readonly IMongoCollection<Review> _reviewsCollection =
         dbContext.Database.GetCollection<Review>(settings.Value.ReviewsCollectionName);
 
+    private IAggregateFluent<ProductWithReviews> BuildBaseProductAggregation(IMongoCollection<Product> collection)
+    {
+        return collection.Aggregate()
+            .Lookup<Product, Review, ProductWithReviews>(
+                _reviewsCollection,
+                p => p.Id,
+                r => r.ProductId,
+                r => r.Reviews
+            )
+            .AppendStage<ProductWithReviews>(
+                new BsonDocument("$addFields", new BsonDocument
+                {
+                    { "rating", new BsonDocument("$avg", "$reviews.rating") },
+                    {
+                        "totalReviews", new BsonDocument(
+                            "$size", new BsonDocument(
+                                "$ifNull", new BsonArray { "$reviews", new BsonArray() }
+                            )
+                        )
+                    }
+                })
+            );
+    }
+
     public async Task<Result<ProductDto[]>> GetProductsAsync()
     {
         return await serviceHelper.ExecuteSafeAsync(async () =>
         {
-            var productWithReviews = await _productsCollection.Aggregate()
-                .Lookup<Product, Review, ProductWithReviews>(
-                    _reviewsCollection,
-                    product => product.Id,
-                    review => review.ProductId,
-                    result => result.Reviews
-                )
-                .AppendStage<ProductWithReviews>(
-                    new BsonDocument("$addFields", new BsonDocument
-                    {
-                        { "rating", new BsonDocument("$avg", "$reviews.rating") },
-                        {
-                            "totalReviews", new BsonDocument(
-                                "$size", new BsonDocument(
-                                    "$ifNull", new BsonArray { "$reviews", new BsonArray() }
-                                )
-                            )
-                        }
-                    })
-                )
+            var productWithReviews = await BuildBaseProductAggregation(_productsCollection)
                 .ToListAsync();
 
             var productDtos = mapper.Map<ProductDto[]>(productWithReviews);
-            return productDtos;
+            return Result<ProductDto[]>.Success(productDtos);
+        });
+    }
+
+    public async Task<Result<ProductDto>> GetProductById(string id)
+    {
+        return await serviceHelper.ExecuteSafeAsync(async () =>
+        {
+            var productWithReviews = await BuildBaseProductAggregation(_productsCollection)
+                .Match(p => p.Id == id)
+                .FirstOrDefaultAsync();
+
+            var productDto = mapper.Map<ProductDto>(productWithReviews);
+            return Result<ProductDto>.Success(productDto);
         });
     }
 }

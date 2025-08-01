851f29deeeefc09200597ec03f55d2044addc242
House197 <101058821+Houser97@users.noreply.github.com>
2025-08-01 09:03:38 -0600

    Create ReviewWithDetails.cs

---
+++ ServerNET/Application/Aggregates/ReviewWithDetails.cs
@@ -0,0 +1,11 @@
+using System;
+using Domain.Entities;
+
+namespace Application.Aggregates;
+
+public class ReviewWithDetails : Review
+{
+    public List<Reactions> Reactions { get; set; } = [];
+    public required User Author { get; set; }
+    public List<Ratings> Ratings { get; set; } = [];
+}

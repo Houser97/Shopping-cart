eaf95e38dc3ecc6e8997eecb7ac4e448632104eb
House197 <101058821+Houser97@users.noreply.github.com>
2025-08-04 07:59:49 -0600

    Create PagedResult.cs

---
+++ ServerNET/Application/Core/PagedResult.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Application.Core;
+
+public class PagedResult<T>
+{
+    public int Page { get; init; }
+    public int Limit { get; init; }
+    public long Total { get; init; }
+    public int TotalPages { get; init; }
+    public string? Next { get; init; }
+    public string? Prev { get; init; }
+}

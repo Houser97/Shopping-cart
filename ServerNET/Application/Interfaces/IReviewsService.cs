01e02b9d85b965d998dc4ccdad7c8eab7558fd66
House197 <101058821+Houser97@users.noreply.github.com>
2025-08-05 10:01:28 -0600

    Update IReviewsService.cs
    Add required methods

--- ServerNET/Application/Interfaces/IReviewsService.cs
+++ ServerNET/Application/Interfaces/IReviewsService.cs
@@ -2,11 +2,16 @@ using System;
 using Application.Core;
 using Application.DTOs.Reviews;
 using Application.Shared;
+using Domain.Entities;
 
 namespace Application.Interfaces;
 
 public interface IReviewsService
 {
-    Task<Result<PagedReviewsResultDto>> GetReviewsAsync(string productId, PaginationDto paginationDto);
-
+    Task<Result<PagedResult<ReviewPageDataDto>>> GetReviewsAsync(string productId, PaginationDto paginationDto);
+    Task<Result<ReviewDto>> GetReviewById(string id);
+    Task<Result<ReviewDto>> GetReviewByProductIdAndUserId(string productId, string userId);
+    Task<Result<ReviewDto>> CreateReview(CreateReviewDto createReviewDto);
+    Task<Result<ReviewDto>> UpdateReview(string id, UpdateReviewDto updateReviewDto);
+    Task<Result<ReviewDto>> DeleteReview(string id, string authorId);
 }

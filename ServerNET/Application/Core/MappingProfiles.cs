562fe2b23be7cb20a4bf7c71a2bb23f5ef16db68
House197 <101058821+Houser97@users.noreply.github.com>
2025-08-06 06:50:23 -0600

    Update MappingProfiles.cs
    Replace DTOs paths with Aggregates.
    Create UserDto, ReviewUserDto, ReactionDto and ReviewDto maps

--- ServerNET/Application/Core/MappingProfiles.cs
+++ ServerNET/Application/Core/MappingProfiles.cs
@@ -1,7 +1,11 @@
 using System;
-using Application.DTOs;
+using Application.Aggregates;
 using Application.DTOs.Products;
+using Application.DTOs.Reactions;
+using Application.DTOs.Reviews;
+using Application.DTOs.User;
 using AutoMapper;
+using Domain.Entities;
 
 namespace Application.Core;
 
@@ -11,5 +15,10 @@ public class MappingProfiles : Profile
     {
         // Se indica la fuente del mapeo y hacia donde se mapea.
         CreateMap<ProductWithReviews, ProductDto>();
+        CreateMap<User, UserDto>();
+        CreateMap<User, ReviewUserDto>();
+        CreateMap<Reactions, ReactionDto>();
+        CreateMap<ReviewWithDetails, ReviewDto>();
+        CreateMap<Review, ReviewDto>();
     }
 }

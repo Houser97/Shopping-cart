6c5d3545214e39d40710392482471d15edd450c1
House197 <101058821+Houser97@users.noreply.github.com>
2025-08-07 15:37:33 -0600

    Add TotalReactions to ReviewPageDataDto

--- ServerNET/Application/DTOs/Reviews/ReviewPageDataDto.cs
+++ ServerNET/Application/DTOs/Reviews/ReviewPageDataDto.cs
@@ -1,8 +1,10 @@
 using System;
+using Domain.Enums;
 
 namespace Application.DTOs.Reviews;
 
 public class ReviewPageDataDto
 {
     public List<ReviewDto> Reviews { get; set; } = [];
+    public Dictionary<string, Dictionary<ReactionType, int>> TotalReactions { get; set; } = [];
 }

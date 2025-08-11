0797946980c1a72995f0e8d0e56a071917bdfb64
House197 <101058821+Houser97@users.noreply.github.com>
2025-08-11 08:16:46 -0600

    Fix mongo config and register services

--- ServerNET/API/Program.cs
+++ ServerNET/API/Program.cs
@@ -1,7 +1,11 @@
 using System.Text.Json;
 using Application.Core;
 using Application.Interfaces;
+using Application.Interfaces.Repositories;
+using Application.Repositories;
 using Application.Services;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 using Persistence;
 using Persistence.Configurations;
 using Persistence.Interfaces;
@@ -17,10 +21,23 @@ builder.Services.Configure<AppDbSettings>(
     builder.Configuration.GetSection("Database")
 );
 
-builder.Services.AddSingleton<IAppDbContext, AppDbContext>();
+// MongoClient como Singleton (thread-safe)
+builder.Services.AddSingleton<IMongoClient>(sp =>
+{
+    var settings = sp.GetRequiredService<IOptions<AppDbSettings>>().Value;
+    return new MongoClient(settings.ConnectionString);
+});
+
+// AppDbContext como Scoped (usa MongoClient pero mantiene contexto del request)
+builder.Services.AddScoped<IAppDbContext, AppDbContext>();
+
+builder.Services.AddScoped<IReviewsRepository, ReviewsRepository>();
 
 builder.Services.AddScoped<IProductsService, ProductsService>();
 builder.Services.AddScoped<IReviewsService, ReviewsService>();
+builder.Services.AddScoped<IReactionsService, ReactionsService>();
+builder.Services.AddScoped<ICartService, CartService>();
+
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 builder.Services

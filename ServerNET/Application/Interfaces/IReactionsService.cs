eda83ac6d27524182dd3c375de41e2f73e25a70a
House197 <101058821+Houser97@users.noreply.github.com>
2025-08-12 09:01:24 -0600

    Set id parameter for UpdateReaction

--- ServerNET/Application/Interfaces/IReactionsService.cs
+++ ServerNET/Application/Interfaces/IReactionsService.cs
@@ -10,5 +10,5 @@ public interface IReactionsService
     Task<Result<List<ReactionDto>>> GetReactionsByProductIdAndAuthorId(string productId, string authorId);
     Task<Result<Dictionary<string, Dictionary<ReactionType, int>>>> GetReviewsTotalReactions(List<string> reviewIds);
     Task<Result<ReactionDto>> CreateReaction(CreateReactionDto createReactionDto);
-    Task<Result<ReactionDto>> UpdateReaction(UpdateReactionDto updateReactionDto);
+    Task<Result<ReactionDto>> UpdateReaction(string id, UpdateReactionDto updateReactionDto);
 }

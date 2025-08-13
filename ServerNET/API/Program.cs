8a545e5b6604cfbeb826b08f6d21bdfea5c15a51
House197 <101058821+Houser97@users.noreply.github.com>
2025-08-13 09:48:01 -0600

    Update Program.cs
    Inject repositories and set DatabaseInitializer

--- ServerNET/API/Program.cs
+++ ServerNET/API/Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped(typeof(IServiceHelper<>), typeof(ServiceHelper<>));
+
 // Database
 MongoDbConventions.Register();
 
@@ -31,12 +32,20 @@ builder.Services.AddSingleton<IMongoClient>(sp =>
 // AppDbContext como Scoped (usa MongoClient pero mantiene contexto del request)
 builder.Services.AddScoped<IAppDbContext, AppDbContext>();
 
+// Repositories
 builder.Services.AddScoped<IReviewsRepository, ReviewsRepository>();
+builder.Services.AddScoped<IProductsRepository, ProductsRepository>();
+builder.Services.AddScoped<IReactionsRepository, ReactionsRepository>();
+builder.Services.AddScoped<ICartRepository, CartRepository>();
 
+// Services
 builder.Services.AddScoped<IProductsService, ProductsService>();
 builder.Services.AddScoped<IReviewsService, ReviewsService>();
 builder.Services.AddScoped<IReactionsService, ReactionsService>();
 builder.Services.AddScoped<ICartService, CartService>();
+
+// DatabaseInitializer
+builder.Services.AddScoped<DatabaseInitializer>();
 
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
@@ -49,6 +58,13 @@ builder.Services
     });
 
 var app = builder.Build();
+
+// DatabaseInitializer
+using (var scope = app.Services.CreateScope())
+{
+    var dbInitializer = scope.ServiceProvider.GetRequiredService<DatabaseInitializer>();
+    await dbInitializer.InitializeAsync();
+}
 
 // Configure the HTTP request pipeline.
 

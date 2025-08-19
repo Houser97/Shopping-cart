2b024f6d9c5e77cebd46fe5d1c0f97ec4bc92dc0
House197 <101058821+Houser97@users.noreply.github.com>
2025-08-19 08:44:21 -0600

    Update CartRepository.cs
    Set BuildCartProductWithDetailsAggregation/GetCartProductWithDetails private methods and set cancellationToken

--- ServerNET/Application/Repositories/CartRepository.cs
+++ ServerNET/Application/Repositories/CartRepository.cs
@@ -22,58 +22,57 @@ public class CartRepository(
     private readonly IMongoCollection<Product> _productsCollection =
         dbContext.Database.GetCollection<Product>(settings.Value.ProductsCollectionName);
 
-    public async Task<bool> ClearAllAsync(string userId)
+
+
+    public async Task<bool> ClearAllAsync(string userId, CancellationToken cancellationToken = default)
     {
-        await _cartCollection.DeleteManyAsync(cartProduct => cartProduct.UserId == userId);
-        return true;
+        var result = await _cartCollection.DeleteManyAsync(
+            cartProduct => cartProduct.UserId == userId,
+            cancellationToken
+        );
+        return result.DeletedCount > 0;
     }
 
-    public async Task<Cart?> DeleteAsync(string id)
+    public async Task<Cart?> DeleteByIdAsync(string id, CancellationToken cancellationToken = default)
     {
-        return await _cartCollection.FindOneAndDeleteAsync(cartProduct => cartProduct.Id == id);
+        return await _cartCollection.FindOneAndDeleteAsync(
+            cartProduct => cartProduct.Id == id,
+            cancellationToken: cancellationToken
+        );
     }
 
-    public async Task<List<CartProductWithDetails>> GetAllByUserIdAsync(string userId)
+    public async Task<List<CartProductWithDetails>> GetAllByUserIdAsync(string userId, CancellationToken cancellationToken = default)
     {
         var filter = Builders<Cart>.Filter.Eq(cartProduct => cartProduct.UserId, userId);
-
-        var aggregation = _cartCollection.Aggregate()
-            .Match(filter)
-            .Lookup<Cart, Product, CartProductWithDetails>(
-                _productsCollection,
-                cartProduct => cartProduct.ProductId,
-                product => product.Id,
-                result => result.Product
-            )
-            .Unwind(cartProduct => cartProduct.Product)
-            .As<CartProductWithDetails>();
-
-        return await aggregation.ToListAsync();
+        return await BuildCartProductWithDetailsAggregation(filter).ToListAsync(cancellationToken);
     }
 
-    public async Task<Cart?> GetByProductIdAndUserId(string productId, string userId)
+    public async Task<Cart?> GetByProductIdAndUserId(string productId, string userId, CancellationToken cancellationToken = default)
     {
         return await _cartCollection.Find(
                 cartProduct => cartProduct.ProductId == productId && cartProduct.UserId == userId
-            ).FirstOrDefaultAsync();
+            )
+            .FirstOrDefaultAsync(cancellationToken);
     }
 
-    public async Task<Cart> InsertAsync(CreateCartProductDto createCartProductDto)
+    public async Task<CartProductWithDetails> InsertAsync(CreateCartProductDto createCartProductDto, CancellationToken cancellationToken = default)
     {
         var cartProduct = new Cart
         {
             ProductId = createCartProductDto.ProductId,
-            UserId = createCartProductDto.UserId,
+            UserId = createCartProductDto.UserId!,
             Quantity = createCartProductDto.Quantity,
             CreatedAt = DateTime.UtcNow
         };
 
-        await _cartCollection.InsertOneAsync(cartProduct);
+        await _cartCollection.InsertOneAsync(cartProduct, cancellationToken: cancellationToken);
 
-        return cartProduct;
+        var cartProductWithDetails = await GetCartProductWithDetails(cartProduct.Id, cancellationToken);
+
+        return cartProductWithDetails!;
     }
 
-    public async Task<Cart> UpdateAsync(string id, UpdateCartProductDto updateCartProductDto)
+    public async Task<CartProductWithDetails?> UpdateAsync(string id, UpdateCartProductDto updateCartProductDto, CancellationToken cancellationToken = default)
     {
         var filter = Builders<Cart>.Filter.Eq(cartProduct => cartProduct.Id, id);
         var update = Builders<Cart>.Update
@@ -84,7 +83,41 @@ public class CartRepository(
             ReturnDocument = ReturnDocument.After
         };
 
-        return await _cartCollection.FindOneAndUpdateAsync(filter, update, options);
+        var cartProduct = await _cartCollection.FindOneAndUpdateAsync(
+            filter,
+            update,
+            options,
+            cancellationToken
+        );
 
+        return (cartProduct is null || string.IsNullOrEmpty(cartProduct.Id))
+            ? null
+            : await GetCartProductWithDetails(cartProduct.Id, cancellationToken);
+    }
+
+    private async Task<CartProductWithDetails?> GetCartProductWithDetails(string id, CancellationToken cancellationToken = default)
+    {
+        var filter = Builders<Cart>.Filter.Eq(c => c.Id, id);
+        return await BuildCartProductWithDetailsAggregation(filter).FirstOrDefaultAsync(cancellationToken);
+    }
+
+    private async Task<CartProductWithDetails?> GetCartProductWithDetails(string id)
+    {
+        var filter = Builders<Cart>.Filter.Eq(c => c.Id, id);
+        return await BuildCartProductWithDetailsAggregation(filter).FirstOrDefaultAsync();
+    }
+
+    private IAggregateFluent<CartProductWithDetails> BuildCartProductWithDetailsAggregation(FilterDefinition<Cart> filter)
+    {
+        return _cartCollection.Aggregate()
+            .Match(filter)
+            .Lookup<Cart, Product, CartProductWithDetails>(
+                _productsCollection,
+                c => c.ProductId,
+                p => p.Id,
+                result => result.Product
+            )
+            .Unwind(c => c.Product)
+            .As<CartProductWithDetails>();
     }
 }

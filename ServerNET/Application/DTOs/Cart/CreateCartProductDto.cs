37c58ee58aceeef1f21a13b73a92cf541211c6ff
House197 <101058821+Houser97@users.noreply.github.com>
2025-08-19 08:40:17 -0600

    Update CreateCartProductDto.cs
    Make UserId field optional

--- ServerNET/Application/DTOs/Cart/CreateCartProductDto.cs
+++ ServerNET/Application/DTOs/Cart/CreateCartProductDto.cs
@@ -4,7 +4,7 @@ namespace Application.DTOs.Cart;
 
 public class CreateCartProductDto
 {
-    public string UserId { get; set; } = string.Empty;
+    public string? UserId { get; set; }
     public string ProductId { get; set; } = string.Empty;
     public int Quantity { get; set; } = 0;
 }

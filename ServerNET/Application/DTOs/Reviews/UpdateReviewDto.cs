a8f9c0e8401b8d8f51ec61a38f87d7f0bde1cec6
House197 <101058821+Houser97@users.noreply.github.com>
2025-08-21 07:55:45 -0600

    Make AuthorId optional in Reaction Dtos

--- ServerNET/Application/DTOs/Reviews/UpdateReviewDto.cs
+++ ServerNET/Application/DTOs/Reviews/UpdateReviewDto.cs
@@ -4,7 +4,7 @@ namespace Application.DTOs.Reviews;
 
 public class UpdateReviewDto
 {
-    public required string AuthorId { get; set; }
+    public string? AuthorId { get; set; }
     public required string Comment { get; set; }
     public required int Rating { get; set; }
 

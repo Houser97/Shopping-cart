c2e0ba19e077a9e2f979511a72107ee7399be101
House197 <101058821+Houser97@users.noreply.github.com>
2025-08-21 07:58:52 -0600

    Make AuthorId optional/set JsonStringEnumConverter

--- ServerNET/Application/DTOs/Reactions/CreateReactionDto.cs
+++ ServerNET/Application/DTOs/Reactions/CreateReactionDto.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.Json.Serialization;
 using Domain.Enums;
 
 namespace Application.DTOs.Reactions;
@@ -7,6 +8,8 @@ public class CreateReactionDto
 {
     public required string ProductId { get; set; }
     public required string ReviewId { get; set; }
-    public required string AuthorId { get; set; }
+    public string? AuthorId { get; set; }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))] // Ayuda a poder mandar el string en la request en lugar del entero del enum
     public required ReactionType Reaction { get; set; }
 }

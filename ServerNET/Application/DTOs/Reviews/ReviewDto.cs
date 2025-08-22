9aaaf0be4a94393f91c00628cb145d3b9ea9247d
House197 <101058821+Houser97@users.noreply.github.com>
2025-08-22 06:40:33 -0600

    Include Rating field in ReviewDto

--- ServerNET/Application/DTOs/Reviews/ReviewDto.cs
+++ ServerNET/Application/DTOs/Reviews/ReviewDto.cs
@@ -9,6 +9,7 @@ public class ReviewDto
     public string Id { get; set; } = string.Empty;
     public string ProductId { get; set; } = string.Empty;
     public string Comment { get; set; } = string.Empty;
+    public int Rating { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public List<ReactionDto> Reactions { get; set; } = [];
     public required ReviewUserDto Author { get; set; }

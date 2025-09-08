b0b89eed40228399e7b9b22c4ba8db6002fdb4fb
House197 <101058821+Houser97@users.noreply.github.com>
2025-09-08 08:35:00 -0600

    Update review.mapper.ts
    Remove underscore from id fiield

--- client/src/infrastructure/mappers/review.mapper.ts
+++ client/src/infrastructure/mappers/review.mapper.ts
@@ -4,14 +4,14 @@ import { ReviewDB } from "../interfaces/review-db.response";
 export class ReviewMapper {
     static fromDbCastToDetailedReview(review: ReviewDB): DetailedReview {
         return {
-            id: review._id,
+            id: review.id,
             productId: review.productId,
             authorId: review.authorId,
             rating: review.rating,
             comment: review.comment,
             createdAt: review.createdAt,
             reactions: review.reactions,
-            author: { ...review.author, id: review._id },
+            author: { ...review.author, id: review.id },
         }
     }
 
